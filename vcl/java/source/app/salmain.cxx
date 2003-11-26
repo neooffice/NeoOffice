10b00064c114d9439981c0822e231fb2be4a1012
pluby <>
2003-11-26 16:35:09 +0000

    Remove keyboard layout change hacks as they cause too many other bugs

--- vcl/java/source/app/salmain.cxx
+++ vcl/java/source/app/salmain.cxx
@@ -57,19 +57,12 @@
 #include <vos/module.hxx>
 #endif
 #include <crt_externs.h>
-#include <premac.h>
-#include <Carbon/Carbon.h>
-#include <postmac.h>
-
-typedef OSStatus ReceiveNextEvent_Type( UInt32, const EventTypeSpec *, EventTimeout, MacOSBoolean, EventRef * );
 
 class SVMainThread : public ::vos::OThread
 {
 protected:
 	virtual void run() { SVMain(); _exit( 0 ); }
 };
-
-static ::vos::OMutex aCarbonLock;
 
 using namespace rtl;
 using namespace vcl;
@@ -78,40 +71,6 @@ using namespace vos;
 #endif
 
 // ============================================================================
-
-#ifdef MACOSX
-static jint JNICALL Java_com_apple_mrj_macos_carbon_CarbonLock_acquire0( JNIEnv *pEnv, jobject object )
-{
-	return ( aCarbonLock.tryToAcquire() ? 0 : 1 );
-}
-#endif
-
-// ----------------------------------------------------------------------------
-
-#ifdef MACOSX
-static void JNICALL Java_com_apple_mrj_macos_carbon_CarbonLock_init( JNIEnv *pEnv, jobject object )
-{
-}
-#endif
-
-// ----------------------------------------------------------------------------
-
-#ifdef MACOSX
-static jint JNICALL Java_com_apple_mrj_macos_carbon_CarbonLock_release0( JNIEnv *pEnv, jobject object )
-{
-	BOOL bAcquired = aCarbonLock.tryToAcquire();
-
-	if ( bAcquired )
-	{
-		aCarbonLock.release();
-		aCarbonLock.release();
-	}
-
-	return ( bAcquired ? 0 : 1 );
-}
-#endif
-
-// ----------------------------------------------------------------------------
 
 BEGIN_C
 
@@ -216,48 +175,7 @@ int main( int argc, char *argv[] )
 	}
 	else
 	{
-		// Panther expects applications to run their event loop in main thread
-		// and Java 1.3.1 runs its event loop in a separate thread. So, we need
-		// to disable the lock that Java 1.3.1 uses and simultaneously run an
-		// event loop in this thread.
-		VCLThreadAttach t;
-		if ( t.pEnv )
-		{
-			jclass carbonLockClass = t.pEnv->FindClass( "com/apple/mrj/macos/carbon/CarbonLock" );
-			if ( carbonLockClass )
-			{
-				JNINativeMethod pMethods[3];
-				pMethods[0].name = "acquire0";
-				pMethods[0].signature = "()I";
-				pMethods[0].fnPtr = Java_com_apple_mrj_macos_carbon_CarbonLock_acquire0;
-				pMethods[1].name = "init";
-				pMethods[1].signature = "()V";
-				pMethods[1].fnPtr = Java_com_apple_mrj_macos_carbon_CarbonLock_init;
-				pMethods[2].name = "release0";
-				pMethods[2].signature = "()I";
-				pMethods[2].fnPtr = Java_com_apple_mrj_macos_carbon_CarbonLock_release0;
-				t.pEnv->RegisterNatives( carbonLockClass, pMethods, sizeof( pMethods ) / sizeof( JNINativeMethod* ) );
-			}
-		}
-
-		// Load Carbon
-		OModule aModule;
-		aModule.load( OUString::createFromAscii( "/System/Library/Frameworks/Carbon.framework/Carbon" ) );
-
-		// Create the SVMain() thread
-		SVMainThread aThread;
-		aThread.create();
-
-		// Start the Carbon event loop
-		ReceiveNextEvent_Type *pReceiveNextEvent = (ReceiveNextEvent_Type *)aModule.getSymbol( OUString::createFromAscii( "ReceiveNextEvent" ) );
-		if ( pReceiveNextEvent )
-		{
-			for ( ; ; )
-				 pReceiveNextEvent( 0, NULL, kEventDurationForever, false, NULL );
-		}
-
-		aModule.unload();
-		aThread.join();
+		SVMain();
 	}
 #else	// MACOSX
 	SVMain();
