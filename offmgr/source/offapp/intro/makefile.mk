b1cb8fa256063f8d889a7cbd89f24a324c752873
pluby <>
2004-06-01 19:54:12 +0000

    Port OOo 1.0.3 modifications to OOo 1.1 codebase

--- offmgr/source/offapp/intro/makefile.mk
+++ offmgr/source/offapp/intro/makefile.mk
@@ -33,7 +33,7 @@
 #   MA  02111-1307  USA
 #   
 #   =================================================
-#   Modified June 2003 by Patrick Luby. SISSL Removed. NeoOffice is
+#   Modified June 2004 by Patrick Luby. SISSL Removed. NeoOffice is
 #   distributed under GPL only under modification term 3 of the LGPL.
 #
 #   Contributor(s): _______________________________________
@@ -47,16 +47,25 @@ TARGET=intro
 
 # --- Settings ------------------------------------------------------------
 
+
 .INCLUDE :  settings.mk
 
 RSCLOCINC!:=$(RSCLOCINC);$(PRJ)$/RES
+
+# setting the compiled by $user$ using $license$ string in the about box
+
+.IF "$(BUILD_SPECIAL)"==""
+RSCUPDVERMAC+=-DOOO_VENDOR="$(USER)$(USERNAME)" -DOOO_LICENSE="LGPL"
+.ELSE
+RSCUPDVERMAC+=-DBUILD_SPECIAL=True
+.ENDIF
+
 
 .INCLUDE :  $(UPD)minor.mk
 RSCUPDVER=$(RSCREVISION)
 
 #RSCUPDVER=$(_SDT_REVISION)(SV$(UPD)$(UPDMINOR))
-IMGLST_SRS=$(SRS)$/iso.srs
-ADD_IMGLST_SRS=$(SRS)$/ooo.srs $(SRS)$/oem.srs
+IMGLST_SRS=$(SRS)$/iso.srs $(SRS)$/ooo.srs
 
 # --- Allgemein -----------------------------------------------------------
 
@@ -95,14 +104,8 @@ RESLIB4SRSFILES= \
 	$(SRS)$/$(SRS4NAME).srs
 
 .IF "$(GUIBASE)"=="java"
-# Version "NeoOffice/J"
 RSCDEFS += -DPRODUCT_NAME="$(PRODUCT_NAME)" -DPRODUCT_VERSION="$(PRODUCT_VERSION)"
 SRS5NAME= neojava
-SRC5FILES=	\
-	$(SRS5NAME).src
-.ELSE
-# Version "NeoOffice"
-SRS5NAME= neo
 SRC5FILES=	\
 	$(SRS5NAME).src
 .ENDIF
@@ -111,42 +114,7 @@ RESLIB5NAME=$(SRS5NAME)
 RESLIB5SRSFILES= \
 	$(SRS)$/$(SRS5NAME).srs
 
-.IF "$(ADD_IMGLST_SRS)"!=""
-.IF "$(make_srs_deps)"==""
-.IF "$(common_build_reslib)"!=""
-ADD_IMGLSTTARGET=$(foreach,j,$(ADD_IMGLST_SRS) $(foreach,i,$(alllangext) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(MISC))$/$(j:b)_img$i.don))
-.ELSE			# "$(common_build_reslib)"!=""
-ADD_IMGLSTTARGET=$(foreach,j,$(ADD_IMGLST_SRS) $(foreach,i,$(alllangext) $(MISC)$/$(j:b)_img$i.don))
-.ENDIF			# "$(common_build_reslib)"!=""
-.ENDIF			# "$(make_srs_deps)"==""
-.ENDIF			# "$(ADD_IMGLST_SRS)"!=""
-
-
 .INCLUDE :  target.mk
 
 ALLTAR : $(ADD_IMGLSTTARGET)
 
-.IF "$(RESLIB4TARGETN)"!=""
-$(RESLIB4TARGETN) :  $(ADD_IMGLSTTARGET)
-.ENDIF			# "$(RESLIB4TARGETN)"!=""
-
-.IF "$(ADD_IMGLSTTARGET)"!=""
-$(ADD_IMGLSTTARGET): $(ADD_IMGLST_SRS)
-	@+echo -----------------
-	@+echo Making Imagelists:
-	@+echo -----------------
-	@+-$(RM) $@ >& $(NULLDEV)
-.IF "$(common_build_reslib)"!=""
-	@-+$(MKDIR) $(RES)$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) >& $(NULLDEV)
-	@-+$(MKDIR) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(RES))$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) >& $(NULLDEV)
-	+$(BMP) $(SRS)$/$(@:b:s/_img/ /:1).srs $(BMP_IN) $(BMP_OUT)$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) $(lang_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) -f $@
-	-+$(GNUCOPY) -pub $(RES)$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))})/* $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(RES))$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) >& $(NULLDEV)
-	+-$(RM) $(subst,$(OUTPATH),$(COMMON_OUTDIR) $(RES))$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))})$/*.bmp~
-.ELSE			# "$(common_build_reslib)"!=""
-	@-+$(MKDIR) $(RES)$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) >& $(NULLDEV)
-	+$(BMP) $(SRS)$/$(@:b:s/_img/ /:1).srs $(BMP_IN) $(BMP_OUT)$/$(langext_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) $(lang_{$(subst,$(@:b:s/_img/ /:1)_img, $(@:b))}) -f $@
-.ENDIF			# "$(common_build_reslib)"!=""
-.IF "$(BMP_WRITES_FLAG)"==""
-	@+echo > $@
-.ENDIF
-.ENDIF
