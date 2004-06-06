8d3bfea412d0752df36c3f7a680a923615c9b4b9
pluby <>
2004-06-06 23:05:39 +0000

    Remove X11 from link list

---
+++ extensions/source/plugin/util/makefile.mk
@@ -0,0 +1,114 @@
+##########################################################################
+# 
+#   $RCSfile$
+# 
+#   $Revision$
+# 
+#   last change: $Author$ $Date$
+# 
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+# 
+#          - GNU General Public License Version 2.1
+# 
+#   Sun Microsystems Inc., October, 2000
+# 
+#   GNU General Public License Version 2.1
+#   =============================================
+#   Copyright 2000 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+# 
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public
+#   License version 2.1, as published by the Free Software Foundation.
+# 
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#   
+#   =================================================
+#   Modified June 2004 by Patrick Luby. SISSL Removed. NeoOffice is
+#   distributed under GPL only under modification term 3 of the LGPL.
+# 
+#   Contributor(s): _______________________________________
+# 
+##########################################################################
+
+PRJ=..$/..$/..
+
+PRJNAME=extensions
+TARGET=pl
+TARGETTYPE=GUI
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+# --- Files --------------------------------------------------------
+
+LIB1TARGET = $(SLB)$/plall.lib
+LIB1FILES  = \
+	$(SLB)$/plbase.lib	\
+	$(SHL1LINKLIB)
+
+.IF "$(GUI)" == "UNX"
+SHL1LINKLIB = $(SLB)$/plunx.lib
+.IF "$(OS)" == "SOLARIS"
+SHL1OWNLIBS = -lsocket
+.ENDIF # SOLARIS
+.ENDIF # UNX
+
+.IF "$(GUI)" == "WNT"
+SHL1LINKLIB = $(SLB)$/plwin.lib
+SHL1OWNLIBS = \
+	version.lib	\
+	ole32.lib	\
+	advapi32.lib
+.ENDIF # WNT
+
+.IF "$(GUI)" == "OS2"
+SHL1LINKLIB = $(SLB)$/plos2.lib
+.ENDIF # OS2
+
+SHL1TARGET= $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1IMPLIB= i$(TARGET)
+
+SHL1VERSIONMAP=exports.map
+SHL1DEF=$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=$(SHL1TARGET)
+
+SHL1LIBS=$(LIB1TARGET)
+
+.IF "$(OS)"=="MACOSX"
+SHL1STDLIBS= \
+	$(LIBSTLPORT)		\
+	$(TKLIB)
+.ELSE
+SHL1STDLIBS= \
+	$(TKLIB)
+.ENDIF
+
+SHL1STDLIBS+= \
+	$(VCLLIB)			\
+	$(SVLLIB)			\
+	$(TOOLSLIB)			\
+	$(VOSLIB)			\
+	$(UCBHELPERLIB)		\
+	$(CPPUHELPERLIB)	\
+	$(CPPULIB)			\
+	$(SALLIB)
+
+SHL1STDLIBS+=$(SHL1OWNLIBS)
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE :  target.mk
+
+
+
