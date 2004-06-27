3212b60a41ed64786d42c3541f6cc7eb4d4ddcae
pluby <>
2004-06-27 04:35:17 +0000

    Add back passing of the locale to the setup script and rerun the script with the "-repair" argument if the script fails

--- desktop/source/app/app.cxx
+++ desktop/source/app/app.cxx
@@ -40,67 +40,6 @@
  *
  ************************************************************************/
 
-/*************************************************************************
- *
- *  $RCSfile$
- *
- *  $Revision$
- *
- *  last change: $Author$ $Date$
- *
- *  The Contents of this file are made available subject to the terms of
- *  either of the following licenses
- *
- *         - GNU Lesser General Public License Version 2.1
- *         - Sun Industry Standards Source License Version 1.1
- *
- *  Sun Microsystems Inc., October, 2000
- *
- *  GNU Lesser General Public License Version 2.1
- *  =============================================
- *  Copyright 2000 by Sun Microsystems, Inc.
- *  901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License version 2.1, as published by the Free Software Foundation.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
- *
- *
- *  Sun Industry Standards Source License Version 1.1
- *  =================================================
- *  The contents of this file are subject to the Sun Industry Standards
- *  Source License Version 1.1 (the "License"); You may not use this file
- *  except in compliance with the License. You may obtain a copy of the
- *  License at http://www.openoffice.org/license.html.
- *
- *  Software provided under this License is provided on an "AS IS" basis,
- *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
- *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
- *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
- *  See the License for the specific provisions governing your rights and
- *  obligations concerning the Software.
- *
- *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
- *
- *  Copyright: 2000 by Sun Microsystems, Inc.
- *
- *  All Rights Reserved.
- *
- *  Contributor(s): _______________________________________
- *
- *
- ************************************************************************/
-
 #include <unistd.h>
 #include "app.hxx"
 #include "desktop.hrc"
@@ -759,20 +698,67 @@ void Desktop::StartSetup( const OUString& aParameters )
 #endif
     }
 
-    OUString                aArgListArray[1];
     ::vos::OSecurity        aSecurity;
     ::vos::OEnvironment        aEnv;
     ::vos::OArgumentList    aArgList;
+    ::vos::OProcess    aProcess( aProgName, aDir );
 
+#ifdef USE_JAVA
+    // Wait for execution to finish since Java is so dependent on it
+    OUString aArgListArray[4];
+    rtl_Locale *pLocale;
+    if ( osl_getProcessLocale( &pLocale ) == osl_Process_E_None )
+    {
+        aArgListArray[0] = OUString( RTL_CONSTASCII_USTRINGPARAM( "-locale" ) );
+        aArgListArray[1] = OUString( rtl_locale_getLanguage( pLocale ) );
+        OUString aCountry = OUString( rtl_locale_getCountry( pLocale ) );
+        if ( aCountry.getLength() )
+        {
+            aArgListArray[1] += OUString( RTL_CONSTASCII_USTRINGPARAM( "-" ) );
+            aArgListArray[1] += OUString( aCountry );
+        }
+        aArgListArray[2] = aParameters;
+    }
+    else
+    {
+        aArgListArray[0] = aParameters;
+        aArgListArray[1] = OUString();
+        aArgListArray[2] = OUString();
+    }
+    aArgListArray[3] = OUString();
+    OArgumentList aArgumentList( aArgListArray, 4 );
+
+    ::vos::OProcess::TProcessError aProcessError =
+        aProcess.execute( OProcess::TOption_Wait,
+                          aSecurity,
+                          aArgumentList,
+                          aEnv );
+#else	// USE_JAVA
+    OUString                aArgListArray[1];
     aArgListArray[0] = aParameters;
     OArgumentList aArgumentList( aArgListArray, 1 );
 
-    ::vos::OProcess    aProcess( aProgName, aDir );
     ::vos::OProcess::TProcessError aProcessError =
         aProcess.execute( OProcess::TOption_Detached,
                           aSecurity,
                           aArgumentList,
                           aEnv );
+#endif	// USE_JAVA
+
+#ifdef USE_JAVA
+	// If the setup command failed, try running it with the "-repair" argument
+    if ( aProcessError != OProcess::E_None )
+    {
+        aArgListArray[3] = OUString( RTL_CONSTASCII_USTRINGPARAM( "-repair" ) );
+        aArgumentList = OArgumentList( aArgListArray, 4 );
+
+        ::vos::OProcess::TProcessError aProcessError =
+            aProcess.execute( OProcess::TOption_Wait,
+                              aSecurity,
+                              aArgumentList,
+                              aEnv );
+    }
+#endif	// USE_JAVA
 
     if ( aProcessError != OProcess::E_None )
     {
@@ -783,11 +769,6 @@ void Desktop::StartSetup( const OUString& aParameters )
         ErrorBox aBootstrapFailedBox( NULL, WB_OK, aMessage );
         aBootstrapFailedBox.Execute();
     }
-
-#ifdef USE_JAVA
-    // Wait for execution to finish since Java is so dependent on it
-	aProcess.join();
-#endif	// USE_JAVA
 }
 
 void Desktop::HandleBootstrapPathErrors( ::utl::Bootstrap::Status aBootstrapStatus, const OUString& aDiagnosticMessage )
