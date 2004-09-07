907d9b84fd31d70a51754854b40fedb90045ee49
pluby <>
2004-09-07 18:22:53 +0000

    Don't replace white space glyphs

--- vcl/java/source/gdi/salatslayout.cxx
+++ vcl/java/source/gdi/salatslayout.cxx
@@ -460,7 +460,7 @@ bool SalATSLayout::LayoutText( ImplLayoutArgs& rArgs )
 				nCharWidth = Float32ToLong( ( mpGlyphInfoArray->glyphs[ i + 1 ].idealX - mpGlyphInfoArray->glyphs[ i ].idealX ) * mnUnitsPerPixel );
 			}
 
-			if ( mpGlyphInfoArray->glyphs[ i ].layoutFlags & ( kATSGlyphInfoIsWhiteSpace | kATSGlyphInfoTerminatorGlyph ) || nGlyph >= 0xffff )
+			if ( nGlyph >= 0xffff )
 				nGlyph = 3;
 
 			int nGlyphFlags = nCharWidth ? 0 : GlyphItem::IS_IN_CLUSTER;
