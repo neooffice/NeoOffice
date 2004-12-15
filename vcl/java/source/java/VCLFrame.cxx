6342a2386af3882cd2661c6c1f9ccb7181d86405
pluby <>
2004-12-15 19:08:22 +0000

    Attempt to fix bug 261 by flushing the port buffer before destroying the native window

--- vcl/java/source/java/VCLFrame.cxx
+++ vcl/java/source/java/VCLFrame.cxx
@@ -212,6 +212,19 @@ void com_sun_star_vcl_VCLFrame::dispose()
 			mID = t.pEnv->GetMethodID( getMyClass(), "dispose", cSignature );
 		}
 		OSL_ENSURE( mID, "Unknown method id!" );
+
+#ifdef MACOSX
+		// Test the JVM version and if it is below 1.4, use Carbon APIs
+		if ( t.pEnv->GetVersion() < JNI_VERSION_1_4 )
+		{
+			// Fix bug 261 by explicitly flushing the window's buffer before
+			// destroying it
+			WindowRef aWindow = (WindowRef)getNativeWindow();
+			if ( aWindow )
+				QDFlushPortBuffer( GetWindowPort( aWindow ), NULL );
+		}
+#endif	// MACOSX
+
 		if ( mID )
 			t.pEnv->CallNonvirtualVoidMethod( object, getMyClass(), mID );
 	}
