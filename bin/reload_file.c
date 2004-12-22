011a38cb1cc916709817836fb254ac81e7992f79
pluby <>
2004-12-22 20:36:43 +0000

    Initial commit

---
+++ bin/reload_file.c
@@ -0,0 +1,52 @@
+/*************************************************************************
+ *
+ *  $RCSfile$
+ *
+ *  $Revision$
+ *
+ *  last change: $Author$ $Date$
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *         - GNU General Public License Version 2.1
+ *
+ *  Patrick Luby, June 2003
+ *
+ *  GNU General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2003 by Patrick Luby (patrick.luby@planamesa.com)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <mach-o/dyld.h>
+
+int main(int argc, char **argv)
+{
+	if ( argc != 2 || access( argv[1], R_OK ) != 0 )
+	{
+		fprintf( stderr, "Usage: %s file\n", argv[0] );
+		exit(1);
+	}
+
+	if ( NSAddImage( argv[1], NSADDIMAGE_OPTION_RETURN_ON_ERROR ) )
+		exit(0);
+
+	exit(1);
+}
