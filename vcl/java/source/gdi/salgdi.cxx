bea5b2129609af29a82d45c59be8d0aff747a86b
pluby <>
2005-01-21 06:52:18 +0000

    Add EPS header and footer data

--- vcl/java/source/gdi/salgdi.cxx
+++ vcl/java/source/gdi/salgdi.cxx
@@ -62,6 +62,12 @@
 #ifndef _OSL_CONDITN_HXX_
 #include <osl/conditn.hxx>
 #endif
+#ifndef _RTL_STRBUF_HXX_
+#include <rtl/strbuf.hxx>
+#endif
+#ifndef _STREAM_HXX
+#include <tools/stream.hxx>
+#endif
 
 #include <premac.h>
 #include <Carbon/Carbon.h>
@@ -70,18 +76,275 @@
 struct EPSData
 {
 	PMPrintSession		maSession;
+	long				mnX;
+	long				mnY;
+	long				mnWidth;
+	long				mnHeight;
 	MacOSPtr			mpPtr;
 	ULONG				mnSize;
 	::osl::Condition	maCondition;
+	BOOL				mbSuccess;
 };
 
 static EventLoopTimerUPP pEventLoopTimerUPP = NULL;
 
+using namespace rtl;
+
 #endif	// MACOSX
+
+static double fExpValues[] = { 1.0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 1e19, 1e20 };
 
 using namespace vcl;
 
 // =======================================================================
+
+inline int isSpace( char cChar )
+{
+	return cChar == ' ' || cChar == '\t' || cChar == '\r' || cChar == '\n' || cChar == 0x0c || cChar == 0x0b;
+}
+
+// -----------------------------------------------------------------------
+
+inline int isProtect( char cChar )
+{
+	return cChar == '`' || cChar == '\'' || cChar == '"';
+}
+
+// -----------------------------------------------------------------------
+
+inline void copyUntil( char*& pTo, const char*& pFrom, char cUntil, int bIncludeUntil = 0 )
+{
+	do
+	{
+		if ( *pFrom == '\\' )
+		{
+			pFrom++;
+			if ( *pFrom )
+			{
+				*pTo = *pFrom;
+				pTo++;
+			}
+		}
+		else if ( bIncludeUntil || ! isProtect( *pFrom ) )
+		{
+			*pTo = *pFrom;
+			pTo++;
+		}
+		pFrom++;
+	} while ( *pFrom && *pFrom != cUntil );
+	// copy the terminating character unless zero or protector
+	if ( ! isProtect( *pFrom ) || bIncludeUntil )
+	{
+		*pTo = *pFrom;
+		if ( *pTo )
+			pTo++;
+	}
+	if ( *pFrom )
+		pFrom++;
+}
+
+// -----------------------------------------------------------------------
+
+static ByteString GetCommandLineToken( int nToken, const ByteString& rLine )
+{
+	int nLen = rLine.Len();
+	if ( ! nLen )
+		return ByteString();
+
+	int nActualToken = 0;
+	char* pBuffer = (char*)alloca( nLen + 1 );
+	const char* pRun = rLine.GetBuffer();
+	char* pLeap;
+
+	while ( *pRun && nActualToken <= nToken )
+	{
+		while ( *pRun && isSpace( *pRun ) )
+			pRun++;
+		pLeap = pBuffer;
+		while ( *pRun && ! isSpace( *pRun ) )
+		{
+			if ( *pRun == '\\' )
+			{
+				// escapement
+				pRun++;
+				*pLeap = *pRun;
+				pLeap++;
+				if ( *pRun )
+					pRun++;
+			}
+			else if ( *pRun == '`' )
+				copyUntil( pLeap, pRun, '`' );
+			else if ( *pRun == '\'' )
+				copyUntil( pLeap, pRun, '\'' );
+			else if ( *pRun == '"' )
+				copyUntil( pLeap, pRun, '"' );
+			else
+			{
+				*pLeap = *pRun;
+				pLeap++;
+				pRun++;
+			}
+		}
+		if ( nActualToken != nToken )
+			pBuffer[0] = 0;
+		nActualToken++;
+	}
+
+	*pLeap = 0;
+
+	ByteString aRet( pBuffer );
+	return aRet;
+}
+
+// -----------------------------------------------------------------------
+
+static ByteString WhitespaceToSpace( const ByteString& rLine )
+{
+	int nLen = rLine.Len();
+	if ( ! nLen )
+		return ByteString();
+
+	char *pBuffer = (char*)alloca( nLen + 1 );
+	const char *pRun = rLine.GetBuffer();
+	char *pLeap = pBuffer;
+
+	while ( *pRun )
+	{
+		if ( *pRun && isSpace( *pRun ) )
+		{
+			*pLeap = ' ';
+			pLeap++;
+			pRun++;
+		}
+		while ( *pRun && isSpace( *pRun ) )
+			pRun++;
+		while ( *pRun && ! isSpace( *pRun ) )
+		{
+			if ( *pRun == '\\' )
+			{
+				// escapement
+				pRun++;
+				*pLeap = *pRun;
+				pLeap++;
+				if ( *pRun )
+					pRun++;
+			}
+			else if ( *pRun == '`' )
+				copyUntil( pLeap, pRun, '`', TRUE );
+			else if ( *pRun == '\'' )
+				copyUntil( pLeap, pRun, '\'', TRUE );
+			else if ( *pRun == '"' )
+				copyUntil( pLeap, pRun, '"', TRUE );
+			else
+			{
+				*pLeap = *pRun;
+				*pLeap++;
+				*pRun++;
+			}
+		}
+	}
+
+	*pLeap = 0;
+
+	// there might be a space at beginning or end
+	pLeap--;
+	if ( *pLeap == ' ' )
+		*pLeap = 0;
+
+	ByteString aRet( *pBuffer == ' ' ? pBuffer+1 : pBuffer );
+	return aRet;
+}
+
+// -----------------------------------------------------------------------
+
+static double calcPow10( int nExp )
+{
+	BOOL bNeg = nExp < 0;
+	nExp = bNeg ? -nExp : nExp;
+	double fRet = 1.0;
+	while ( nExp >= 20 )
+	{
+		fRet *= fExpValues[20];
+		nExp -= 20;
+	}
+	fRet *= fExpValues[ nExp ];
+	return bNeg ? 1.0/fRet : fRet;
+}
+
+// -----------------------------------------------------------------------
+
+static double StringToDouble( const ByteString& rStr )
+{
+	const char* pStr	= rStr.GetBuffer();
+	const char* pOrg	= pStr;
+	const int nLen	  = rStr.Len();
+
+	BOOL bExp = FALSE, bNeg = FALSE, bNegExp = FALSE, bDecimal=FALSE;
+	int nExp = 0;
+	double fRet = 0.0, fDiv = 0.1;
+
+	while ( isSpace( *pStr ) )
+		pStr++;
+	for( ; pStr - pOrg < nLen; pStr++ )
+	{
+		if ( *pStr >= '0' && *pStr <= '9' )
+		{
+			if ( bExp )
+				nExp = nExp*10 + ( *pStr - '0' );
+			else if ( ! bDecimal )
+				fRet = 10.0 * fRet + (double)( *pStr - '0' );
+			else
+			{
+				fRet += fDiv * (double)( *pStr - '0' );
+				fDiv /= 10.0;
+			}
+		}
+		else if ( *pStr == '.' )
+		{
+			if ( bExp || bDecimal )
+				break;
+			bDecimal = TRUE;
+		}
+		else if ( *pStr == '-' )
+		{
+			if ( bExp )
+			{
+				if ( nExp != 0 )
+					break;
+				bNegExp = ! bNegExp;
+			}
+			else
+			{
+				if ( fRet != 0.0 )
+					break;
+				bNeg = ! bNeg;
+			}
+		}
+		else if ( *pStr == '+' )
+		{
+			if ( bExp && nExp != 0 )
+				break;
+			if ( fRet != 0.0 )
+				break;
+		}
+		else if ( *pStr == 'e' || *pStr == 'E' )
+		{
+			if ( bExp )
+				break;
+			bExp = TRUE;
+		}
+		else
+			break;
+	}
+	if ( bExp )
+	{
+		nExp = bNegExp ? -nExp : nExp;
+		fRet *= calcPow10( nExp );
+	}
+	return bNeg ? -fRet : fRet;
+}
+
+// -----------------------------------------------------------------------
 
 #ifdef MACOSX
 static void ImplDrawEPS( EPSData *pData )
@@ -115,8 +378,109 @@ static void ImplDrawEPS( EPSData *pData )
 				{
 					if ( PMSessionPostScriptBegin( pData->maSession ) == kPMNoError )
 					{
-						PMSessionPostScriptData( pData->maSession, pData->mpPtr, pData->mnSize );
+						// First find the bounding box iin the EPS data
+						SvMemoryStream aStream( pData->mpPtr, pData->mnSize, STREAM_READ );
+						aStream.Seek( STREAM_SEEK_TO_BEGIN );
+						ByteString aLine;
+						ByteString aDocTitle;
+						double fLeft = 0;
+						double fRight = 0;
+						double fTop = 0;
+						double fBottom = 0;
+						bool bEndComments = false;
+						while ( ! aStream.IsEof() && ( ( !fLeft && !fRight && !fTop && !fBottom ) || ( !aDocTitle.Len() && !bEndComments ) ) )
+						{
+							aStream.ReadLine( aLine );
+							if ( aLine.Len() > 1 && aLine.GetChar( 0 ) == '%' )
+							{
+								char cChar = aLine.GetChar( 1 );
+								if ( cChar == '%' )
+								{
+									if ( aLine.CompareIgnoreCaseToAscii( "%%BoundingBox:", 14 ) == COMPARE_EQUAL )
+									{
+										aLine = WhitespaceToSpace( aLine.GetToken( 1, ':' ) );
+										if ( aLine.Len() && aLine.Search( "atend" ) == STRING_NOTFOUND )
+										{
+											fLeft = StringToDouble( GetCommandLineToken( 0, aLine ) );
+											fBottom = StringToDouble( GetCommandLineToken( 1, aLine ) );
+											fRight = StringToDouble( GetCommandLineToken( 2, aLine ) );
+											fTop = StringToDouble( GetCommandLineToken( 3, aLine ) );
+										}
+									}
+									else if ( aLine.CompareIgnoreCaseToAscii( "%%Title:", 8 ) == COMPARE_EQUAL )
+									{
+										aDocTitle = WhitespaceToSpace( aLine.Copy( 8 ) );
+									}
+									else if ( aLine.CompareIgnoreCaseToAscii( "%%EndComments", 13 ) == COMPARE_EQUAL )
+									{
+										bEndComments = true;
+									}
+								}
+								else if ( cChar == ' ' || cChar == '\t' || cChar == '\r' || cChar == '\n' )
+								{
+									bEndComments = true;
+								}
+							}
+							else
+							{
+								bEndComments = true;
+							}
+						}
+
+						static sal_uInt16 nEps = 0;
+						if ( ! aDocTitle.Len() )
+							aDocTitle = ByteString::CreateFromInt32( (sal_Int32)nEps++ );
+
+						if ( fLeft != fRight && fTop != fBottom )
+						{
+							double fScaleX = pData->mnWidth / ( fRight - fLeft );
+							double fScaleY = pData->mnHeight / ( fTop - fBottom ) * -1;
+
+							// Write EPS header
+							OStringBuffer aBuf( "/b4_Inc_state save def\n"
+								"/dict_count countdictstack def\n"
+								"/op_count count 1 sub def\n"
+								"userdict begin\n"
+								"/showpage {} def\n"
+								"0 setgray 0 setlinecap 1 setlinewidth 0 setlinejoin\n"
+								"10 setmiterlimit [] 0 setdash newpath\n"
+								"/languagelevel where\n"
+								"{\n"
+								"  pop languagelevel\n"
+								"  1 ne\n"
+								"  {\n"
+								"    false setstrokeadjust false setoverprint\n"
+								"  } if\n"
+								"} if\n" );
+							aBuf.append( "\n\n [" );
+							aBuf.append( fScaleX );
+							aBuf.append( (long)0 );
+							aBuf.append( (long)0 );
+							aBuf.append( fScaleY );
+							aBuf.append( pData->mnX - ( fScaleX * fLeft ) );
+							aBuf.append( pData->mnY - ( fScaleX * fBottom ) );
+							aBuf.append( "] concat\n" );
+							aBuf.append( "%%BeginDocument: " );
+							aBuf.append( aDocTitle );
+							aBuf.append( "\n" );
+							PMSessionPostScriptData( pData->maSession, (MacOSPtr)aBuf.getStr(), aBuf.getLength() );
+
+							// Write EPS data
+							PMSessionPostScriptData( pData->maSession, pData->mpPtr, pData->mnSize );
+							if ( ((char*)pData->mpPtr)[ pData->mnSize - 1 ] != '\n' )
+								PMSessionPostScriptData( pData->maSession, (MacOSPtr)"\n", 1 );
+
+							// Write EPS footer
+							aBuf = OStringBuffer( "%%EndDocument\n"
+								"count op_count sub {pop} repeat\n"
+								"countdictstack dict_count sub {end} repeat\n"
+								"b4_Inc_state restore\n" );
+							PMSessionPostScriptData( pData->maSession, (MacOSPtr)aBuf.getStr(), aBuf.getLength() );
+						}
+
 						PMSessionPostScriptEnd( pData->maSession );
+
+						pData->mbSuccess = TRUE;
 					}
 
 					PMSessionSetDocumentFormatGeneration( pData->maSession, kPMDocumentFormatPDF, aContextArray, NULL );
@@ -430,8 +794,13 @@ BOOL SalGraphics::DrawEPS( long nX, long nY, long nWidth, long nHeight,
 	{
 		EPSData aData;
 		aData.maSession = pSession;
+		aData.mnX = nX;
+		aData.mnY = nY;
+		aData.mnWidth = nWidth;
+		aData.mnHeight = nHeight;
 		aData.mpPtr = (MacOSPtr)pPtr;
 		aData.mnSize = nSize;
+		aData.mbSuccess = FALSE;
 
 		VCLThreadAttach t;
 		if ( t.pEnv->GetVersion() < JNI_VERSION_1_4 && !pEventLoopTimerUPP )
@@ -448,7 +817,7 @@ BOOL SalGraphics::DrawEPS( long nX, long nY, long nWidth, long nHeight,
 			ImplDrawEPS( &aData );
 		}
 
-		bRet = TRUE;
+		bRet = aData.mbSuccess;
 	}
 #else	// MACOSX
 #ifdef DEBUG
@@ -499,4 +868,3 @@ SalGraphicsData::~SalGraphicsData()
 	for ( ::std::map< int, com_sun_star_vcl_VCLFont* >::const_iterator it = maFallbackFonts.begin(); it != maFallbackFonts.end(); ++it )
 		delete it->second;
 }
-
