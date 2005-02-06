b070406c2a4fe4e58a77bf88980b01afc36d3867
pluby <>
2005-02-06 17:48:37 +0000

    Correct #ifdef

--- vcl/source/gdi/sallayout.cxx
+++ vcl/source/gdi/sallayout.cxx
@@ -100,10 +100,10 @@ int GetVerticalFlags( sal_Unicode nChar )
         || (nChar >= 0xFF5B && nChar <= 0xFF9F) // halfwidth forms
         ||  nChar == 0xFFE3 )
             return GF_NONE; // not rotated
-#ifndef MACOSX
+#if !defined USE_JAVA || !defined MACOSX
         else if( nChar == 0x30fc )
             return GF_ROTR; // right
-#endif	// MACOSX
+#endif	// !USE_JAVA || !MACOSX
         return GF_ROTL;     // left
     }
 
