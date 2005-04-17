230408460aa8fb1e78eb9f3269c7da8dfb6b8353
pluby <>
2005-04-17 21:48:29 +0000

    Merge Mac OS X code with code from other platforms

--- desktop/scripts/mozwrapper.sh
+++ desktop/scripts/mozwrapper.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-while [ "$1" != "" ] ; do
-	URL=`echo "$1" | sed 's#^/#file:///#'`
-	if [ "$1" != "" ] ; then
-		/usr/bin/open "$URL"
+if [ `uname` = "Darwin" ] ; then
+	while [ "$1" != "" ] ; do
+		URL=`echo "$1" | sed 's#^/#file:///#'`
+		if [ "$1" != "" ] ; then
+			/usr/bin/open "$URL"
+		fi
+		shift
+	done
+else
+	# if mozilla is not found, specify full path here
+	MOZILLA=mozilla
+
+	if ${MOZILLA} -remote "openURL($1,new-window)" 2>&1 | egrep -si "not running on display"; then
+		${MOZILLA} $1
 	fi
-	shift
 fi
 
 exit 0
--- desktop/scripts/nswrapper.sh
+++ desktop/scripts/nswrapper.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-while [ "$1" != "" ] ; do
-	URL=`echo "$1" | sed 's#^/#file:///#'`
-	if [ "$1" != "" ] ; then
-		/usr/bin/open "$URL"
+if [ `uname` = "Darwin" ] ; then
+	while [ "$1" != "" ] ; do
+		URL=`echo "$1" | sed 's#^/#file:///#'`
+		if [ "$1" != "" ] ; then
+			/usr/bin/open "$URL"
+		fi
+		shift
+	done
+else
+	# if netscape is not found, specify full path here
+	NETSCAPE=netscape
+
+	if ${NETSCAPE} -remote "openURL($1,new-window)" 2>&1 | egrep -si "not running on display"; then
+		${NETSCAPE} $1
 	fi
-	shift
 fi
 
 exit 0
