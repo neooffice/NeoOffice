3442c5c5583c82ea0606ffb2e953c1049681fd90
pluby <>
2005-08-22 08:41:04 +0000

    Eliminate methods that are not defined in the JVM's ApplicationDelegate or AWTApplicationDelegate classes

--- sfx2/source/appl/shutdownicon_cocoa.m
+++ sfx2/source/appl/shutdownicon_cocoa.m
@@ -38,40 +38,21 @@
 
 /*
  * Create a class that is a facade for the application delegate set by the JVM.
- * Note that this class does not implement any of the openFiles: and
- * printFiles: methods as the JVM is apparently does not have these by design
- * so that the openFile: and printFile: methods are called instead.
+ * Note that this class only implement the delegate methods in the JVM's
+ * ApplicationDelegate and AWTApplicationDelegate classes.
  */
 @interface ShutdownIconDelegate : NSObject
 {
 	NSMenu*				mpDockMenu;
 	id					mpDelegate;
 }
-- (BOOL)application:(NSApplication *)pApplication delegateHandlesKey:(NSString *)pKey;
 - (BOOL)application:(NSApplication *)pApplication openFile:(NSString *)pFilename;
-- (BOOL)application:(NSApplication *)pApplication openFileWithoutUI:(NSString *)pFilename;
-- (BOOL)application:(NSApplication *)pApplication openTempFile:(NSString *)pFilename;
 - (BOOL)application:(NSApplication *)pApplication printFile:(NSString *)pFilename;
-- (void)applicationDidBecomeActive:(NSNotification *)pNotification;
 - (void)applicationDidChangeScreenParameters:(NSNotification *)pNotification;
-- (void)applicationDidFinishLaunching:(NSNotification *)pNotification;
-- (void)applicationDidHide:(NSNotification *)pNotification;
-- (void)applicationDidResignActive:(NSNotification *)pNotification;
-- (void)applicationDidUnhide:(NSNotification *)pNotification;
-- (void)applicationDidUpdate:(NSNotification *)pNotification;
 - (NSMenu *)applicationDockMenu:(NSApplication *)pApplication;
-- (BOOL)applicationOpenUntitledFile:(NSApplication *)pApplication;
 - (BOOL)applicationShouldHandleReopen:(NSApplication *)pApplication hasVisibleWindows:(BOOL)bFlag;
-- (BOOL)applicationShouldOpenUntitledFile:(NSApplication *)pApplication;
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)pApplication;
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)pApplication;
-- (void)applicationWillBecomeActive:(NSNotification *)pNotification;
 - (void)applicationWillFinishLaunching:(NSNotification *)pNotification;
-- (void)applicationWillHide:(NSNotification *)pNotification;
-- (void)applicationWillResignActive:(NSNotification *)pNotification;
-- (void)applicationWillTerminate:(NSNotification *)pNotification;
-- (void)applicationWillUnhide:(NSNotification *)pNotification;
-- (void)applicationWillUpdate:(NSNotification *)pNotification;
 - (void)dealloc;
 - (id)init;
 - (void)handleCalcCommand:(id)pObject;
@@ -86,34 +67,10 @@
 
 @implementation ShutdownIconDelegate
 
-- (BOOL)application:(NSApplication *)pApplication delegateHandlesKey:(NSString *)pKey;
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(application:delegateHandlesKey:)] )
-		return [mpDelegate application:pApplication delegateHandlesKey:pKey];
-	else
-		return NO;
-}
-
 - (BOOL)application:(NSApplication *)pApplication openFile:(NSString *)pFilename
 {
 	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(application:openFile:)] )
 		return [mpDelegate application:pApplication openFile:pFilename];
-	else
-		return NO;
-}
-
-- (BOOL)application:(NSApplication *)pApplication openFileWithoutUI:(NSString *)pFilename
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(application:openFileWithoutUI:)] )
-		return [mpDelegate application:pApplication openFileWithoutUI:pFilename];
-	else
-		return NO;
-}
-
-- (BOOL)application:(NSApplication *)pApplication openTempFile:(NSString *)pFilename
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(application:openTempFile:)] )
-		return [mpDelegate application:pApplication openTempFile:pFilename];
 	else
 		return NO;
 }
@@ -126,11 +83,10 @@
 		return NO;
 }
 
-- (void)applicationDidBecomeActive:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationDidBecomeActive:)] )
-		[mpDelegate applicationDidBecomeActive:pNotification];
-}
+- (NSMenu *)applicationDockMenu:(NSApplication *)pApplication
+{   
+    return mpDockMenu;
+} 
 
 - (void)applicationDidChangeScreenParameters:(NSNotification *)pNotification
 {
@@ -138,61 +94,10 @@
 		[mpDelegate applicationDidChangeScreenParameters:pNotification];
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationDidFinishLaunching:)] )
-		[mpDelegate applicationDidFinishLaunching:pNotification];
-}
-
-- (void)applicationDidHide:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationDidHide:)] )
-		[mpDelegate applicationDidHide:pNotification];
-}
-
-- (void)applicationDidResignActive:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationDidResignActive:)] )
-		[mpDelegate applicationDidResignActive:pNotification];
-}
-
-- (void)applicationDidUnhide:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationDidUnhide:)] )
-		[mpDelegate applicationDidUnhide:pNotification];
-}
-
-- (void)applicationDidUpdate:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationDidUpdate:)] )
-		[mpDelegate applicationDidUpdate:pNotification];
-}
-
-- (NSMenu *)applicationDockMenu:(NSApplication *)pApplication
-{   
-    return mpDockMenu;
-} 
-
-- (BOOL)applicationOpenUntitledFile:(NSApplication *)pApplication
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationOpenUntitledFile:)] )
-		return [mpDelegate applicationOpenUntitledFile:pApplication];
-	else
-		return NO;
-}
-
 - (BOOL)applicationShouldHandleReopen:(NSApplication *)pApplication hasVisibleWindows:(BOOL)bFlag
 {
 	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationShouldHandleReopen:hasVisibleWindows:)] )
 		return [mpDelegate applicationShouldHandleReopen:pApplication hasVisibleWindows:bFlag];
-	else
-		return NO;
-}
-
-- (BOOL)applicationShouldOpenUntitledFile:(NSApplication *)pApplication
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationShouldOpenUntitledFile:)] )
-		return [mpDelegate applicationShouldOpenUntitledFile:pApplication];
 	else
 		return NO;
 }
@@ -205,54 +110,10 @@
 		return NSTerminateCancel;
 }
 
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)pApplication
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationShouldTerminateAfterLastWindowClosed:)] )
-		return [mpDelegate applicationShouldTerminateAfterLastWindowClosed:pApplication];
-	else
-		return NO;
-}
-
-- (void)applicationWillBecomeActive:(NSNotification *)pNotification;
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationWillBecomeActive:)] )
-		[mpDelegate applicationWillBecomeActive:pNotification];
-}
-
 - (void)applicationWillFinishLaunching:(NSNotification *)pNotification
 {
 	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationWillFinishLaunching:)] )
 		[mpDelegate applicationWillFinishLaunching:pNotification];
-}
-
-- (void)applicationWillHide:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationWillHide:)] )
-		[mpDelegate applicationWillHide:pNotification];
-}
-
-- (void)applicationWillResignActive:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationWillResignActive:)] )
-		[mpDelegate applicationWillResignActive:pNotification];
-}
-
-- (void)applicationWillTerminate:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationWillTerminate:)] )
-		[mpDelegate applicationWillTerminate:pNotification];
-}
-
-- (void)applicationWillUnhide:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationWillUnhide:)] )
-		[mpDelegate applicationWillUnhide:pNotification];
-}
-
-- (void)applicationWillUpdate:(NSNotification *)pNotification
-{
-	if ( mpDelegate && [mpDelegate respondsToSelector:@selector(applicationWillUpdate:)] )
-		[mpDelegate applicationWillUpdate:pNotification];
 }
 
 - (void)dealloc
