3814256c991c4dbd6a707f632921ac81908d56a2
pluby <>
2005-10-15 21:43:34 +0000

    Disable setting of the scaling factor in the page layout dialog since Java does not support it

--- vcl/java/source/java/VCLPageFormat_cocoa.m
+++ vcl/java/source/java/VCLPageFormat_cocoa.m
@@ -112,6 +112,17 @@
 	{
 		if ( [mpInfo orientation] != mnOrientation )
 			[mpInfo setOrientation:mnOrientation ];
+
+		// Set the scaling factor to 100% until we can accurately implement
+		// scaling in the Java printing code
+		NSMutableDictionary *pDict = [mpInfo dictionary];
+		if ( pDict )
+		{
+			NSNumber *pNum = [NSNumber numberWithFloat:1.0];
+			if ( pNum )
+				[pDict setObject:pNum forKey:NSPrintScalingFactor];
+        }
+
 		mbFinished = NO;
 		[pLayout beginSheetWithPrintInfo:mpInfo modalForWindow:mpWindow delegate:self didEndSelector:@selector(pageLayoutDidEnd:returnCode:contextInfo:) contextInfo:nil];
 	}
