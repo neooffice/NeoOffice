d00416c86401676df36c3cc8107989f1e7b3fa18
pluby <>
2005-11-11 01:06:36 +0000

    Merge changes from the NeoOfficeJ-1_1_Java-1_4_experimental_branch branch

--- vcl/java/source/window/salframe_cocoa.m
+++ vcl/java/source/window/salframe_cocoa.m
@@ -115,7 +115,7 @@
 					aBounds = [pScreen visibleFrame];
 
 				// Flip the coordinate system to match the VCL coordinate system
-				aBounds.origin.y = aVirtualBounds.size.height - aBounds.origin.y - aBounds.size.height;
+				aBounds.origin.y = aVirtualBounds.origin.y + aVirtualBounds.size.height - aBounds.origin.y - aBounds.size.height;
 
 				// Test the closeness of the point to the center of the screen
 				unsigned nArea = abs( (unsigned)( ( ( ( aBounds.origin.x + aBounds.size.width ) / 2 ) - maPoint.x ) * ( ( ( aBounds.origin.y + aBounds.size.height ) / 2 ) - maPoint.y ) ) );
