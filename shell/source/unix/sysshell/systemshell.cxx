da8eb3ade634c6cc2e1e90dd6e425c86a0c06e1f
pluby <>
2005-12-14 21:31:22 +0000

    Update shell module to OOo 2.0 code

--- shell/source/unix/sysshell/systemshell.cxx
+++ shell/source/unix/sysshell/systemshell.cxx
@@ -6,37 +6,31 @@
  *
  *  last change: $Author$ $Date$
  *
- *  The Contents of this file are made available subject to the terms of
- *  either of the following licenses
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU General Public License Version 2.1.
  *
- *         - GNU General Public License Version 2.1
  *
- *  Sun Microsystems Inc., October, 2000
+ *    GNU General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
  *
- *  GNU General Public License Version 2.1
- *  =============================================
- *  Copyright 2000 by Sun Microsystems, Inc.
- *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU General Public
+ *    License version 2.1, as published by the Free Software Foundation.
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public
- *  License version 2.1, as published by the Free Software Foundation.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    General Public License for more details.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
+ *    You should have received a copy of the GNU General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
  *
- *  You should have received a copy of the GNU General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
- *  
- *  =================================================
- *  Modified April 2005 by Patrick Luby. SISSL Removed. NeoOffice is
- *  distributed under GPL only under modification term 3 of the LGPL.
- *
- *  Contributor(s): _______________________________________
+ *    Modified December 2005 by Patrick Luby. NeoOffice is distributed under
+ *    GPL only under modification term 3 of the LGPL.
  *
  ************************************************************************/
 
@@ -47,11 +41,7 @@
 #include "osl/module.hxx"
 
 const rtl::OUString SYM_ADD_TO_RECENTLY_USED_FILE_LIST = rtl::OUString::createFromAscii("add_to_recently_used_file_list");
-#ifdef MACOSX
-const rtl::OUString LIB_RECENT_FILE = rtl::OUString::createFromAscii("librecentfile.dylib");
-#else	// MACOSX
 const rtl::OUString LIB_RECENT_FILE = rtl::OUString::createFromAscii("librecentfile.so");
-#endif	// MACOSX
 
 namespace SystemShell {
     
@@ -61,7 +51,7 @@ namespace SystemShell {
     rtl::OUString get_absolute_library_url(const rtl::OUString& lib_name)
     {
         rtl::OUString url;
-        if (osl::Module::getUrlFromAddress(reinterpret_cast<void*>(AddToRecentDocumentList), url))
+        if (osl::Module::getUrlFromAddress(reinterpret_cast<oslGenericFunction>(AddToRecentDocumentList), url))
         {        
             sal_Int32 index = url.lastIndexOf('/');
             url = url.copy(0, index + 1);
@@ -82,8 +72,10 @@ namespace SystemShell {
             
             if (module.is())
             {
+               // convert from reinterpret_cast<PFUNC_ADD_TO_RECENTLY_USED_LIST>
+			   // not allowed in gcc 3.3 without permissive.
                 PFUNC_ADD_TO_RECENTLY_USED_LIST add_to_recently_used_file_list = 
-                    reinterpret_cast<PFUNC_ADD_TO_RECENTLY_USED_LIST>(module.getSymbol(SYM_ADD_TO_RECENTLY_USED_FILE_LIST));
+                    reinterpret_cast<PFUNC_ADD_TO_RECENTLY_USED_LIST>(module.getFunctionSymbol(SYM_ADD_TO_RECENTLY_USED_FILE_LIST));
                 
                 if (add_to_recently_used_file_list)
                     add_to_recently_used_file_list(aFileUrl, aMimeType);
