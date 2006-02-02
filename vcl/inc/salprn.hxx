dd7062653fba3d5c241dc4730f1e3dcd455d94f8
pluby <>
2006-02-02 21:08:41 +0000

    Update code to OOo 2.0.1 code

--- vcl/inc/salprn.hxx
+++ vcl/inc/salprn.hxx
@@ -6,37 +6,31 @@
  *
  *  last change: $Author$ $Date$
  *
- *  The Contents of this file are made available subject to the terms of
- *  either of the following licenses
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU General Public License Version 2.1.
  *
- *         - GNU General Public License Version 2.1
  *
- *  Sun Microsystems Inc., October, 2000
+ *    GNU General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
  *
- *  GNU General Public License Version 2.1
- *  =============================================
- *  Copyright 2000 by Sun Microsystems, Inc.
- *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU General Public
+ *    License version 2.1, as published by the Free Software Foundation.
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public
- *  License version 2.1, as published by the Free Software Foundation.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    General Public License for more details.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
+ *    You should have received a copy of the GNU General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
  *
- *  You should have received a copy of the GNU General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
- *  
- *  =================================================
- *  Modified June 2004 by Patrick Luby. SISSL Removed. NeoOffice is
- *  distributed under GPL only under modification term 3 of the LGPL.
- *
- *  Contributor(s): _______________________________________
+ *    Modified February 2006 by Patrick Luby. NeoOffice is distributed under
+ *    GPL only under modification term 3 of the LGPL.
  *
  ************************************************************************/
 
@@ -50,9 +44,10 @@
 #ifndef _SV_SV_H
 #include <sv.h>
 #endif
-#ifndef _SV_SALPRN_H
-#include <salprn.h>
+#ifndef _VCL_DLLAPI_H
+#include "dllapi.h"
 #endif
+
 #ifndef _SV_PRNTYPES_HXX
 #include <prntypes.hxx>
 #endif
@@ -67,7 +62,7 @@ struct ImplJobSetup;
 // - SalPrinterQueueInfo -
 // -----------------------
 
-struct SalPrinterQueueInfo
+struct VCL_DLLPUBLIC SalPrinterQueueInfo
 {
 	XubString				maPrinterName;
 	XubString				maDriver;
@@ -86,51 +81,42 @@ struct SalPrinterQueueInfo
 // - SalInfoPrinter -
 // ------------------
 
-class SalInfoPrinter
+class VCL_DLLPUBLIC SalInfoPrinter
 {
-	friend class SalInstance;
-
 public:
     std::vector< vcl::PaperInfo  >		m_aPaperFormats;	// all printer supported formats
     bool								m_bPapersInit;		// set to true after InitPaperFormats
+    
+    SalInfoPrinter() {}
+    virtual ~SalInfoPrinter();
 
-							SalInfoPrinter();
-							~SalInfoPrinter();
-
-public: 					// public for Sal Implementation
-	SalInfoPrinterData		maPrinterData;
-
-#ifdef _INCL_SAL_SALIPRN_IMP
-#include <saliprn.imp>
-#endif
-
-public:
 	// SalGraphics or NULL, but two Graphics for all SalFrames
 	// must be returned
-	SalGraphics*			GetGraphics();
-	void					ReleaseGraphics( SalGraphics* pGraphics );
+	virtual SalGraphics*			GetGraphics() = 0;
+	virtual void					ReleaseGraphics( SalGraphics* pGraphics ) = 0;
 
-	BOOL					Setup( SalFrame* pFrame, ImplJobSetup* pSetupData );
-							// This function set the driver data and
-							// set the new indepen data in pSetupData
-	BOOL					SetPrinterData( ImplJobSetup* pSetupData );
-							// This function merged the indepen driver data
-							// and set the new indepen data in pSetupData
-							// Only the data must changed, where the bit
-							// in nFlags is set
-	BOOL					SetData( ULONG nFlags, ImplJobSetup* pSetupData );
+	virtual BOOL					Setup( SalFrame* pFrame, ImplJobSetup* pSetupData ) = 0;
+    // This function set the driver data and
+    // set the new indepen data in pSetupData
+	virtual BOOL					SetPrinterData( ImplJobSetup* pSetupData ) = 0;
+    // This function merged the indepen driver data
+    // and set the new indepen data in pSetupData
+    // Only the data must changed, where the bit
+    // in nFlags is set
+	virtual BOOL					SetData( ULONG nFlags, ImplJobSetup* pSetupData ) = 0;
 
-	void					GetPageInfo( const ImplJobSetup* pSetupData,
-										 long& rOutWidth, long& rOutHeight,
-										 long& rPageOffX, long& rPageOffY,
-										 long& rPageWidth, long& rPageHeight );
-	ULONG					GetCapabilities( const ImplJobSetup* pSetupData, USHORT nType );
-	ULONG					GetPaperBinCount( const ImplJobSetup* pSetupData );
-	XubString				GetPaperBinName( const ImplJobSetup* pSetupData, ULONG nPaperBin );
+	virtual void					GetPageInfo( const ImplJobSetup* pSetupData,
+                                                 long& rOutWidth, long& rOutHeight,
+                                                 long& rPageOffX, long& rPageOffY,
+                                                 long& rPageWidth, long& rPageHeight ) = 0;
+	virtual ULONG					GetCapabilities( const ImplJobSetup* pSetupData, USHORT nType ) = 0;
+	virtual ULONG					GetPaperBinCount( const ImplJobSetup* pSetupData ) = 0;
+	virtual String					GetPaperBinName( const ImplJobSetup* pSetupData, ULONG nPaperBin ) = 0;
 	// fills m_aPaperFormats and sets m_bPapersInit to true
-    void					InitPaperFormats( const ImplJobSetup* pSetupData );
+    virtual void					InitPaperFormats( const ImplJobSetup* pSetupData ) = 0;
     // returns angle that a landscape page will be turned counterclockwise wrt to portrait
-    int						GetLandscapeAngle( const ImplJobSetup* pSetupData );
+    virtual int					GetLandscapeAngle( const ImplJobSetup* pSetupData ) = 0;
+    virtual DuplexMode          GetDuplexMode( const ImplJobSetup* pSetupData ) = 0;
 };
 
 
@@ -138,35 +124,25 @@ public:
 // - SalPrinter -
 // --------------
 
-class SalPrinter
+class VCL_DLLPUBLIC SalPrinter
 {
-	friend class SalInstance;
-
-private:
-							SalPrinter();
-							~SalPrinter();
-
 public: 					// public for Sal Implementation
-	SalPrinterData			maPrinterData;
+    SalPrinter() {}
+    virtual ~SalPrinter();
 
-#ifdef _INCL_SAL_SALPRN_IMP
-#include <salprn.imp>
-#endif
-
-public:
-	BOOL					StartJob( const XubString* pFileName,
-									  const XubString& rJobName,
-									  const XubString& rAppName,
-									  ULONG nCopies, BOOL bCollate,
-									  ImplJobSetup* pSetupData );
-	BOOL					EndJob();
-	BOOL					AbortJob();
-	SalGraphics*			StartPage( ImplJobSetup* pSetupData, BOOL bNewJobData );
-	BOOL					EndPage();
-	ULONG					GetErrorCode();
+	virtual BOOL					StartJob( const XubString* pFileName,
+                                              const XubString& rJobName,
+                                              const XubString& rAppName,
+                                              ULONG nCopies, BOOL bCollate,
+                                              ImplJobSetup* pSetupData ) = 0;
+	virtual BOOL					EndJob() = 0;
+	virtual BOOL					AbortJob() = 0;
+	virtual SalGraphics*			StartPage( ImplJobSetup* pSetupData, BOOL bNewJobData ) = 0;
+	virtual BOOL					EndPage() = 0;
+	virtual ULONG					GetErrorCode() = 0;
 #ifdef USE_JAVA
-	XubString				GetPageRange();
-#endif	// USE_JAVA
+	virtual XubString				GetPageRange() = 0;
+#endif// USE_JAVA
 };
 
 #endif // _SV_SALPRN_HXX
