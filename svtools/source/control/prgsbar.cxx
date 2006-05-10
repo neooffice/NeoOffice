ba52d9bab76e8ba23bb3c8ce2e949bfa6eb560fd
pluby <>
2006-05-10 07:34:22 +0000

    Correct author

--- svtools/source/control/prgsbar.cxx
+++ svtools/source/control/prgsbar.cxx
@@ -29,7 +29,7 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  *    MA  02111-1307  USA
  *
- *    Modified May 2006 by Patrick Luby. NeoOffice is distributed under
+ *    Modified May 2006 by Edward Peterlin. NeoOffice is distributed under
  *    GPL only under modification term 3 of the LGPL.
  *
  ************************************************************************/
