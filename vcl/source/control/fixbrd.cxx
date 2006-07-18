638f638083b1214417ced086c4732958eae752b1
peterlin <>
2006-07-18 06:27:17 +0000

    Fix up VCL implementation to use NWF primary group boxes (Tools > Options). Note that the implementation of group boxes in dialogs appears to use tab pages as the peers of their containing group boxes that provide the border.  New code in tabpage.cxx checks to see whether either the parent window or one of its peers is a fixed border window and, if so, toggles transparent drawing as required to allow the fixed border window's NWF background to be made visible
    Edward Peterlin
    OPENSTEP@neooffice.org

---
+++ vcl/source/control/fixbrd.cxx
@@ -0,0 +1,255 @@
+/*************************************************************************
+ *
+ *  $RCSfile$
+ *
+ *  $Revision$
+ *
+ *  last change: $Author$ $Date$
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU General Public License Version 2.1.
+ *
+ *
+ *    GNU General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ *    Modified July 2006 by Edward Peterlin. NeoOffice is distributed under
+ *    GPL only under modification term 3 of the LGPL.
+ *
+ ************************************************************************/
+
+#ifndef _SV_RC_H
+#include <tools/rc.h>
+#endif
+#ifndef _SV_EVENT_HXX
+#include <event.hxx>
+#endif
+#ifndef _SV_FIXBRD_HXX
+#include <fixbrd.hxx>
+#endif
+
+
+
+// =======================================================================
+
+void FixedBorder::ImplInit( Window* pParent, WinBits nStyle )
+{
+	mnType			= FIXEDBORDER_TYPE_DOUBLEOUT;
+	mbTransparent	= TRUE;
+
+	nStyle = ImplInitStyle( nStyle );
+	Control::ImplInit( pParent, nStyle, NULL );
+	ImplInitSettings();
+
+#ifdef USE_JAVA
+	if( IsNativeControlSupported( CTRL_FIXEDBORDER, PART_ENTIRE_CONTROL ) )
+	{
+		SetBackground();
+		SetControlBackground();
+        EnableChildTransparentMode( TRUE );
+    }
+#endif
+}
+
+// -----------------------------------------------------------------------
+
+WinBits FixedBorder::ImplInitStyle( WinBits nStyle )
+{
+	if ( !(nStyle & WB_NOGROUP) )
+		nStyle |= WB_GROUP;
+	return nStyle;
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::ImplInitSettings()
+{
+	Window* pParent = GetParent();
+	if ( (pParent->IsChildTransparentModeEnabled() ||
+		  !(pParent->GetStyle() & WB_CLIPCHILDREN) ) &&
+		 !IsControlBackground() && mbTransparent )
+	{
+		SetMouseTransparent( TRUE );
+		EnableChildTransparentMode( TRUE );
+		SetParentClipMode( PARENTCLIPMODE_NOCLIP );
+		SetPaintTransparent( TRUE );
+		SetBackground();
+	}
+	else
+	{
+		SetMouseTransparent( FALSE );
+		EnableChildTransparentMode( FALSE );
+		SetParentClipMode( 0 );
+		SetPaintTransparent( FALSE );
+
+		if ( IsControlBackground() )
+			SetBackground( GetControlBackground() );
+		else
+			SetBackground( pParent->GetBackground() );
+	}
+}
+
+// -----------------------------------------------------------------------
+
+FixedBorder::FixedBorder( Window* pParent, WinBits nStyle ) :
+	Control( WINDOW_FIXEDBORDER )
+{
+	ImplInit( pParent, nStyle );
+}
+
+// -----------------------------------------------------------------------
+
+FixedBorder::FixedBorder( Window* pParent, const ResId& rResId ) :
+	Control( WINDOW_FIXEDBORDER )
+{
+	rResId.SetRT( RSC_CONTROL );
+	WinBits nStyle = ImplInitRes( rResId );
+	ImplInit( pParent, nStyle );
+	ImplLoadRes( rResId );
+
+	if ( !(nStyle & WB_HIDE) )
+		Show();
+}
+
+// -----------------------------------------------------------------------
+
+FixedBorder::~FixedBorder()
+{
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::ImplDraw( OutputDevice* pDev, ULONG nDrawFlags,
+							const Point& rPos, const Size& rSize )
+{
+	const StyleSettings&	rStyleSettings = GetSettings().GetStyleSettings();
+	Rectangle				aRect( rPos, rSize );
+	USHORT					nBorderStyle = mnType;
+
+	if ( (nDrawFlags & WINDOW_DRAW_MONO) ||
+		 (rStyleSettings.GetOptions() & STYLE_OPTION_MONO) )
+		nBorderStyle |= FRAME_DRAW_MONO;
+
+#if defined( USE_JAVA )
+    // seems only to be used in tools->options around a tabpage (ie, no tabcontrol!)
+    // as tabpages that are not embedded in a tabcontrol should not be drawn natively
+    // the fixedborder must also not be drawn (reason was, that it looks too ugly, dialogs must be redesigned)
+    Window *pWin = ( ( pDev->GetOutDevType() == OUTDEV_WINDOW ) ? (Window*) pDev : NULL );
+    if( pWin && pWin->IsNativeControlSupported( CTRL_FIXEDBORDER, PART_ENTIRE_CONTROL ) )
+    {
+        ImplControlValue aControlValue;
+        Point aPt;
+        Region aCtrlRegion( Rectangle( aPt, GetOutputSizePixel() ) );
+        ControlState nState = IsEnabled() ? CTRL_STATE_ENABLED : 0;
+        pWin->DrawNativeControl( CTRL_FIXEDBORDER, PART_ENTIRE_CONTROL, aCtrlRegion, nState,
+	     						aControlValue, rtl::OUString() );
+    }
+    else
+#endif
+    {
+	    DecorationView	aDecoView( pDev );
+	    aDecoView.DrawFrame( aRect, nBorderStyle );
+    }
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::Paint( const Rectangle& rRect )
+{
+	ImplDraw( this, 0, Point(), GetOutputSizePixel() );
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::Draw( OutputDevice* pDev, const Point& rPos, const Size& rSize,
+						ULONG nFlags )
+{
+	Point	aPos  = pDev->LogicToPixel( rPos );
+	Size	aSize = pDev->LogicToPixel( rSize );
+
+	pDev->Push();
+	pDev->SetMapMode();
+	ImplDraw( pDev, nFlags, aPos, aSize );
+	pDev->Pop();
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::Resize()
+{
+	Invalidate();
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::StateChanged( StateChangedType nType )
+{
+	Control::StateChanged( nType );
+
+	if ( (nType == STATE_CHANGE_DATA) ||
+		 (nType == STATE_CHANGE_UPDATEMODE) )
+	{
+		if ( IsUpdateMode() )
+			Invalidate();
+	}
+	else if ( nType == STATE_CHANGE_STYLE )
+		SetStyle( ImplInitStyle( GetStyle() ) );
+	else if ( nType == STATE_CHANGE_CONTROLBACKGROUND )
+	{
+		ImplInitSettings();
+		Invalidate();
+	}
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::DataChanged( const DataChangedEvent& rDCEvt )
+{
+	Control::DataChanged( rDCEvt );
+
+	if ( (rDCEvt.GetType() == DATACHANGED_SETTINGS) &&
+		 (rDCEvt.GetFlags() & SETTINGS_STYLE) )
+	{
+		ImplInitSettings();
+		Invalidate();
+	}
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::SetTransparent( BOOL bTransparent )
+{
+	if ( mbTransparent != bTransparent )
+	{
+		mbTransparent = bTransparent;
+		ImplInitSettings();
+		Invalidate();
+	}
+}
+
+// -----------------------------------------------------------------------
+
+void FixedBorder::SetBorderType( USHORT nType )
+{
+	if ( mnType != nType )
+	{
+		mnType = nType;
+		Invalidate();
+	}
+}
---
+++ vcl/source/window/tabpage.cxx
@@ -0,0 +1,228 @@
+/*************************************************************************
+ *
+ *  $RCSfile$
+ *
+ *  $Revision$
+ *
+ *  last change: $Author$ $Date$
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU General Public License Version 2.1.
+ *
+ *
+ *    GNU General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ *    Modified July 2006 by Edward Peterlin. NeoOffice is distributed under
+ *    GPL only under modification term 3 of the LGPL.
+ *
+ ************************************************************************/
+#include <tools/ref.hxx>
+
+#ifndef _SV_RC_H
+#include <tools/rc.h>
+#endif
+#ifndef _SV_SVDATA_HXX
+#include <svdata.hxx>
+#endif
+#ifndef _SV_SVAPP_HXX
+#include <svapp.hxx>
+#endif
+#ifndef _SV_EVENT_HXX
+#include <event.hxx>
+#endif
+#ifndef _SV_TABPAGE_HXX
+#include <tabpage.hxx>
+#endif
+#ifndef _SV_TABCTRL_HXX
+#include <tabctrl.hxx>
+#endif
+
+#ifndef _COM_SUN_STAR_ACCESSIBILITY_XACCESSIBLE_HPP_
+#include <com/sun/star/accessibility/XAccessible.hpp>
+#endif
+
+
+
+
+// =======================================================================
+
+void TabPage::ImplInit( Window* pParent, WinBits nStyle )
+{
+    if ( !(nStyle & WB_NODIALOGCONTROL) )
+        nStyle |= WB_DIALOGCONTROL;
+
+    Window::ImplInit( pParent, nStyle, NULL );
+
+    ImplInitSettings();
+
+    // if the tabpage is drawn (ie filled) by a native widget, make sure all contols will have transparent background
+    // otherwise they will paint with a wrong background
+    if( IsNativeControlSupported(CTRL_TAB_BODY, PART_ENTIRE_CONTROL) && GetParent() && (GetParent()->GetType() == WINDOW_TABCONTROL) )
+        EnableChildTransparentMode( TRUE );
+#ifdef USE_JAVA
+	else if ( IsNativeControlSupported( CTRL_FIXEDBORDER, PART_ENTIRE_CONTROL ) && GetParent() )
+	{
+		if ( GetParent()->GetType() == WINDOW_FIXEDBORDER )
+		{
+			EnableChildTransparentMode( TRUE );
+		}
+		else
+		{
+			// dialogs will implement tabpages as peers of their borders, not
+			// as contained
+			for ( USHORT i = 0; i < GetParent()->GetChildCount(); i++ )
+			{
+				Window* pChild = GetParent()->GetChild( i );
+				if ( pChild->GetType() == WINDOW_FIXEDBORDER )
+				{
+					EnableChildTransparentMode( TRUE );
+					SetBackground();
+					SetControlBackground();
+					break;
+				}
+			}
+		}
+	}
+#endif
+}
+
+// -----------------------------------------------------------------------
+
+void TabPage::ImplInitSettings()
+{
+    Window* pParent = GetParent();
+    if ( pParent->IsChildTransparentModeEnabled() && !IsControlBackground() )
+    {
+        EnableChildTransparentMode( TRUE );
+        SetParentClipMode( PARENTCLIPMODE_NOCLIP );
+        SetPaintTransparent( TRUE );
+        SetBackground();
+    }
+    else
+    {
+        EnableChildTransparentMode( FALSE );
+        SetParentClipMode( 0 );
+        SetPaintTransparent( FALSE );
+
+        if ( IsControlBackground() )
+            SetBackground( GetControlBackground() );
+        else
+            SetBackground( pParent->GetBackground() );
+    }
+}
+
+// -----------------------------------------------------------------------
+
+TabPage::TabPage( Window* pParent, WinBits nStyle ) :
+    Window( WINDOW_TABPAGE )
+{
+    ImplInit( pParent, nStyle );
+}
+
+// -----------------------------------------------------------------------
+
+TabPage::TabPage( Window* pParent, const ResId& rResId ) :
+    Window( WINDOW_TABPAGE )
+{
+    rResId.SetRT( RSC_TABPAGE );
+    WinBits nStyle = ImplInitRes( rResId );
+    ImplInit( pParent, nStyle );
+    ImplLoadRes( rResId );
+
+    if ( !(nStyle & WB_HIDE) )
+        Show();
+}
+
+// -----------------------------------------------------------------------
+
+void TabPage::StateChanged( StateChangedType nType )
+{
+    Window::StateChanged( nType );
+
+    if ( nType == STATE_CHANGE_INITSHOW )
+    {
+        if ( GetSettings().GetStyleSettings().GetAutoMnemonic() )
+            ImplWindowAutoMnemonic( this );
+    }
+    else if ( nType == STATE_CHANGE_CONTROLBACKGROUND )
+    {
+        ImplInitSettings();
+        Invalidate();
+    }
+}
+
+// -----------------------------------------------------------------------
+
+void TabPage::DataChanged( const DataChangedEvent& rDCEvt )
+{
+    Window::DataChanged( rDCEvt );
+
+    if ( (rDCEvt.GetType() == DATACHANGED_SETTINGS) &&
+         (rDCEvt.GetFlags() & SETTINGS_STYLE) )
+    {
+        ImplInitSettings();
+        Invalidate();
+    }
+}
+
+// -----------------------------------------------------------------------
+
+void TabPage::Paint( const Rectangle& rRect )
+{
+    // draw native tabpage only inside tabcontrols, standalone tabpages look ugly (due to bad dialog design)
+    if( IsNativeControlSupported(CTRL_TAB_BODY, PART_ENTIRE_CONTROL) && GetParent() && (GetParent()->GetType() == WINDOW_TABCONTROL) )
+    {
+        const ImplControlValue aControlValue( BUTTONVALUE_DONTKNOW, rtl::OUString(), 0 );
+
+        ControlState nState = CTRL_STATE_ENABLED;
+        int part = PART_ENTIRE_CONTROL;
+        if ( !IsEnabled() )
+            nState &= ~CTRL_STATE_ENABLED;
+        if ( HasFocus() )
+            nState |= CTRL_STATE_FOCUSED;
+        Point aPoint;
+        // pass the whole window region to NWF as the tab body might be a gradient or bitmap
+        // that has to be scaled properly, clipping makes sure that we do not paint too much
+        Region aCtrlRegion( Rectangle( aPoint, GetOutputSizePixel() ) );
+        DrawNativeControl( CTRL_TAB_BODY, part, aCtrlRegion, nState,
+                aControlValue, rtl::OUString() );
+    }
+}
+
+// -----------------------------------------------------------------------
+
+void TabPage::ActivatePage()
+{
+}
+
+// -----------------------------------------------------------------------
+
+void TabPage::DeactivatePage()
+{
+}
+
+// -----------------------------------------------------------------------
+
+::com::sun::star::uno::Reference< ::com::sun::star::accessibility::XAccessible > TabPage::CreateAccessible()
+{
+    // TODO: remove this method (incompatible)
+
+    return Window::CreateAccessible();
+}
