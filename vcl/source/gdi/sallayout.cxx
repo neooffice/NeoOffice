1496d66643d58a6eb69b313cfa1c9118564a113a
pluby <>
2006-07-22 02:18:53 +0000

    Partial fix for bug 1594

--- vcl/source/gdi/sallayout.cxx
+++ vcl/source/gdi/sallayout.cxx
@@ -1031,7 +1031,7 @@ void GenericSalLayout::ApplyDXArray( ImplLayoutArgs& rArgs )
                 {
                     // Fix bug 823 by handling inappropriate placement of
                     // kashidas by upper layers
-                    if ( pG > mpGlyphItems && pG[-1].mnCharPos - pG->mnCharPos == 1 )
+                    if ( pG > mpGlyphItems && pG[-1].IsRTLGlyph() && pG[-1].mnCharPos - pG->mnCharPos == 1 )
                     {
                         UJoiningType nTypeLeft = (UJoiningType)u_getIntPropertyValue( rArgs.mpStr[ pG[-1].mnCharPos ], UCHAR_JOINING_TYPE );
                         UJoiningType nTypeRight = (UJoiningType)u_getIntPropertyValue( rArgs.mpStr[ pG->mnCharPos ], UCHAR_JOINING_TYPE );
