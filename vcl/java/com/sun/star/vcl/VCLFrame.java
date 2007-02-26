6d41ad242f7c741617846f97efa029f4f2f6d9a4
pluby <>
2007-02-26 19:15:26 +0000

    Attempt to fix bug 2189 by detecting duplicate Enter key input

--- vcl/java/com/sun/star/vcl/VCLFrame.java
+++ vcl/java/com/sun/star/vcl/VCLFrame.java
@@ -1518,7 +1518,7 @@ public final class VCLFrame implements ComponentListener, FocusListener, KeyList
 		// These are handled in the key pressed and released events.
 		char keyChar = e.getKeyChar();
 		int modifiers = e.getModifiersEx();
-		if (keyChar == (char)0x08 || keyChar == (char)0x7f)
+		if (keyChar == (char)0x08 || keyChar == (char)0x7f || (e.getKeyLocation() == KeyEvent.KEY_LOCATION_NUMPAD && keyChar == (char)0x0a))
 			return;
 
 		// Fix bug 710 by stripping out the Alt modifier. Note that we do it
