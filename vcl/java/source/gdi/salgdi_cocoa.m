0813ecffa4c92939fc5cdc94637205152d80111c
pluby <>
2007-03-05 05:59:15 +0000

    Fix bug 2218 by handling 24 bit EPS images

--- vcl/java/source/gdi/salgdi_cocoa.m
+++ vcl/java/source/gdi/salgdi_cocoa.m
@@ -70,21 +70,41 @@
 			[pEPSImage lockFocus];
 
 			NSBitmapImageRep *pBitmapImageRep = [[NSBitmapImageRep alloc] initWithFocusedViewRect:NSMakeRect(0, 0, mnDestWidth, mnDestHeight )];
-			if ( pBitmapImageRep && [pBitmapImageRep bitsPerPixel] == 32 )
+			if ( pBitmapImageRep )
 			{
-				int *pBitmapBuffer = (int *)[pBitmapImageRep bitmapData];
-				if ( pBitmapBuffer )
+				int nBitsPerPixel = [pBitmapImageRep bitsPerPixel];
+				if ( nBitsPerPixel == 24 )
 				{
-					// Pixels are in RBGA format on PowerPC and in ABGR format
-					// on Intel
-					long nPixels = mnDestWidth * mnDestHeight;
-					long i = 0;
-					for ( ; i < nPixels; i++ )
+					// Fix bug 2218 by handling 24 bit EPS images
+					char *pBitmapBuffer = (char *)[pBitmapImageRep bitmapData];
+					if ( pBitmapBuffer )
+					{
+						// Pixels are in RBG format
+						long nPixels = mnDestWidth * mnDestHeight;
+						long i = 0;
+						long j = 0;
+						for ( ; i < nPixels; i++, j += 3 )
+							mpDestPtr[ i ] = ( ( (int)pBitmapBuffer[ j ] & 0x000000ff ) << 16 ) | ( ( (int)pBitmapBuffer[ j + 1 ] & 0x000000ff ) << 8 ) | ( (int)pBitmapBuffer[ j + 2 ] & 0x000000ff ) | 0xff000000;
+					}
+
+					bRet = YES;
+				}
+				else if ( nBitsPerPixel == 32 )
+				{
+					int *pBitmapBuffer = (int *)[pBitmapImageRep bitmapData];
+					if ( pBitmapBuffer )
+					{
+						// Pixels are in RBGA format on PowerPC and in ABGR
+						// format on Intel
+						long nPixels = mnDestWidth * mnDestHeight;
+						long i = 0;
+						for ( ; i < nPixels; i++ )
 #ifdef POWERPC
-						mpDestPtr[ i ] = ( ( pBitmapBuffer[ i ] & 0xffffff00 ) >> 8 ) | ( ( pBitmapBuffer[ i ] & 0x000000ff ) << 24 );
+							mpDestPtr[ i ] = ( ( pBitmapBuffer[ i ] & 0xffffff00 ) >> 8 ) | ( ( pBitmapBuffer[ i ] & 0x000000ff ) << 24 );
 #else	// POWERPC
-						mpDestPtr[ i ] = ( pBitmapBuffer[ i ] & 0xff00ff00 ) | ( ( pBitmapBuffer[ i ] & 0x00ff0000 ) >> 16 ) | ( ( pBitmapBuffer[ i ] & 0x000000ff ) << 16 );
+							mpDestPtr[ i ] = ( pBitmapBuffer[ i ] & 0xff00ff00 ) | ( ( pBitmapBuffer[ i ] & 0x00ff0000 ) >> 16 ) | ( ( pBitmapBuffer[ i ] & 0x000000ff ) << 16 );
 #endif	// POWERPC
+					}
 
 					bRet = YES;
 				}	
