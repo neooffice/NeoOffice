7bbacf24b09f5160bb219142516584bf190a23e7
pluby <>
2007-09-22 17:22:54 +0000

    Fix bug 2606 by respecting this method's bHandleAllCurrentEvents paramter

--- vcl/java/source/app/salinst.cxx
+++ vcl/java/source/app/salinst.cxx
@@ -562,19 +562,25 @@ void JavaSalInstance::Yield( bool bWait, bool bHandleAllCurrentEvents )
 
 		com_sun_star_vcl_VCLFrame::flushAllFrames();
 
-		switch ( nID )
+		// Fix bug 2606 by respecting this method's bHandleAllCurrentEvents
+		// paramter
+		bContinue = bHandleAllCurrentEvents;
+		if ( bContinue )
 		{
-			case SALEVENT_CLOSE:
-				// Fix bug 1971 by breaking after closing a window
-				bContinue = false;
-				break;
-			case SALEVENT_KEYINPUT:
-			case SALEVENT_MOUSEBUTTONDOWN:
-				// Fix bugs 437 and 2264 by ensuring that if the next event is a
-				// a matching event, it will be dispatched before the painting
-				// timer runs
-				OThread::yield();
-				break;
+			switch ( nID )
+			{
+				case SALEVENT_CLOSE:
+					// Fix bug 1971 by breaking after closing a window
+					bContinue = false;
+					break;
+				case SALEVENT_KEYINPUT:
+				case SALEVENT_MOUSEBUTTONDOWN:
+					// Fix bugs 437 and 2264 by ensuring that if the next
+					// event is a matching event, it will be dispatched before
+					// the painting timer runs
+					OThread::yield();
+					break;
+			}
 		}
 	}
 
