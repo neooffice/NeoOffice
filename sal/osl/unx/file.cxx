81f3025f1216636d3d06180777516b6d3ba18657
pluby <>
2007-09-26 18:39:42 +0000

    Revert changes in previous commit as they have no effect on bugs 2620 and 2627 and the changes could cause bug 2504 to reoccur

--- sal/osl/unx/file.cxx
+++ sal/osl/unx/file.cxx
@@ -147,7 +147,15 @@ static const sal_Char* MOUNTTAB="/etc/mtab";
 #include <sys/param.h>
 #include <sys/mount.h>
 #define HAVE_STATFS_H
+#ifndef USE_JAVA
+/*
+ * Fix bug 2504 and other file locking bugs by not trying lock files while
+ * opening and performing the lock after opening like on Linux and Solaris.
+ * Some filesystems do not support any locking so open the file without any
+ * locking and detect the ENOTSUP errno when we try to lock the file.
+ */
 #define HAVE_O_EXLOCK
+#endif	/* USE_JAVA */
 
 // add MACOSX Time Value
 
@@ -331,7 +339,7 @@ static sal_Bool       osl_getFloppyMountEntry(const sal_Char* pszPath, oslVolume
 static void           osl_printFloppyHandle(oslVolumeDeviceHandleImpl* hFloppy);
 #endif
 
-#ifdef MACOSX
+#if defined MACOSX && !defined USE_JAVA
 
 /*******************************************************************
  *	adjustLockFlags
@@ -355,21 +363,12 @@ static int adjustLockFlags(const char * path, int flags)
             flags &= ~O_EXLOCK;
             flags |= O_SHLOCK;
         }    
-#ifdef USE_JAVA
-        /*
-         * Fix bug 2504 and not trying lock webdav files
-         */
-        else if( 0 == strncmp("afpfs", s.f_fstypename, 5) )
-        {
-            flags &= ~( O_EXLOCK | O_SHLOCK );
-        }
-#endif	/* USE_JAVA */
     }
 
     return flags;
 }
 
-#endif	/* MACOSX */
+#endif	/* MACOSX && !USE_JAVA */
 
 
 /*******************************************************************
@@ -771,9 +770,9 @@ oslFileError osl_openFile( rtl_uString* ustrFileURL, oslFileHandle* pHandle, sal
             {
                 mode |= S_IWUSR | S_IWGRP | S_IWOTH;
                 flags = OPEN_WRITE_FLAGS;
-#ifdef MACOSX
+#if defined MACOSX && !defined USE_JAVA
                 flags = adjustLockFlags(buffer, flags);
-#endif	/* MACOSX */
+#endif	/* MACOSX && !USE_JAVA */
                 aflock.l_type = F_WRLCK;
             }
 
@@ -781,9 +780,9 @@ oslFileError osl_openFile( rtl_uString* ustrFileURL, oslFileHandle* pHandle, sal
             {
                 mode |= S_IWUSR | S_IWGRP | S_IWOTH;
                 flags = OPEN_CREATE_FLAGS;
-#ifdef MACOSX
+#if defined MACOSX && !defined USE_JAVA
                 flags = adjustLockFlags(buffer, flags);
-#endif	/* MACOSX */
+#endif	/* MACOSX && !USE_JAVA */
             }
 
             /* open the file */
