dfc7c7e171e743c1f287f0bce6b7d749a5d25061
pluby <>
2007-10-02 21:42:49 +0000

    Fix bug 2645 by making the Mozilla/Thunderbird/Firefox address book code work on Mac OS X

---
+++ connectivity/source/drivers/mozab/bootstrap/makefile.mk
@@ -0,0 +1,160 @@
+#*************************************************************************
+#
+#   $RCSfile$
+#
+#   $Revision$
+#
+#   last change: $Author$ $Date$
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU General Public License Version 2.1.
+#
+#
+#     GNU General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#     Modified September 2007 by Patrick Luby. NeoOffice is distributed under
+#     GPL only under modification term 3 of the LGPL.
+#
+#*************************************************************************
+CALL_CDECL=TRUE
+
+#mozilla specific stuff.
+MOZ_LIB=$(SOLARVERSION)$/$(INPATH)$/lib$(UPDMINOREXT)
+MOZ_INC=$(SOLARVERSION)$/$(INPATH)$/inc$(UPDMINOREXT)$/mozilla
+#End of mozilla specific stuff.
+
+PRJ=..$/..$/..$/..
+PRJINC=..$/..$/..
+PRJNAME=connectivity
+TARGET=mozbootstrap
+
+USE_DEFFILE=TRUE
+ENABLE_EXCEPTIONS=TRUE
+
+# --- Settings ----------------------------------
+.IF "$(DBGUTIL_OJ)"!=""
+ENVCFLAGS+=/FR$(SLO)$/
+.ENDIF
+
+
+.INCLUDE : settings.mk
+
+.IF ("$(SYSTEM_MOZILLA)" == "YES" && "$(WITH_MOZILLA)" == "YES") || "$(WITH_MOZILLA)" == "NO"
+dummy:
+	@echo "		Not building the mozillasrc stuff in OpenOffice.org build"
+	@echo "		dependency to Mozilla developer snapshots not feasable at the moment"
+
+.ELSE
+
+.INCLUDE :  $(PRJ)$/version.mk
+
+
+#mozilla specific stuff.
+# --- Files -------------------------------------
+
+SLOFILES = \
+	$(SLO)$/MMozillaBootstrap.obj	\
+	$(SLO)$/MNSFolders.obj	\
+	$(SLO)$/MNSProfileDiscover.obj				\
+	$(SLO)$/MNSInit.obj			            \
+	$(SLO)$/MNSProfileManager.obj	\
+	$(SLO)$/MNSINIParser.obj	\
+	$(SLO)$/MNSRunnable.obj	\
+	$(SLO)$/MNSProfile.obj					\
+	$(SLO)$/MNSProfileDirServiceProvider.obj
+
+
+.IF "$(GUI)"=="UNX"
+.IF "$(COMNAME)"=="sunpro5"
+CFLAGS += -features=tmplife
+#This flag is needed to build mozilla 1.7 code
+.ENDIF		# "$(COMNAME)"=="sunpro5"
+.ENDIF
+
+MOZINC = . -I.. -I..$/mozillasrc -I$(MOZ_INC)  -I$(MOZ_INC)$/nspr -I$(MOZ_INC)$/xpcom \
+	    -I$(MOZ_INC)$/string -I$(MOZ_INC)$/rdf -I$(MOZ_INC)$/msgbase \
+	    -I$(MOZ_INC)$/addrbook -I$(MOZ_INC)$/mork -I$(MOZ_INC)$/locale \
+	    -I$(MOZ_INC)$/pref -I$(MOZ_INC)$/mime -I$(MOZ_INC)$/chrome \
+	    -I$(MOZ_INC)$/necko -I$(MOZ_INC)$/intl -I$(MOZ_INC)$/profile \
+	    -I$(MOZ_INC)$/embed_base -I$(MOZ_INC)$/mozldap -I$(MOZ_INC)$/uconv\
+	    -I$(MOZ_INC)$/xpcom_obsolete -I$(MOZ_INC)$/content
+	    
+.IF "$(GUI)" == "WNT"
+.IF "$(DBG_LEVEL)" == "0"
+INCPRE += $(MOZINC)
+CDEFS +=    -DWINVER=0x400 -DMOZILLA_CLIENT \
+	    -DNS_NET_FILE -DCookieManagement -DSingleSignon -DClientWallet \
+            -DTRACING -DXP_PC -DXP_WIN -DXP_WIN32 -DHW_THREADS \
+            -DDMSVC4 -DNS_MT_SUPPORTED -DNETLIB_THREAD \
+            -DOJI -DWIN32 -D_X86_ -D_WINDOWS \
+	    -DMOZ_XUL -DMOZ_REFLOW_PERF -DMOZ_REFLOW_PERF_DSP \
+	    -DNSPR20 -DOS_HAS_DLL -DNO_JNI_STUBS \
+	    -DNETSCAPE -DMOZILLA_CLIENT -DJS_THREADSAFE -DNECKO -DINCLUDE_XUL	\
+	    -UDEBUG
+CFLAGS +=   -GR- -W3 -Gy -MD -UDEBUG
+.ELSE
+INCPRE += $(MOZINC)
+CDEFS +=    -DWINVER=0x400 -DMOZILLA_CLIENT \
+	    -DNS_NET_FILE -DCookieManagement -DSingleSignon -DClientWallet \
+            -DTRACING -DXP_PC -DXP_WIN -DXP_WIN32 -DHW_THREADS \
+            -DDMSVC4 -DDEVELOPER_DEBUG -DNS_MT_SUPPORTED -DNETLIB_THREAD \
+            -DOJI -DWIN32 -D_X86_ -D_WINDOWS \
+	    -DMOZ_XUL -DMOZ_REFLOW_PERF -DMOZ_REFLOW_PERF_DSP \
+	    -DDEBUG_Administrator -DNSPR20 -DOS_HAS_DLL -DNO_JNI_STUBS \
+	    -DNETSCAPE -DMOZILLA_CLIENT -DJS_THREADSAFE -DNECKO -DINCLUDE_XUL	\
+	    -UDEBUG
+CFLAGS +=   -Zi -GR- -W3 -Gy -MDd -UDEBUG
+.ENDIF
+.ENDIF
+.IF "$(GUI)" == "UNX"
+INCPOST += $(MOZINC)
+CDEFS+=	    -DMOZILLA_CLIENT \
+            -DXP_UNIX
+.IF "$(OS)" == "LINUX"
+CFLAGS +=   -fPIC -g
+CDEFS+=     -DOSTYPE=\"Linux2.2.14-5\" -DOJI
+
+CFLAGSCXX += \
+            -fno-rtti -Wconversion -Wpointer-arith \
+            -Wcast-align -Woverloaded-virtual -Wsynth \
+            -Wno-long-long -pthread
+CDEFS     += -DTRACING
+.ELIF "$(OS)" == "NETBSD" || "$(OS)" == "MACOSX"
+CFLAGS +=   -fPIC
+CFLAGSCXX += \
+            -fno-rtti -Wconversion -Wpointer-arith \
+            -Wcast-align -Woverloaded-virtual -Wsynth \
+            -Wno-long-long
+CDEFS     += -DTRACING
+.ENDIF
+.ENDIF
+
+.IF "$(GUIBASE)" == "java"
+CDEFS     += -DXP_MACOSX
+.ENDIF
+
+SHL1TARGET_NAME=$(TARGET)$(MOZAB_MAJOR)
+
+.ENDIF # MACOSX
+
+# --- Targets ----------------------------------
+
+.INCLUDE : target.mk
+
---
+++ connectivity/source/drivers/mozab/makefile.mk
@@ -0,0 +1,212 @@
+#*************************************************************************
+#
+#   $RCSfile$
+#
+#   $Revision$
+#
+#   last change: $Author$ $Date$
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU General Public License Version 2.1.
+#
+#
+#     GNU General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#     Modified September 2007 by Patrick Luby. NeoOffice is distributed under
+#     GPL only under modification term 3 of the LGPL.
+#
+#*************************************************************************
+PRJ=..$/..$/..
+PRJINC=..$/..
+PRJNAME=connectivity
+TARGET=mozab
+TARGET2=$(TARGET)drv
+
+.IF ( "$(SYSTEM_MOZILLA)" == "YES" && "$(WITH_MOZILLA)" == "YES") || "$(WITH_MOZILLA)" == "NO"
+all: 
+	@echo "		Not building the mozabsrc stuff in OpenOffice.org build"
+	@echo "		dependency to Mozilla developer snapshots not feasable at the moment"
+	@echo "         see http://bugzilla.mozilla.org/show_bug.cgi?id=135137"
+.ENDIF
+
+#mozilla specific stuff.
+MOZ_LIB=$(SOLARVERSION)$/$(INPATH)$/lib$(UPDMINOREXT)
+MOZ_INC=$(SOLARVERSION)$/$(INPATH)$/inc$(UPDMINOREXT)$/mozilla
+#.ENDIF
+
+.IF "$(OS)"=="WNT" 
+.IF "$(USE_SHELL)"=="4nt"
+MOZ_EMBED_LIB := $(shell +-dir /ba:f $(MOZ_LIB)$/embed_base_s.lib 2>NUL )
+MOZ_REG_LIB	  := $(shell +-dir /ba:f $(MOZ_LIB)$/mozreg_s.lib 2>NUL )
+.ELSE	#"$(USE_SHELL)"=="4nt"
+MOZ_EMBED_LIB := $(shell +-test -f $(MOZ_LIB)$/embed_base_s.lib && echo $(MOZ_LIB)$/embed_base_s.lib )
+MOZ_REG_LIB	  := $(shell +-test -f $(MOZ_LIB)$/mozreg_s.lib && echo $(MOZ_LIB)$/mozreg_s.lib )
+.ENDIF
+
+.IF X"$(MOZ_EMBED_LIB)"=="X"
+MOZ_EMBED_LIB := $(MOZ_LIB)$/baseembed_s.lib
+.ENDIF
+.IF X"$(MOZ_REG_LIB)" == "X"
+MOZ_REG_LIB := $(MOZ_LIB)$/mozreg.lib
+.ENDIF
+.ENDIF
+
+.IF "$(COM)"=="MSC" 
+LIB += $(MOZ_LIB)
+MOZ_LIB_XPCOM= $(MOZ_EMBED_LIB) $(MOZ_LIB)$/nspr4.lib $(MOZ_REG_LIB) $(MOZ_LIB)$/xpcom.lib
+.ELSE "$(COM)"=="MSC" 
+MOZ_LIB_XPCOM= -L$(MOZ_LIB) -lembed_base_s -lnspr4 -lmozreg_s -lxpcom
+.ENDIF
+#End of mozilla specific stuff.
+
+# Disable '-z defs' due to broken libxpcom.
+LINKFLAGSDEFS=$(0)
+
+USE_DEFFILE=TRUE
+ENABLE_EXCEPTIONS=TRUE
+
+# --- Settings ----------------------------------
+
+.IF "$(DBGUTIL_OJ)"!=""
+ENVCFLAGS+=/FR$(SLO)$/
+.ENDIF
+
+.INCLUDE : settings.mk
+
+.INCLUDE :  $(PRJ)$/version.mk
+
+# --- Files -------------------------------------
+
+.IF "$(GUIBASE)" == "java"
+CDEFS     += -DXP_MACOSX
+.ENDIF
+
+SLOFILES=\
+		$(SLO)$/MDriver.obj						\
+		$(SLO)$/MServices.obj
+
+			
+# --- MOZAB BASE Library -----------------------------------
+
+SHL1VERSIONMAP= $(TARGET).map
+SHL1TARGET=	$(TARGET)$(MOZAB_MAJOR)
+SHL1OBJS=$(SLOFILES)
+SHL1STDLIBS=\
+	$(CPPULIB)					\
+	$(CPPUHELPERLIB)			\
+	$(VOSLIB)					\
+	$(OSLLIB)					\
+	$(SALLIB)					\
+	$(DBTOOLSLIB)				\
+	$(COMPHELPERLIB)
+
+
+SHL1DEPN=
+SHL1IMPLIB=	i$(TARGET)$(MOZAB_MAJOR)
+
+SHL1DEF=	$(MISC)$/$(SHL1TARGET).def
+
+DEF1NAME=	$(SHL1TARGET)
+DEF1DEPN=	$(MISC)$/$(SHL1TARGET).flt \
+			$(SLB)$/$(TARGET).lib
+DEFLIB1NAME=$(TARGET)
+#DEF1EXPORTFILE=	exports.dxp
+
+
+# --- Files -------------------------------------
+MOZSLOFILES=\
+	$(SLO)$/MNSInit.obj			            \
+	$(SLO)$/MQueryHelper.obj			    \
+	$(SLO)$/MDatabaseMetaDataHelper.obj		\
+	$(SLO)$/MQuery.obj			            \
+	$(SLO)$/MTypeConverter.obj              \
+	$(SLO)$/MNameMapper.obj					\
+	$(SLO)$/MNSMozabProxy.obj	\
+	$(SLO)$/MNSTerminateListener.obj	\
+	$(SLO)$/MMozillaBootstrap.obj	\
+	$(SLO)$/MNSFolders.obj	\
+	$(SLO)$/MNSProfileDiscover.obj	\
+	$(SLO)$/MNSProfileManager.obj	\
+	$(SLO)$/MNSINIParser.obj	\
+	$(SLO)$/MNSRunnable.obj	\
+	$(SLO)$/MNSProfile.obj					\
+	$(SLO)$/MNSProfileDirServiceProvider.obj
+
+
+SLO2FILES=\
+		$(SLO)$/MConfigAccess.obj				\
+		$(SLO)$/MCatalog.obj					\
+		$(SLO)$/MColumns.obj					\
+		$(SLO)$/MTable.obj						\
+		$(SLO)$/MTables.obj						\
+		$(SLO)$/MColumnAlias.obj				\
+		$(SLO)$/MPreparedStatement.obj			\
+		$(SLO)$/MStatement.obj					\
+		$(SLO)$/MResultSetMetaData.obj			\
+		$(SLO)$/MResultSet.obj					\
+		$(SLO)$/MDatabaseMetaData.obj			\
+		$(SLO)$/MConnection.obj					\
+		$(MOZSLOFILES)
+
+DEPOBJFILES=$(SLO2FILES)
+
+# --- MOZAB BASE Library -----------------------------------
+
+SHL2VERSIONMAP= $(TARGET2).map
+SHL2TARGET=	$(TARGET2)$(MOZAB_MAJOR)
+SHL2OBJS=$(SLO2FILES)
+SHL2STDLIBS=\
+	$(CPPULIB)					\
+	$(CPPUHELPERLIB)			\
+	$(VOSLIB)					\
+	$(OSLLIB)					\
+	$(SALLIB)					\
+	$(DBTOOLSLIB)				\
+	$(COMPHELPERLIB)			\
+	$(MOZ_LIB_XPCOM)
+
+.IF "$(GUI)"=="WNT"
+	SHL2STDLIBS += \
+                 shell32.lib
+.ENDIF # "$(GUI)"=="WNT"
+
+SHL2DEPN=
+SHL2IMPLIB=	i$(TARGET2)
+SHL2DEF=	$(MISC)$/$(SHL2TARGET).def
+DEF2NAME=	$(SHL2TARGET)
+
+# --- Targets ----------------------------------
+
+.INCLUDE : target.mk
+
+# --- filter file ------------------------------
+
+$(MISC)$/$(SHL1TARGET).flt: makefile.mk
+	@echo ------------------------------
+    @echo CLEAR_THE_FILE	> $@
+	@echo _TI				>>$@
+	@echo _real				>>$@
+
+$(MISC)$/$(SHL2TARGET).flt: makefile.mk
+	@echo ------------------------------
+    @echo CLEAR_THE_FILE	> $@
+	@echo _TI				>>$@
+	@echo _real				>>$@
+
