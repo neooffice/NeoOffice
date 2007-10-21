22358aa76b34206ed5ed343f090e31d1cc515d0c
pluby <>
2007-10-21 19:21:42 +0000

    Correct header

--- goodies/source/filter.vcl/ipict/makefile.mk
+++ goodies/source/filter.vcl/ipict/makefile.mk
@@ -31,6 +31,9 @@
 #     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #     MA  02111-1307  USA
 #
+#     Modified October 2007 by Patrick Luby. NeoOffice is distributed under
+#     GPL only under modification term 3 of the LGPL.
+#
 #*************************************************************************
 
 PRJ=..$/..$/..
