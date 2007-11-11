2cae0287374073d8c71436189fd74864fa4b34aa
pluby <>
2007-11-11 21:21:49 +0000

    Attempt to fix bug 2730 by reverting filtered font to the Symbol font instead of the OpenSymbol font

--- vcl/java/source/gdi/salgdi3.cxx
+++ vcl/java/source/gdi/salgdi3.cxx
@@ -667,7 +667,7 @@ void JavaSalGraphics::GetDevFontList( ImplDevFontList* pList )
 	}
 
 	// Iterate through fonts and add each to the font list
-	String aSymbol( RTL_CONSTASCII_USTRINGPARAM( "OpenSymbol" ) );
+	String aSymbol( RTL_CONSTASCII_USTRINGPARAM( "Symbol" ) );
 	for ( ::std::map< String, JavaImplFontData* >::const_iterator it = pSalData->maFontNameMapping.begin(); it != pSalData->maFontNameMapping.end(); ++it )
 	{
 		// Fix bugs 747 and 1040 by only allowing the Mac OS X Symbol font to
