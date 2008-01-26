9ead7553e1742139c9ab224967a8c1604eaf191e
pluby <>
2008-01-26 23:09:35 +0000

    Build with libwpd-0.8.13 obtained from http://go-ooo.org/packages/libwpd/libwpd-0.8.13.tar.gz

---
+++ writerperfect/util/makefile.mk
@@ -0,0 +1,125 @@
+PRJ=..
+PRJNAME=writerperfect
+TARGET=writerperfect
+VERSION=$(UPD)
+
+.INCLUDE :  settings.mk
+
+
+.IF "$(GUIBASE)"=="java"
+SOLARLIB := -L$(PRJ)$/..$/libwpd$/$(ROUT)$/lib $(SOLARLIB)
+.ENDIF		# "$(GUIBASE)"=="java"
+
+.IF "$(GUI)"=="UNX" || "$(GUI)"=="MAC"
+
+.IF "$(SYSTEM_LIBWPD)" == "YES"
+LIBWPD=$(LIBWPD_LIBS)
+.ELSE
+LIBWPD=-lwpdlib
+.ENDIF
+
+.IF "$(SYSTEM_LIBWPS)" == "YES"
+LIBWPS=$(LIBWPS_LIBS)
+.ELSE
+LIBWPS=-lwpslib
+.ENDIF
+
+.IF "$(SYSTEM_LIBWPG)" == "YES"
+LIBWPG=$(LIBWPG_LIBS)
+.ELSE
+LIBWPG=-lwpglib
+.ENDIF
+
+.ELSE
+
+LIBWPD=$(LIBPRE) wpdlib.lib
+LIBWPS=$(LIBPRE) wpslib.lib
+LIBWPG=$(LIBPRE) wpglib.lib
+
+.ENDIF
+
+LIB1TARGET= $(SLB)$/wpft.lib
+LIB1FILES= \
+	$(SLB)$/stream.lib  \
+	$(SLB)$/filter.lib  \
+	$(SLB)$/wpdimp.lib
+SHL1LIBS=$(LIB1TARGET)
+SHL1STDLIBS+= \
+	$(SVLLIB)	\
+	$(SOTLIB) \
+	$(SO2LIB) \
+	$(SVTOOLLIB) \
+	$(UNOTOOLSLIB) \
+	$(TOOLSLIB) \
+	$(COMPHELPERLIB) \
+	$(UCBHELPERLIB) \
+	$(CPPUHELPERLIB) \
+	$(CPPULIB) \
+	$(SALLIB) \
+	$(XMLOFFLIB) \
+	$(LIBWPD)
+
+SHL1TARGET = wpft$(UPD)$(DLLPOSTFIX)
+SHL1IMPLIB = i$(SHL1TARGET)
+SHL1LIBS = $(LIB1TARGET)
+SHL1VERSIONMAP=wpft.map
+DEF1NAME=$(SHL1TARGET)
+
+
+LIB2TARGET= $(SLB)$/msworks.lib
+LIB2FILES= \
+	$(SLB)$/stream.lib  \
+	$(SLB)$/filter.lib  \
+	$(SLB)$/wpsimp.lib
+SHL2LIBS=$(LIB2TARGET)
+SHL2STDLIBS+= \
+	$(SVLLIB)	\
+	$(SOTLIB) \
+	$(SO2LIB) \
+	$(SVTOOLLIB) \
+	$(UNOTOOLSLIB) \
+	$(TOOLSLIB) \
+	$(COMPHELPERLIB) \
+	$(UCBHELPERLIB) \
+	$(CPPUHELPERLIB) \
+	$(CPPULIB) \
+	$(SALLIB) \
+	$(XMLOFFLIB) \
+	$(LIBWPS) \
+	$(LIBWPD)
+
+SHL2TARGET = msworks$(UPD)$(DLLPOSTFIX)
+SHL2IMPLIB = i$(SHL2TARGET)
+SHL2LIBS = $(LIB2TARGET)
+SHL2VERSIONMAP=msworks.map
+DEF2NAME=$(SHL2TARGET)
+
+LIB3TARGET= $(SLB)$/wpgimport.lib
+LIB3FILES= \
+	$(SLB)$/stream.lib  \
+	$(SLB)$/filter.lib  \
+	$(SLB)$/wpgimp.lib
+SHL3LIBS=$(LIB3TARGET)
+SHL3STDLIBS+= \
+	$(SVLLIB)	\
+	$(SOTLIB) \
+	$(SO2LIB) \
+	$(SVTOOLLIB) \
+	$(UNOTOOLSLIB) \
+	$(TOOLSLIB) \
+	$(COMPHELPERLIB) \
+	$(UCBHELPERLIB) \
+	$(CPPUHELPERLIB) \
+	$(CPPULIB) \
+	$(SALLIB) \
+	$(XMLOFFLIB) \
+	$(LIBWPD) \
+	$(LIBWPG)
+
+SHL3TARGET = wpgimport$(UPD)$(DLLPOSTFIX)
+SHL3IMPLIB = i$(SHL3TARGET)
+SHL3LIBS = $(LIB3TARGET)
+SHL3VERSIONMAP=wpgimport.map
+DEF3NAME=$(SHL3TARGET)
+
+.INCLUDE :  target.mk
