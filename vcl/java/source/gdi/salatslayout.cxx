70b9b49b2b016ea1425d6d175a82a553a28b41b5
pluby <>
2008-05-23 18:41:55 +0000

    Reinstate zero glyph fix but this time set the char width to zero to prevent the Hebrew layout mess

--- vcl/java/source/gdi/salatslayout.cxx
+++ vcl/java/source/gdi/salatslayout.cxx
@@ -1350,6 +1350,16 @@ bool SalATSLayout::LayoutText( ImplLayoutArgs& rArgs )
 						else
 							continue;
 					}
+					// Prevent display of zero glyphs in fallback levels where
+					// we know that there is a valid fallback font
+					else if ( !nGlyph && mnFallbackLevel && mnFallbackLevel < MAX_FALLBACK - 1 && pFallbackFont )
+					{
+						nCharWidth = 0;
+						if ( bFirstGlyph )
+							nGlyph = nChar | GF_ISCHAR;
+						else
+							continue;
+					}
 
 					// Fix bug 2512 without breaking fix for bug 2453 by
 					// allowing spacing glyphs to go through but marking when
