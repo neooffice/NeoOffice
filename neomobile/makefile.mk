4d4dcc6bdf612ab06a6ae00596eb924aea32a4d2
pluby <>
2008-07-12 21:30:41 +0000

    Initial commit of NeoOffice Mobile stub files

---
+++ neomobile/makefile.mk
@@ -0,0 +1,100 @@
+##########################################################################
+# 
+#   $RCSfile$
+# 
+#   $Revision$
+# 
+#   last change: $Author$ $Date$
+# 
+#   The Contents of this file are made available subject to the terms of
+#   either of the following licenses
+# 
+#          - GNU General Public License Version 2.1
+# 
+#   GNU General Public License Version 2.1
+#   =============================================
+#   Copyright 2008 by Planamesa Inc.
+# 
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public
+#   License version 2.1, as published by the Free Software Foundation.
+# 
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+# 
+##########################################################################
+
+PRJ=.
+
+PRJNAME=NeoOfficeMobile
+TARGET=neomobile.uno
+TARGETTYPE=CUI
+
+ENABLE_EXCEPTIONS=TRUE
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+CDEFS+=-DDLLPOSTFIX=$(DLLPOSTFIX)
+
+# Don't put lib prefix on shared library
+DLLPRE=
+
+# Add locally built types registry to cppumaker search path
+UNOUCRRDB+=$(OUT)$/ucr$/$(TARGET).db
+
+# --- Files --------------------------------------------------------
+
+IDLFILES= \
+	XNeoOfficeMobile.idl
+
+UNOTYPES= \
+	org.neooffice.XNeoOfficeMobile
+
+# Force creation of the IDL header files before the compiling source files
+UNOUCRDEP=$(OUT)$/ucr$/$(TARGET).db
+
+SLOFILES= \
+	$(SLO)$/neomobile.obj
+
+SHL1TARGET=$(TARGET)
+SHL1OBJS=$(SLOFILES)
+SHL1STDLIBS= \
+	$(SALLIB) \
+	$(CPPULIB) \
+	$(CPPUHELPERLIB) \
+	$(COMPHELPERLIB) \
+	$(VOSLIB)	\
+	$(VCLLIB)
+
+SHL1STDLIBS += -framework AppKit -framework WebKit -framework Carbon
+
+# --- Targets ------------------------------------------------------
+
+# Force zipping recipe to execute at the end
+makeoxt : ALLTAR
+
+.INCLUDE :  target.mk
+
+makeoxt :
+	$(RM) $(BIN)$/$(PRJNAME).oxt
+	zip -r $(BIN)$/$(PRJNAME).oxt META-INF $(PRJNAME) uiIntegration.xcu Images -x "*CVS*"
+	zip $(ZIPFLAGS) $(PWD)$/$(BIN)$/$(PRJNAME).oxt $(UCR)$/$(TARGET).db -x "*CVS*"
+.IF "$(debug)" == ""
+# Use stripped library if not in debug mode
+	$(RM) $(BIN)$/stripped
+	$(MKDIRHIER) $(LB)$/stripped
+	$(COPY) $(LB)$/$(TARGET)$(DLLPOST) $(LB)$/stripped$/$(TARGET)$(DLLPOST)
+	strip -S -x $(LB)$/stripped$/$(TARGET)$(DLLPOST)
+	zip $(ZIPFLAGS) $(PWD)$/$(BIN)$/$(PRJNAME).oxt $(LB)$/stripped/$(TARGET)$(DLLPOST) -x "*CVS*"
+.ELSE		# "$(debug)" == ""
+	zip $(ZIPFLAGS) $(PWD)$/$(BIN)$/$(PRJNAME).oxt $(LB)$/$(TARGET)$(DLLPOST) -x "*CVS*"
+.ENDIF		# "$(debug)" == ""
---
+++ neomobile/neomobile.mm
@@ -0,0 +1,333 @@
+/*************************************************************************
+ *
+ *  $RCSfile$
+ *
+ *  $Revision$
+ *
+ *  last change: $Author$ $Date$
+ *
+ *  The Contents of this file are made available subject to the terms of
+ *  either of the following licenses
+ *
+ *		 - GNU General Public License Version 2.1
+ *
+ *  GNU General Public License Version 2.1
+ *  =============================================
+ *  Copyright 2008 by Planamesa Inc.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ *
+ *************************************************************************/
+
+/*************************************************************************
+ *************************************************************************
+ *
+ * service implementation:	 org.neooffice.NeoOfficeMobile
+ * exported interfaces:		 org.neooffice.XNeoOfficeMobile
+ *
+ *************************************************************************
+ *************************************************************************/
+
+#include <stdio.h>
+
+#ifndef _RTL_USTRING_HXX_
+#include <rtl/ustring.hxx>
+#endif
+
+#ifndef _CPPUHELPER_QUERYINTERFACE_HXX_
+#include <cppuhelper/queryinterface.hxx> // helper for queryInterface() impl
+#endif
+#ifndef _CPPUHELPER_FACTORY_HXX_
+#include <cppuhelper/factory.hxx> // helper for component factory
+#endif
+#ifndef _CPPUHELPER_IMPLEMENATIONENTRY_HXX_
+#include <cppuhelper/implementationentry.hxx>
+#endif
+
+// generated c++ interfaces
+
+#ifndef _COM_SUN_STAR_LANG_XSINGLESERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XSingleServiceFactory.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XSERVICEINFO_HPP_
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#endif
+#ifndef _COM_SUN_STAR_REGISTRY_XREGISTRYKEY_HPP_
+#include <com/sun/star/registry/XRegistryKey.hpp>
+#endif
+#ifndef _ORG_NEOOFFICE_XGRAMMARCHECKER_HPP_
+#include <org/neooffice/XNeoOfficeMobile.hpp>
+#endif
+#ifndef _CPPUHELPER_IMPLBASE_HXX_
+#include <cppuhelper/implbase2.hxx>
+#endif
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
+
+#include "premac.h"
+#import <Carbon/Carbon.h>
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+#include "postmac.h"
+
+#include <string>
+#include <strstream>
+
+#define SERVICENAME "org.neooffice.NeoOfficeMobile"
+#define IMPLNAME	"org.neooffice.XNeoOfficeMobile"
+
+using namespace ::rtl;
+using namespace ::osl;
+using namespace ::cppu;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::registry;
+using namespace ::org::neooffice;
+
+//========================================================================
+class MacOSXNeoOfficeMobileImpl
+	: public ::cppu::WeakImplHelper2<XServiceInfo, XNeoOfficeMobile>
+{
+	// to obtain other services if needed
+	Reference< XComponentContext > m_xServiceManager;
+	
+	sal_Int32 m_nRefCount;
+	sal_Int32 m_nCount;
+	OUString m_aLocale;
+	
+public:
+	MacOSXNeoOfficeMobileImpl( const Reference< XComponentContext > & xServiceManager )
+		: m_xServiceManager( xServiceManager ), m_nRefCount( 0 ) {}
+	virtual ~MacOSXNeoOfficeMobileImpl() {}
+
+    // XServiceInfo	implementation
+    virtual OUString SAL_CALL getImplementationName(  ) throw(RuntimeException);
+    virtual sal_Bool SAL_CALL supportsService( const OUString& ServiceName ) throw(RuntimeException);
+    virtual Sequence< OUString > SAL_CALL getSupportedServiceNames(  ) throw(RuntimeException);
+    static Sequence< OUString > SAL_CALL getSupportedServiceNames_Static(  );
+
+	// XNeoOfficeMobile implementation
+	virtual ::sal_Bool 
+		SAL_CALL hasNeoOfficeMobile( ) 
+		throw (::com::sun::star::uno::RuntimeException);
+	virtual ::sal_Bool 
+		SAL_CALL showNeoOfficeMobile( ) 
+		throw (::com::sun::star::uno::RuntimeException);
+};
+
+//*************************************************************************
+OUString SAL_CALL MacOSXNeoOfficeMobileImpl::getImplementationName(  ) 
+	throw(RuntimeException)
+{
+	return OUString( RTL_CONSTASCII_USTRINGPARAM(IMPLNAME) );
+}	
+
+//*************************************************************************
+sal_Bool SAL_CALL MacOSXNeoOfficeMobileImpl::supportsService( const OUString& ServiceName ) 
+	throw(RuntimeException)
+{
+	Sequence< OUString > aSNL = getSupportedServiceNames();
+	const OUString * pArray = aSNL.getArray();
+	for( sal_Int32 i = 0; i < aSNL.getLength(); i++ )
+		if( pArray[i] == ServiceName )
+			return sal_True;
+	return sal_False;
+}	
+
+//*************************************************************************
+Sequence<OUString> SAL_CALL MacOSXNeoOfficeMobileImpl::getSupportedServiceNames(  ) 
+	throw(RuntimeException)
+{
+	return getSupportedServiceNames_Static();
+}	
+
+//*************************************************************************
+Sequence<OUString> SAL_CALL MacOSXNeoOfficeMobileImpl::getSupportedServiceNames_Static(  ) 
+{
+	OUString aName( RTL_CONSTASCII_USTRINGPARAM(SERVICENAME) );
+	return Sequence< OUString >( &aName, 1 );
+}	
+
+
+
+
+/**
+ * Function to create a new component instance; is needed by factory helper implementation.
+ * @param xMgr service manager to if the components needs other component instances
+ */
+Reference< XInterface > SAL_CALL MacOSXNeoOfficeMobileImpl_create(
+	const Reference< XComponentContext > & xContext )
+{
+	Reference< XTypeProvider > xRet = static_cast<XTypeProvider *>(new MacOSXNeoOfficeMobileImpl(xContext));
+	return xRet;
+}
+
+
+//#########################################################################
+//#### EXPORTED ###########################################################
+//#########################################################################
+
+		/* shared lib exports implemented without helpers in service_impl1.cxx */
+		namespace neo_macosxneomobile_impl
+		{
+		
+	static Sequence< OUString > getSupportedServiceNames_MacOSXNeoOfficeMobileImpl()
+		{
+			static Sequence < OUString > *pNames = 0;
+			if( ! pNames )
+			{
+				if( !pNames )
+				{
+					static Sequence< OUString > seqNames(1);
+					seqNames.getArray()[0] = OUString(RTL_CONSTASCII_USTRINGPARAM(SERVICENAME));
+					pNames = &seqNames;
+				}
+			}
+			return *pNames;
+		}
+		 
+		static OUString getImplementationName_MacOSXNeoOfficeMobileImpl()
+		{
+			static OUString *pImplName = 0;
+			if( ! pImplName )
+			{
+				if( ! pImplName )
+				{
+					static OUString implName( RTL_CONSTASCII_USTRINGPARAM(IMPLNAME) );
+				pImplName = &implName;
+				}
+			}
+			return *pImplName;
+	}
+	
+		static struct ::cppu::ImplementationEntry s_component_entries [] =
+		{
+		    //{
+		    //    create_MyService1Impl, getImplementationName_MyService1Impl,
+		    //    getSupportedServiceNames_MyService1Impl, ::cppu::createSingleComponentFactory,
+		    //    0, 0
+		    //},
+		    {
+		        MacOSXNeoOfficeMobileImpl_create, getImplementationName_MacOSXNeoOfficeMobileImpl,
+		        getSupportedServiceNames_MacOSXNeoOfficeMobileImpl, ::cppu::createSingleComponentFactory,
+		        0, 0
+		    },
+		    { 0, 0, 0, 0, 0, 0 }
+		};
+		}
+
+/**
+ * Gives the environment this component belongs to.
+ */
+extern "C" void SAL_CALL component_getImplementationEnvironment(const sal_Char ** ppEnvTypeName, uno_Environment ** ppEnv)
+{
+	*ppEnvTypeName = CPPU_CURRENT_LANGUAGE_BINDING_NAME;
+}
+
+/**
+ * This function creates an implementation section in the registry and another subkey
+ *
+ * for each supported service.
+ * @param pServiceManager   the service manager
+ * @param pRegistryKey      the registry key
+ */
+extern "C" sal_Bool SAL_CALL component_writeInfo(XMultiServiceFactory * pServiceManager, XRegistryKey * pRegistryKey)
+{
+	return ::cppu::component_writeInfoHelper(pServiceManager, pRegistryKey, ::neo_macosxneomobile_impl::s_component_entries);
+}
+
+/**
+ * This function is called to get service factories for an implementation.
+ *
+ * @param pImplName       name of implementation
+ * @param pServiceManager a service manager, need for component creation
+ * @param pRegistryKey    the registry key for this component, need for persistent data
+ * @return a component factory 
+ */
+extern "C" void * SAL_CALL component_getFactory(const sal_Char * pImplName, XMultiServiceFactory * pServiceManager, XRegistryKey * pRegistryKey)
+{
+	return ::cppu::component_getFactoryHelper(pImplName, pServiceManager, pRegistryKey, ::neo_macosxneomobile_impl::s_component_entries);
+}
+
+#pragma mark -
+
+@interface CreateWebViewImpl : NSObject
+{
+}
+- (id)init;
+- (void)showWebView:(id)obj;
+@end
+
+@implementation CreateWebViewImpl
+
+- (id)init
+{
+	self = [super init];
+	return(self);
+}
+
+- (void)showWebView:(id)obj
+{
+	fprintf( stderr, "NeoOffice Mobile WebView not implemented\n" );
+}
+@end
+
+/**
+ * Check if the we have full WebView support available
+ */
+::sal_Bool 
+		SAL_CALL MacOSXNeoOfficeMobileImpl::hasNeoOfficeMobile( ) 
+		throw (::com::sun::star::uno::RuntimeException)
+{
+	// we currently need to be running on 10.3 in order to have full WebView
+	// support.  Check using our gestalt.
+	
+	long res=0;
+	if(Gestalt(gestaltSystemVersion, &res)==noErr)
+	{
+		bool isPantherOrHigher = ( ( ( ( res >> 8 ) & 0x00FF ) == 0x10 ) && ( ( ( res >> 4 ) & 0x000F ) >= 0x3 ) );
+		if(!isPantherOrHigher)
+			return(false);
+	}
+	
+	return(true);
+}
+
+/**
+ * Construct a new media browser instance.
+ */
+::sal_Bool 
+		SAL_CALL MacOSXNeoOfficeMobileImpl::showNeoOfficeMobile( ) 
+		throw (::com::sun::star::uno::RuntimeException)
+{
+	NSAutoreleasePool *pool=[[NSAutoreleasePool alloc] init];
+	
+	CreateWebViewImpl *imp=[[CreateWebViewImpl alloc] init];
+
+	NSArray *pModes = [NSArray arrayWithObjects:NSDefaultRunLoopMode, NSEventTrackingRunLoopMode, NSModalPanelRunLoopMode, @"AWTRunLoopMode", nil];
+	unsigned long nCount = Application::ReleaseSolarMutex();
+	[imp performSelectorOnMainThread:@selector(showWebView:) withObject:imp waitUntilDone: 1 modes: pModes];
+	Application::AcquireSolarMutex( nCount );
+		
+	[imp release];
+	
+	[pool release];
+	
+	return(true);
+}
