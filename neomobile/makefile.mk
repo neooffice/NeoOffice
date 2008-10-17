d990c10836d4415ba86de04290d5afd3806f7348
pluby <>
2008-10-17 19:44:51 +0000

    Correct @loader_path link paths so that our custom extensions will load via unopkg.bin

--- neomobile/makefile.mk
+++ neomobile/makefile.mk
@@ -90,6 +90,9 @@ makeoxt :
 	$(RM) $(BIN)$/$(PRJNAME).oxt
 	zip -r $(BIN)$/$(PRJNAME).oxt META-INF $(PRJNAME) uiIntegration.xcu Images -x "*CVS*"
 	zip $(ZIPFLAGS) $(PWD)$/$(BIN)$/$(PRJNAME).oxt $(UCR)$/$(TARGET).db -x "*CVS*"
+# Change install names to avoid library loading issues
+	sh -c -e 'for i in `otool -L "$(LB)$/$(TARGET)$(DLLPOST)" | awk "{ print \\$$1 }" | grep "^@loader_path\/"` ; do install_name_tool -change "$${i}" `echo "$${i}" | sed "s#^@loader_path/\.\./ure-link/lib/#@loader_path/../../../../../../basis-link/ure-link/lib/#" | sed "s#^@loader_path/lib#@loader_path/../../../../../../basis-link/program/lib#"` "$(LB)$/$(TARGET)$(DLLPOST)" ; done'
+	$(PERL) $(SOLARENV)$/bin$/macosx-change-install-names.pl extshl OOO "$(LB)$/$(TARGET)$(DLLPOST)"
 .IF "$(debug)" == ""
 # Use stripped library if not in debug mode
 	$(RM) $(BIN)$/stripped
