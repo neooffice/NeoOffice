fd46e8ef224016eb25e8bb345f99ca4edef0bb9d
cvs2git <>
2008-12-17 07:35:42 +0000

    This commit was manufactured by cvs2svn to create branch 'NeoOffice- 2_2_5_branch'.

---
+++ neomobile/NSDataAdditions.h
@@ -0,0 +1,17 @@
+#ifndef NSDATAADDITIONS_H_
+#define NSDATAADDITIONS_H_
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (NSDataAdditions)
++ (NSData *) dataWithBase64EncodedString:(NSString *) string;
+- (id) initWithBase64EncodedString:(NSString *) string;
+
+- (NSString *) base64Encoding;
+- (NSString *) base64EncodingWithLineLength:(unsigned int) lineLength;
+
+- (BOOL) hasPrefix:(NSData *) prefix;
+- (BOOL) hasPrefixBytes:(void *) prefix length:(unsigned int) length;
+@end
+
+#endif
