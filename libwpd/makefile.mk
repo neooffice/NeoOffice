94b39646a8e9f8b9fadb110560cfffc6f135006c
pluby <>
2008-12-30 17:30:08 +0000

    Fix bug 3050 by reapplying the fix in the NeoOffice-2_2_5_branch branch

---
+++ libwpd/makefile.mk
@@ -0,0 +1,64 @@
+#*************************************************************************
+#
+# Copyright 2008 by Sun Microsystems, Inc.
+#
+# $RCSfile$
+#
+# $Revision$
+#
+# This file is part of NeoOffice.
+#
+# NeoOffice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# NeoOffice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU General Public License
+# version 3 along with NeoOffice.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.txt>
+# for a copy of the GPLv3 License.
+#
+# Modified December 2008 by Patrick Luby. NeoOffice is distributed under
+# GPL only under modification term 2 of the LGPL.
+#
+#*************************************************************************
+
+PRJ=.
+
+PRJNAME=wpd
+TARGET=wpd
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+# --- Files --------------------------------------------------------
+
+.IF "$(SYSTEM_LIBWPD)" == "YES"
+@all:
+	@echo "Using system libwpd..."
+.ENDIF
+
+TARFILE_NAME=libwpd-0.8.14
+.IF "$(GUIBASE)" == "java"
+PATCH_FILE_NAME=$(TARFILE_NAME).diff
+.ELSE		# "$(GUIBASE)" == "java"
+#PATCH_FILE_NAME=$(TARFILE_NAME).diff
+.ENDIF		# "$(GUIBASE)" == "java"
+BUILD_ACTION=dmake $(MFLAGS) $(CALLMACROS)
+BUILD_DIR=src$/lib
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE :	target.mk
+.INCLUDE :	tg_ext.mk
+
+.IF "$(GUIBASE)" == "java"
+BACK_PATH:=$(BACK_PATH)..$/..$/libwpd$/
+.ENDIF		# "$(GUIBASE)" == "java"
