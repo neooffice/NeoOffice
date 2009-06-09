c074453e70b41fe1d2f71e2d6381eb55a6ce2f6d
pluby <>
2009-06-09 21:03:44 +0000

    Reenable fix for bug 3467 that was partially disabled by fix for the crashing while dragging bug

--- vcl/java/source/app/salinst.cxx
+++ vcl/java/source/app/salinst.cxx
@@ -141,6 +141,7 @@ public:
 	virtual void			toggle() {}
 };
 
+static bool bAllowReleaseYieldMutex = false;
 static SalYieldMutex aEventQueueMutex;
 
 using namespace rtl;
@@ -188,7 +189,9 @@ BOOL VCLInstance_updateNativeMenus()
 	// mutex was unlocked are already dispatched before we try to lock the
 	// mutex. Fix bug 3467 by speeding up acquiring of the event queue mutex
 	// by releasing the application mutex if already acquired by this thread.
+	bAllowReleaseYieldMutex = true;
 	ULONG nCount = Application::ReleaseSolarMutex();
+	bAllowReleaseYieldMutex = false;
 	aEventQueueMutex.acquire();
 	Application::AcquireSolarMutex( nCount );
 
@@ -424,7 +427,7 @@ ULONG JavaSalInstance::ReleaseYieldMutex()
 	// Never release the mutex in the main thread as it can cause crashing
 	// when dragging when the OOo code's VCL event dispatching thread runs
 	// while we are in the middle of a native drag event
-	if ( GetCurrentEventLoop() == GetMainEventLoop() )
+	if ( !bAllowReleaseYieldMutex && GetCurrentEventLoop() == GetMainEventLoop() )
 		return 0;
 
 	SalYieldMutex* pYieldMutex = mpSalYieldMutex;
@@ -527,21 +530,22 @@ void JavaSalInstance::Yield( bool bWait, bool bHandleAllCurrentEvents )
 			pSalData->maTimeout += pSalData->mnTimerInterval;
 			pSVData->mpSalTimer->CallCallback();
 			com_sun_star_vcl_VCLFrame::flushAllFrames();
+
+			// Reduce noticeable pause when opening a new document by delaying
+			// update of submenus until next available timer timeout
+			if ( pSalData->maNativeEventCondition.check() && pSalData->mpFocusFrame && pSalData->mpFocusFrame->mbVisible )
+			{
+				for ( int i = 0; pSalData->mpFocusFrame->maUpdateMenuList.size() && i < 8; i++ )
+					UpdateMenusForFrame( pSalData->mpFocusFrame, pSalData->mpFocusFrame->maUpdateMenuList.front(), false );
+			}
 		}
 	}
 
 	// Determine timeout
 	ULONG nTimeout = 0;
-	if ( !bMainEventLoop && pSalData->maNativeEventCondition.check() && !Application::IsShutDown() )
+	if ( !bMainEventLoop && bWait && pSalData->maNativeEventCondition.check() && !Application::IsShutDown() )
 	{
-		// Reduce noticeable pause when opening a new document by delaying
-		// update of submenus until next available timer timeout
-		if ( pSalData->mpFocusFrame && pSalData->mpFocusFrame->mbVisible && pSalData->mpFocusFrame->maUpdateMenuList.size() )
-		{
-			for ( int i = 0; pSalData->mpFocusFrame->maUpdateMenuList.size() && i < 8; i++ )
-				UpdateMenusForFrame( pSalData->mpFocusFrame, pSalData->mpFocusFrame->maUpdateMenuList.front(), false );
-		}
-		else if ( bWait && pSalData->mnTimerInterval )
+		if ( pSalData->mnTimerInterval )
 		{
 			timeval aTimeout;
 			gettimeofday( &aTimeout, NULL );
@@ -550,13 +554,13 @@ void JavaSalInstance::Yield( bool bWait, bool bHandleAllCurrentEvents )
 				aTimeout = pSalData->maTimeout - aTimeout;
 				nTimeout = aTimeout.tv_sec * 1000 + aTimeout.tv_usec / 1000;
 			}
-
-			// Wait a little bit to prevent excessive CPU usage. Fix bug 2588
-			// by only doing so when the timeout is already set to a non-zero
-			// value.
-			if ( nTimeout < 10 )
-				nTimeout = 10;
 		}
+
+		// Wait a little bit to prevent excessive CPU usage. Fix bug 2588
+		// by only doing so when the timeout is already set to a non-zero
+		// value.
+		if ( nTimeout < 10 )
+			nTimeout = 10;
 	}
 
 	if ( nTimeout )
