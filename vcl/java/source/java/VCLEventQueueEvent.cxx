72ca81df1b84150a340878f398c3225562d33194
pluby <>
2009-09-24 18:01:28 +0000

    Yield before invoking JavaSalFrame::SetPosSize() to further reduce the chance of window jumping back while moving it

--- vcl/java/source/java/VCLEventQueueEvent.cxx
+++ vcl/java/source/java/VCLEventQueueEvent.cxx
@@ -736,7 +736,10 @@ void com_sun_star_vcl_VCLEvent::dispatch()
 				// do this for while dragging in a frame as it can cause the
 				// frame to unexpectedly jump backwards.
 				if ( pFrame->mbVisible && !pFrame->mbInSetPosSize && !pFrame->mbInShow && pSalData->mpLastDragFrame )
+				{
+					OThread::yield();
 					pFrame->SetPosSize( 0, 0, 0, 0, 0 );
+				}
 			}
 			if ( pPosSize )
 				delete pPosSize;
