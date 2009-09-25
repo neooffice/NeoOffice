430ecaf76a7142542179b7a3fea4efa05ccee204
pluby <>
2009-09-25 00:12:47 +0000

    Fix bug 3557 and replace hardcoded Control-F6 check with a lookup in the com.apple.universalaccess preferences

--- vcl/java/source/java/VCLEventQueue_cocoa.m
+++ vcl/java/source/java/VCLEventQueue_cocoa.m
@@ -49,6 +49,53 @@ static NSString *pNSWindowViewAWTString = @"NSWindowViewAWT";
 
 inline long Float32ToLong( Float32 f ) { return (long)( f == 0 ? f : f < 0 ? f - 1.0 : f + 1.0 ); }
 
+static BOOL EventMatchesShortcutKey( NSEvent *pEvent, unsigned int nKey )
+{
+	BOOL bRet = NO;
+
+	if ( pEvent && [pEvent type] == NSKeyDown )
+	{
+		CFPreferencesAppSynchronize( CFSTR( "com.apple.universalaccess" ) );
+		CFPropertyListRef pPref = CFPreferencesCopyAppValue( CFSTR( "UserAssignableHotKeys" ), CFSTR( "com.apple.universalaccess" ) );
+		if ( pPref )
+		{
+			if ( CFGetTypeID( pPref ) == CFArrayGetTypeID() )
+			{
+				CFIndex nCount = CFArrayGetCount( pPref );
+				CFIndex i = 0;
+				for ( ; i < nCount; i++ ) {
+					NSDictionary *pDict = (NSDictionary *)CFArrayGetValueAtIndex( pPref, i );
+					if ( pDict && CFGetTypeID( pDict ) == CFDictionaryGetTypeID() )
+					{
+						// Note that Apple uses an odd spelling for this key
+						NSNumber *pSybmolicHotKey = (NSNumber *)[pDict valueForKey:@"sybmolichotkey"];
+						if ( pSybmolicHotKey && [pSybmolicHotKey unsignedIntValue] == nKey )
+						{
+							NSNumber *pEnabled = (NSNumber *)[pDict valueForKey:@"enabled"];
+							if ( pEnabled && [pEnabled intValue] )
+							{
+								NSNumber *pKeyCode = (NSNumber *)[pDict valueForKey:@"key"];
+								if ( pKeyCode && [pKeyCode unsignedShortValue] == [pEvent keyCode] )
+								{
+									NSNumber *pModifiers = (NSNumber *)[pDict valueForKey:@"modifier"];
+									if ( pModifiers && ( [pModifiers unsignedIntValue]  & ~NSHelpKeyMask & ~NSFunctionKeyMask & NSDeviceIndependentModifierFlagsMask ) == ( (unsigned int)[pEvent modifierFlags] & ~NSHelpKeyMask & ~NSFunctionKeyMask & NSDeviceIndependentModifierFlagsMask ) )
+										bRet = YES;
+								}
+							}
+
+							break;
+						}
+					}
+				}
+			}
+
+			CFRelease( pPref );
+		}
+	}
+
+	return bRet;
+}
+
 @interface NSObject (ApplicationHasDelegate)
 - (void)cancelTermination;
 @end
@@ -474,20 +521,8 @@ static VCLResponder *pSharedResponder = nil;
 		{
 			// Do not allow utility windows to grab the focus except when the
 			// user presses Control-F6
-			BOOL bGrabFocus = NO;
 			NSApplication *pApp = [NSApplication sharedApplication];
-			if ( pApp )
-			{
-				NSEvent *pEvent = [pApp currentEvent];
-				if ( pEvent && [pEvent type] == NSKeyDown && [pEvent modifierFlags] & NSControlKeyMask )
-				{
-					NSString *pChars = [pEvent charactersIgnoringModifiers];
-					if ( pChars && [pChars length] == 1 && [pChars characterAtIndex:0] == NSF6FunctionKey )
-						bGrabFocus = YES;
-				}
-			}
-
-			if ( !bGrabFocus )
+			if ( pApp && !EventMatchesShortcutKey( [pApp currentEvent], 11 ) )
 				return;
 		}
 	}
@@ -645,6 +680,16 @@ static VCLResponder *pSharedResponder = nil;
 			if ( [pResponder hasMarkedText] )
 				[pResponder insertText:pCancelInputMethodText];
 			[pResponder abandonInput];
+		}
+
+		// Fix bug 3557 by forcing any non-utility windows to the back when
+		// they lose focus while cycling through windows with the Command-`
+		// shortcut
+		if ( ![super respondsToSelector:@selector(_isUtilityWindow)] || ![super _isUtilityWindow] )
+		{
+			NSApplication *pApp = [NSApplication sharedApplication];
+			if ( pApp && EventMatchesShortcutKey( [pApp currentEvent], 27 ) )
+				[self orderBack:self];
 		}
 	}
 
