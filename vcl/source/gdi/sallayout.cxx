159fed222d3b96b2d84139073b50f06411adf909
pluby <>
2009-10-02 21:09:34 +0000

    Fix bug 3564 by only adjusting the next glyph if there is a valid glyph

--- vcl/source/gdi/sallayout.cxx
+++ vcl/source/gdi/sallayout.cxx
@@ -1164,7 +1164,7 @@ void GenericSalLayout::ApplyDXArray( ImplLayoutArgs& rArgs )
                 if( nDiff > 0 )
                 {
                     // Fix bug 823 by handling inappropriate placement of
-                    // kashidas by upper layers
+                    // kashidas by upper layers.
                     if( pG->IsKashidaAllowedAfterGlyph() || IsSpacingGlyph( pG->mnGlyphIndex ) )
                         bHandled = true;
                 }
@@ -1174,7 +1174,9 @@ void GenericSalLayout::ApplyDXArray( ImplLayoutArgs& rArgs )
                     bHandled = true;
                 }
 
-                if( !bHandled && i < mnGlyphCount - 1 )
+                // Fix bug 3564 by only adjusting the next glyph if there is a
+                // valid glyph
+                if( !bHandled && i < mnGlyphCount - 1 && pG->mnGlyphIndex & GF_IDXMASK )
                 {
                     nNewClusterWidth -= nDiff;
                     pNewGlyphWidths[i + 1] += nDiff;
