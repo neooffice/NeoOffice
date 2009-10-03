eba62c63e9df497a2bf2c22e4addc109001d0019
pluby <>
2009-10-03 06:05:49 +0000

    Explicitly set the help menu on Snow Leopard as Snow Leopard rarely sets it when not using English

--- vcl/java/source/app/salinst_cocoa.h
+++ vcl/java/source/app/salinst_cocoa.h
@@ -45,6 +45,7 @@ BEGIN_C
 #endif
 void NSApplication_dispatchPendingEvents();
 id NSApplication_getModalWindow();
+void NSApplication_setHelpMenu();
 BOOL VCLInstance_updateNativeMenus();
 #ifdef __cplusplus
 END_C
