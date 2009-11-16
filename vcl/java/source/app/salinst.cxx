3de9fc5c20af6e7d173ce7180bffb65959eccfd3
pluby <>
2009-11-16 17:06:09 +0000

    Fix bug 3571 by not cancelling menu display when all windows                // are minimized

--- vcl/java/source/app/salinst.cxx
+++ vcl/java/source/app/salinst.cxx
@@ -241,7 +241,9 @@ BOOL VCLInstance_updateNativeMenus()
 			// only cancelling dislay of menus when there are visible windows
 			for ( ::std::list< JavaSalFrame* >::const_iterator it = pSalData->maFrameList.begin(); it != pSalData->maFrameList.end(); ++it )
 			{
-				if ( (*it)->mbVisible )
+				// Fix bug 3571 by not cancelling menu display when all windows
+				// are minimized
+				if ( (*it)->mbVisible && (*it)->mpVCLFrame && (*it)->mpVCLFrame->getState() != SAL_FRAMESTATE_MINIMIZED )
 				{
 					bRet = FALSE;
 					break;
