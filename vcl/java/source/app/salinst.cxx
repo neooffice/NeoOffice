db89f631629f937f9b3960dec927eb23a5ecda1c
pluby <>
2009-11-16 17:15:49 +0000

    Merge changes from the HEAD branch

--- vcl/java/source/app/salinst.cxx
+++ vcl/java/source/app/salinst.cxx
@@ -238,7 +238,9 @@ static OSStatus CarbonEventHandler( EventHandlerCallRef aNextHandler, EventRef a
 						// when there are visible windows
 						for ( ::std::list< JavaSalFrame* >::const_iterator it = pSalData->maFrameList.begin(); it != pSalData->maFrameList.end(); ++it )
 						{
-							if ( (*it)->mbVisible )
+							// Fix bug 3571 by not cancelling menu display when
+							// all windows are minimized
+							if ( (*it)->mbVisible && (*it)->mpVCLFrame && (*it)->mpVCLFrame->getState() != SAL_FRAMESTATE_MINIMIZED )
 							{
 								bSucceeded = false;
 								break;
