ad64aa7696259862502600f4182f9f4fb8047b70
pluby <>
2010-03-11 22:10:48 +0000

    Fix bug 3594 by never setting the help bit checkboxes as all versions of OpenOffice.org do not mask the help bit when importing files

--- sw/source/filter/ww8/WW8FFData.cxx
+++ sw/source/filter/ww8/WW8FFData.cxx
@@ -1,30 +1,29 @@
 /*************************************************************************
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
  * Copyright 2008 by Sun Microsystems, Inc.
- *
- * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile$
  * $Revision$
  *
- * This file is part of OpenOffice.org.
+ * This file is part of NeoOffice.
  *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
+ * NeoOffice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
  * only, as published by the Free Software Foundation.
  *
- * OpenOffice.org is distributed in the hope that it will be useful,
+ * NeoOffice is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
+ * GNU General Public License version 3 for more details
  * (a copy is included in the LICENSE file that accompanied this code).
  *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
+ * You should have received a copy of the GNU General Public License
+ * version 3 along with NeoOffice.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.txt>
+ * for a copy of the GPLv3 License.
+ *
+ * Modified March 2010 by Patrick Luby. NeoOffice is distributed under
+ * GPL only under modification term 2 of the LGPL.
  *
  ************************************************************************/
 
@@ -112,7 +111,13 @@ void WW8FFData::Write(SvStream * pDataStrm)
     
     aData[4] = mnType | (mnResult << 2);
     
+#ifdef USE_JAVA 
+    // Fix bug 3594 by never setting the help bit checkboxes as all versions
+    // of OpenOffice.org do not mask the help bit when importing files
+    if (mbOwnHelp && mnType != 1 && msHelp.getLength() )
+#else	// USE_JAVA 
     if (mbOwnHelp)
+#endif	// USE_JAVA 
         aData[4] |= (1 << 7);
         
     aData[5] = (mnTextType << 3);
