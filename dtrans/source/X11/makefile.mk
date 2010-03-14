735cda264db3eb8d0f3f6b2ca63d1f096cb1d6ea
pluby <>
2010-03-14 15:48:15 +0000

    Correct typo added in last commit

--- dtrans/source/X11/makefile.mk
+++ dtrans/source/X11/makefile.mk
@@ -88,7 +88,7 @@ SHL1VERSIONMAP=exports.map
 SHL1DEF=$(MISC)$/$(SHL1TARGET).def
 DEF1NAME=$(SHL1TARGET)
 
-.ENDIIF		# "$(GUIBASE)"=="aqua" || "$(GUIBASE)"=="java"
+.ENDIF		# "$(GUIBASE)"=="aqua" || "$(GUIBASE)"=="java"
 
 # --- Targets ------------------------------------------------------
 
