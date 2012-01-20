62200874498bb88a42c8f38bf39fe79f27cc3434
pluby <>
2012-01-20 20:13:45 +0000

    Reenabled the cancel button when download bytes are receive as it may have been cancelled by clicking on a link

--- extensions/source/update/check/updatewebview_cocoa.mm
+++ extensions/source/update/check/updatewebview_cocoa.mm
@@ -896,6 +896,10 @@ static NSMutableDictionary *pRetryDownloadURLs = nil;
 	std::map< NSURLDownload*, UpdateDownloadData* >::iterator it = aDownloadDataMap.find(download);
 	if(it!=aDownloadDataMap.end())
 	{
+		// Reenabled the cancel button as it may have been cancelled by
+		// clicking on a link
+		[mpcancelButton setEnabled:YES];
+
 		[it->second addBytesReceived:length];
 
 		unsigned long nBytesReceived=[it->second bytesReceived];
