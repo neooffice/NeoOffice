abd0cd2cffc7ad5d4a570f4742c59185aa6b1997
pluby <>
2012-06-15 11:15:01 +0000

    Revert maximum Mac OS X version to 10.8 as we will build NeoOffice 3.2.1 patches off a separate branch

--- desktop/source/app/main_java.m
+++ desktop/source/app/main_java.m
@@ -41,7 +41,7 @@
 #import <Cocoa/Cocoa.h>
 
 #define MIN_MACOSX_MAJOR_VERSION 5
-#define MAX_MACOSX_MAJOR_VERSION 7
+#define MAX_MACOSX_MAJOR_VERSION 8
 #define TMPDIR "/var/tmp"
 
 typedef OSErr Gestalt_Type( OSType selector, long *response );
