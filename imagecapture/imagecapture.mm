437943fbe38feed16e5e8714e1ebf4645d8483ea
pluby <>
2012-09-17 00:14:20 +0000

    Rerun modal loop as Mac OS X 10.8 will throw KVO-compliance exceptions when the selected device or the number of devices changes

--- imagecapture/imagecapture.mm
+++ imagecapture/imagecapture.mm
@@ -576,20 +576,24 @@ extern "C" void * SAL_CALL component_getFactory(const sal_Char * pImplName, XMul
 									[mpPanel setDelegate:self];
 									mpDeviceBrowserView.delegate = self;
 									mbPanelIsInModal = true;
-									@try
+									while ( mbPanelIsInModal )
 									{
-										[pApp runModalForWindow:mpPanel];
+										@try
+										{
+											[pApp runModalForWindow:mpPanel];
+											mbPanelIsInModal = false;
+										}
+										@catch ( NSException *pExc )
+										{
+											// Rerun modal loop as Mac OS X
+											// 10.8 will throw KVO-compliance
+											// exceptions when the selected
+											// device or the number of devices
+											// changes
+											if ( pExc )
+												CFShow( pExc );
+										}
 									}
-									@catch ( NSException *pExc )
-									{
-										// Close the window after catching an
-										// exception as the device browser view
-										// will likely crash
-										[mpPanel close];
-										if ( pExc )
-											CFShow( pExc );
-									}
-									mbPanelIsInModal = false;
 								}
 							}
 						}
