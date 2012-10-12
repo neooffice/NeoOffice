40732f05f6ed09ae4065da230a5a56bd8afa1ab3
pluby <>
2012-10-12 02:23:42 +0000

    Add override functions for functions that are affected by sandbox file restrictions

--- sal/osl/unx/system_cocoa.mm
+++ sal/osl/unx/system_cocoa.mm
@@ -39,6 +39,173 @@
 
 #include "system.h"
 
+typedef int chmod_Type(const char *path, mode_t mode);
+typedef int link_Type(const char *path1, const char *path2);
+typedef int mkdir_Type(const char *path, mode_t mode);
+typedef int open_Type(const char *path, int oflag, ...);
+typedef DIR *opendir_Type(const char *dirname);
+typedef int rename_Type(const char *oldpath, const char *newpath);
+typedef int rmdir_Type(const char *path);
+typedef int symlink_Type(const char *path1, const char *path2);
+typedef int unlink_Type(const char *path);
+
+static chmod_Type *pChmod = NULL;
+static link_Type *pLink = NULL;
+static mkdir_Type *pMkdir = NULL;
+static open_Type *pOpen = NULL;
+static opendir_Type *pOpendir = NULL;
+static rename_Type *pRename = NULL;
+static rmdir_Type *pRmdir = NULL;
+static symlink_Type *pSymlink = NULL;
+static unlink_Type *pUnlink = NULL;
+
+extern "C" int chmod(const char *path, mode_t mode)
+{
+	int nRet = -1;
+
+	if ( !pChmod )
+		pChmod = (chmod_Type *)dlsym( RTLD_NEXT, "chmod");
+
+	if ( pChmod )
+		nRet = pChmod( path, mode );
+	else
+		errno = EFAULT;
+
+	return nRet;
+}
+
+extern "C" int link(const char *path1, const char *path2)
+{
+	int nRet = -1;
+
+	if ( !pLink )
+		pLink = (link_Type *)dlsym( RTLD_NEXT, "link");
+
+	if ( pLink )
+		nRet = pLink( path1, path2 );
+	else
+		errno = EFAULT;
+
+	return nRet;
+}
+
+extern "C" int mkdir(const char *path, mode_t mode)
+{
+	int nRet = -1;
+
+	if ( !pMkdir )
+		pMkdir = (mkdir_Type *)dlsym( RTLD_NEXT, "mkdir");
+
+	if ( pMkdir )
+		nRet = pMkdir( path, mode );
+	else
+		errno = EFAULT;
+
+	return nRet;
+}
+
+extern "C" int open(const char *path, int oflag, ...)
+{
+	int nRet = -1;
+
+	if ( !pOpen )
+		pOpen = (open_Type *)dlsym( RTLD_NEXT, "open" );
+
+	if ( pOpen )
+	{
+		if ( oflag & O_CREAT )
+		{
+			va_list argp;
+			va_start( argp, oflag );
+			nRet = pOpen( path, oflag, (mode_t)va_arg( argp, int ) );
+			va_end( argp );
+		}
+		else
+		{
+			nRet = pOpen( path, oflag );
+		}
+	}
+	else
+	{
+		errno = EFAULT;
+	}
+
+	return nRet;
+}
+
+extern "C" DIR *opendir(const char *dirname)
+{
+	DIR *pRet = NULL;
+
+	if ( !pOpendir )
+		pOpendir = (opendir_Type *)dlsym( RTLD_NEXT, "opendir");
+
+	if ( pOpendir )
+		pRet = pOpendir( dirname );
+
+	return pRet;
+}
+
+extern "C" int rename(const char *oldpath, const char *newpath)
+{
+	int nRet = -1;
+
+	if ( !pRename )
+		pRename = (rename_Type *)dlsym( RTLD_NEXT, "rename");
+
+	if ( pRename )
+		nRet = pRename( oldpath, newpath );
+	else
+		errno = EFAULT;
+
+	return nRet;
+}
+
+extern "C" int rmdir(const char *path)
+{
+	int nRet = -1;
+
+	if ( !pRmdir )
+		pRmdir = (rmdir_Type *)dlsym( RTLD_NEXT, "rmdir");
+
+	if ( pRmdir )
+		nRet = pRmdir( path );
+	else
+		errno = EFAULT;
+
+	return nRet;
+}
+
+extern "C" int symlink(const char *path1, const char *path2)
+{
+	int nRet = -1;
+
+	if ( !pSymlink )
+		pSymlink = (symlink_Type *)dlsym( RTLD_NEXT, "symlink");
+
+	if ( pSymlink )
+		nRet = pSymlink( path1, path2 );
+	else
+		errno = EFAULT;
+
+	return nRet;
+}
+
+extern "C" int unlink(const char *path)
+{
+	int nRet = -1;
+
+	if ( !pUnlink )
+		pUnlink = (unlink_Type *)dlsym( RTLD_NEXT, "unlink");
+
+	if ( pUnlink )
+		nRet = pUnlink( path );
+	else
+		errno = EFAULT;
+
+	return nRet;
+}
+
 sal_Bool macxp_getNSHomeDirectory(char *path, int buflen)
 {
 	sal_Bool bRet = sal_False;
