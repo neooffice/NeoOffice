cd3799992056aaae28c5be9bf9f53a89d595e90d
pluby <>
2013-02-20 19:51:33 +0000

    Fix bug reported in the following NeoOffice forum topic by merging fix for .docx tables with fixed cell widths bar code from LibreOffice 4.0.0.3 source code licensed under the Mozilla Public License, v. 2.0 (MPL). The LibreOffice 4.0.0.3 source code has been modified to be distributed under the GPL under modification term 3.3 of the MPL:
    http://trinity.neooffice.org/modules.php?name=Forums&file=viewtopic&t=8546

--- writerfilter/inc/resourcemodel/TableManager.hxx
+++ writerfilter/inc/resourcemodel/TableManager.hxx
@@ -1,28 +1,27 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*************************************************************************
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
  * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
- * OpenOffice.org - a multi-platform office productivity suite
+ * This file is part of NeoOffice.
  *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
+ * NeoOffice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
  * only, as published by the Free Software Foundation.
  *
- * OpenOffice.org is distributed in the hope that it will be useful,
+ * NeoOffice is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
+ * GNU General Public License version 3 for more details
  * (a copy is included in the LICENSE file that accompanied this code).
  *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
+ * You should have received a copy of the GNU General Public License
+ * version 3 along with NeoOffice.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.txt>
+ * for a copy of the GPLv3 License.
+ *
+ * Modified February 2013 by Patrick Luby. NeoOffice is distributed under
+ * GPL only under modification term 2 of the LGPL.
  *
  ************************************************************************/
 
@@ -217,7 +216,11 @@ class TableManager
         
         void resetRowProps()
         {
+#ifdef NO_LIBO_4_0_TABLE_FIXES
             mpCellProps.reset();
+#else	// NO_LIBO_4_0_TABLE_FIXES
+            mpRowProps.reset();
+#endif	// NO_LIBO_4_0_TABLE_FIXES
         }
         
         void setRowProps(PropertiesPointer pProps)
@@ -467,6 +470,13 @@ protected:
        Return current table depth.
      */
     sal_uInt32 getTableDepthNew() { return mnTableDepthNew; }
+
+#ifndef NO_LIBO_4_0_TABLE_FIXES
+    /**
+       Return the current table difference, i.e. 1 if we are in the first cell of a new table, etc.
+     */
+    sal_uInt32 getTableDepthDifference() { return mnTableDepthNew - mnTableDepth; }
+#endif	// !NO_LIBO_4_0_TABLE_FIXES
 
     /**
        Action to be carried out at the end of the last paragraph of a
--- writerfilter/source/dmapper/DomainMapperTableManager.hxx
+++ writerfilter/source/dmapper/DomainMapperTableManager.hxx
@@ -1,28 +1,27 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*************************************************************************
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
  * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
- * OpenOffice.org - a multi-platform office productivity suite
+ * This file is part of NeoOffice.
  *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
+ * NeoOffice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
  * only, as published by the Free Software Foundation.
  *
- * OpenOffice.org is distributed in the hope that it will be useful,
+ * NeoOffice is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
+ * GNU General Public License version 3 for more details
  * (a copy is included in the LICENSE file that accompanied this code).
  *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
+ * You should have received a copy of the GNU General Public License
+ * version 3 along with NeoOffice.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.txt>
+ * for a copy of the GPLv3 License.
+ *
+ * Modified February 2013 by Patrick Luby. NeoOffice is distributed under
+ * GPL only under modification term 2 of the LGPL.
  *
  ************************************************************************/
 #ifndef INCLUDED_DOMAIN_MAPPER_TABLE_MANAGER_HXX
@@ -44,8 +43,16 @@ class DomainMapperTableManager : public DomainMapperTableManager_Base_t
     typedef boost::shared_ptr< std::vector<sal_Int32> > IntVectorPtr;
 
     sal_uInt32      m_nRow;
+#ifdef NO_LIBO_4_0_TABLE_FIXES
     sal_uInt32      m_nCell;
+#else	// NO_LIBO_4_0_TABLE_FIXES
+    ::std::vector< sal_uInt32 > m_nCell;
+#endif	// NO_LIBO_4_0_TABLE_FIXES
     sal_uInt32      m_nGridSpan;
+#ifndef NO_LIBO_4_0_TABLE_FIXES
+    sal_uInt32      m_nGridBefore; ///< number of grid columns in the parent table's table grid which must be skipped before the contents of this table row are added to the parent table
+    sal_uInt32      m_nGridAfter; ///< number of grid columns in the parent table's table grid which shall be left after the last cell in the table row
+#endif	// !NO_LIBO_4_0_TABLE_FIXES
     sal_uInt32      m_nCellBorderIndex; //borders are provided for all cells and need counting
     sal_Int32       m_nHeaderRepeat; //counter of repeated headers - if == -1 then the repeating stops
     sal_Int32       m_nTableWidth; //might be set directly or has to be calculated from the column positions
@@ -55,6 +62,12 @@ class DomainMapperTableManager : public DomainMapperTableManager_Base_t
 
     ::std::vector< IntVectorPtr >  m_aTableGrid;
     ::std::vector< IntVectorPtr >  m_aGridSpans;
+#ifndef NO_LIBO_4_0_TABLE_FIXES
+    /// If this is true, then we pushed a width before the next level started, and that should be carried over when starting the next level.
+    bool            m_bPushCurrentWidth;
+    /// Individual table cell width values, used only in case the number of cells doesn't match the table grid.
+    ::std::vector< IntVectorPtr >  m_aCellWidths;
+#endif	// !NO_LIBO_4_0_TABLE_FIXES
     
     TablePropertiesHandler   *m_pTablePropsHandler;
     PropertyMapPtr            m_pStyleProps;
@@ -80,6 +93,9 @@ public:
 
     IntVectorPtr getCurrentGrid( );
     IntVectorPtr getCurrentSpans( );
+#ifndef NO_LIBO_4_0_TABLE_FIXES
+    IntVectorPtr getCurrentCellWidths( );
+#endif	// !NO_LIBO_4_0_TABLE_FIXES
 
     const ::rtl::OUString& getTableStyleName() const { return m_sTableStyleName; }
     /// copy the text properties of the table style and its parent into pContext
