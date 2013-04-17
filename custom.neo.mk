bc6b1579cb000180943f23e7bc1e093b2fbb1c1c
pluby <>
2013-04-17 05:47:34 +0000

    Remove underlines from non-space product name and use Mac App Store certificates

--- custom.neo.mk
+++ custom.neo.mk
@@ -1,6 +1,6 @@
 # Important: Note that there may be escape characters in the PRODUCT_NAME for
 # the UTF-8 trademark symbol. Don't replace these with "\x##" literal strings!
 PRODUCT_NAME=NeoOffice Secure Edition
-PRODUCT_DIR_NAME=NeoOffice_Secure_Edition
+PRODUCT_DIR_NAME=NeoOfficeSecureEdition
 PRODUCT_TRADEMARKED_NAME=NeoOffice® Secure Edition
 PRODUCT_TRADEMARKED_NAME_RTF=NeoOffice\\\'a8 Secure Edition
