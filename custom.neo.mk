865b773cd543837a1fc9d61dfda7bd4925278ad0
pluby <>
2013-06-29 21:58:52 +0000

    Revert PRODUCT_DIR_NAME to NeoOfficeSecureEdition since PRODUCT_DIR_NAME is the Mac App Store product ID

--- custom.neo.mk
+++ custom.neo.mk
@@ -1,6 +1,6 @@
 # Important: Note that there may be escape characters in the PRODUCT_NAME for
 # the UTF-8 trademark symbol. Don't replace these with "\x##" literal strings!
 PRODUCT_NAME=NeoOffice
-PRODUCT_DIR_NAME=NeoOffice
+PRODUCT_DIR_NAME=NeoOfficeSecureEdition
 PRODUCT_TRADEMARKED_NAME=NeoOffice®
 PRODUCT_TRADEMARKED_NAME_RTF=NeoOffice\\\'a8
