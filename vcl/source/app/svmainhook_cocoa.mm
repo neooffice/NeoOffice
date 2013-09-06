b131eba944e7a868ae6a61cd1b96a2a920a50f33
pluby <>
2013-09-06 03:44:11 +0000

    Check if the info dictionary has been changed

--- vcl/source/app/svmainhook_cocoa.mm
+++ vcl/source/app/svmainhook_cocoa.mm
@@ -33,6 +33,9 @@
  *
  ************************************************************************/
 
+
+#include <rtl/digest.h>
+
 #include <premac.h>
 #import <objc/objc-runtime.h>
 #import <Cocoa/Cocoa.h>
@@ -54,6 +57,42 @@ void NSApplication_run()
 	if ( pApp )
 	{
 		NSBundle *pBundle = [NSBundle mainBundle];
+
+		// Check if the info dictionary has been changed
+		MacOSBOOL bBundleOK = NO;
+		if ( pBundle )
+		{
+			NSDictionary *pInfoDict = [pBundle infoDictionary];
+			if ( pInfoDict )
+			{
+				NSString *pBundleIdentifier = [pInfoDict objectForKey:@"CFBundleIdentifier"];
+				NSString *pBundleName = [pInfoDict objectForKey:@"CFBundleName"];
+				if ( pBundleIdentifier && pBundleName )
+				{
+					NSString *pKey = [pBundleIdentifier stringByAppendingFormat:@"/%@", pBundleName];
+					const char *pKeyString = [pKey UTF8String];
+					if ( pKeyString )
+					{
+						NSString *pKeyMD5 = nil;
+						sal_uInt8 aBuf[ RTL_DIGEST_LENGTH_MD5 ];
+
+						if ( rtl_digest_MD5( pKeyString, strlen( pKeyString ), aBuf, sizeof( aBuf ) ) == rtl_Digest_E_None )
+							pKeyMD5 = [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", aBuf[0], aBuf[1], aBuf[2], aBuf[3], aBuf[4], aBuf[5], aBuf[6], aBuf[7], aBuf[8], aBuf[9], aBuf[10], aBuf[11], aBuf[12], aBuf[13], aBuf[14], aBuf[15]];
+
+						if ( pKeyMD5 )
+						{
+							NSString *pProductMD5 = [NSString stringWithUTF8String:PRODUCT_MD5];
+							if ( pProductMD5 && [pProductMD5 length] && [pProductMD5 caseInsensitiveCompare:pKeyMD5] == NSOrderedSame )
+								bBundleOK = YES;
+						}
+					}
+				}
+			}
+		}
+
+		if ( !bBundleOK )
+			_exit( 1 );
+
 		if ( pBundle && [pBundle respondsToSelector:@selector(loadNibNamed:owner:topLevelObjects:)] )
  			[pBundle loadNibNamed:@"MainMenu" owner:pApp topLevelObjects:nil];
 		else if ( class_getClassMethod( [NSBundle class], @selector(loadNibNamed:owner:) ) )
