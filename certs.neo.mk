9bfb6bed43e235d3574b17bac953b68ffe5ae6c2
pluby <>
2014-04-11 05:54:06 +0000

    Disable iCloud support by default

--- certs.neo.mk
+++ certs.neo.mk
@@ -1,4 +1,5 @@
 CERTAPPIDENTITY=3rd Party Mac Developer Application: Planamesa Inc (8V953UDD5Q)
 CERTPKGIDENTITY=3rd Party Mac Developer Installer: Planamesa Inc (8V953UDD5Q)
 CERTSANDBOXTEAMIDENTIFIER=8V953UDD5Q
-CERTICLOUDENABLED=TRUE
+# Uncomment when doing iCloud development
+#CERTICLOUDENABLED=TRUE
