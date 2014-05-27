d37d0ea3b271caf94476b950d2cbdd99342c6923
pluby <>
2014-05-27 03:52:12 +0000

    Handle case where SQLWCHAR is 32 bits instead of only 16 bits

--- connectivity/source/drivers/odbcbase/makefile.mk
+++ connectivity/source/drivers/odbcbase/makefile.mk
@@ -1,21 +1,30 @@
 #**************************************************************
 #  
 #  Licensed to the Apache Software Foundation (ASF) under one
-#  or more contributor license agreements.  See the NOTICE file
-#  distributed with this work for additional information
-#  regarding copyright ownership.  The ASF licenses this file
-#  to you under the Apache License, Version 2.0 (the
-#  "License"); you may not use this file except in compliance
-#  with the License.  You may obtain a copy of the License at
+#  or more contributor license agreements.
 #  
-#    http://www.apache.org/licenses/LICENSE-2.0
+#  $RCSfile$
+#  $Revision$
 #  
-#  Unless required by applicable law or agreed to in writing,
-#  software distributed under the License is distributed on an
-#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-#  KIND, either express or implied.  See the License for the
-#  specific language governing permissions and limitations
-#  under the License.
+#  This file is part of NeoOffice.
+#  
+#  NeoOffice is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 3
+#  only, as published by the Free Software Foundation.
+#  
+#  NeoOffice is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License version 3 for more details
+#  (a copy is included in the LICENSE file that accompanied this code).
+#  
+#  You should have received a copy of the GNU General Public License
+#  version 3 along with NeoOffice.  If not, see
+#  <http://www.gnu.org/licenses/gpl-3.0.txt>
+#  for a copy of the GPLv3 License.
+#  
+#  Modified May 2014 by Patrick Luby. NeoOffice is distributed under
+#  GPL only under Section 4 of the Apache License v2.0.
 #  
 #**************************************************************
 
@@ -64,6 +73,10 @@ SHL1STDLIBS=\
 	$(CPPULIB)					\
 	$(VOSLIB)					\
 	$(SALLIB)
+
+.IF "$(GUIBASE)" == "java"
+SHL1STDLIBS+=-framework CoreFoundation
+.ENDIF		# "$(GUIBASE)" == "java"
 
 SHL1DEPN=
 SHL1IMPLIB=	i$(ODBC2_TARGET)
