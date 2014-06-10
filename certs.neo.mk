7edb3be89e1f77ffe53449c4719ca8c25e16226e
pluby <>
2014-06-10 04:02:40 +0000

    Update version to 2014

--- certs.neo.mk
+++ certs.neo.mk
@@ -1,7 +1,7 @@
-CERTAPPIDENTITY=Developer ID Application: Planamesa Inc
-CERTPKGIDENTITY=Developer ID Installer: Planamesa Inc
-CERTAPPIDENTITY2=3rd Party Mac Developer Application: Planamesa Inc (8V953UDD5Q)
-CERTPKGIDENTITY2=3rd Party Mac Developer Installer: Planamesa Inc (8V953UDD5Q)
+CERTAPPIDENTITY=3rd Party Mac Developer Application: Planamesa Inc (8V953UDD5Q)
+CERTPKGIDENTITY=3rd Party Mac Developer Installer: Planamesa Inc (8V953UDD5Q)
+CERTAPPIDENTITY2=Developer ID Application: Planamesa Inc
+CERTPKGIDENTITY2=Developer ID Installer: Planamesa Inc
 CERTSANDBOXTEAMIDENTIFIER=8V953UDD5Q
 # Uncomment when doing iCloud development
 #CERTICLOUDENABLED=TRUE
