78cb97c19a6ce2d33e4af9117c8b8219d725d926
pluby <>
2014-11-29 18:54:50 +0000

    Update license header

--- sc/source/filter/xlsx/makefile.mk
+++ sc/source/filter/xlsx/makefile.mk
@@ -1,31 +1,30 @@
 #*************************************************************************
 #
-# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-#
 # Copyright 2008 by Sun Microsystems, Inc.
-#
-# OpenOffice.org - a multi-platform office productivity suite
 #
 # $RCSfile$
 #
 # $Revision$
 #
-# This file is part of OpenOffice.org.
+# This file is part of NeoOffice.
 #
-# OpenOffice.org is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License version 3
+# NeoOffice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3
 # only, as published by the Free Software Foundation.
 #
-# OpenOffice.org is distributed in the hope that it will be useful,
+# NeoOffice is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License version 3 for more details
+# GNU General Public License version 3 for more details
 # (a copy is included in the LICENSE file that accompanied this code).
 #
-# You should have received a copy of the GNU Lesser General Public License
-# version 3 along with OpenOffice.org.  If not, see
-# <http://www.openoffice.org/license.html>
-# for a copy of the LGPLv3 License.
+# You should have received a copy of the GNU General Public License
+# version 3 along with NeoOffice.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.txt>
+# for a copy of the GPLv3 License.
+#
+# Modified November 2014 by Patrick Luby. NeoOffice is distributed under
+# GPL only under modification term 2 of the LGPL.
 #
 #*************************************************************************
 
