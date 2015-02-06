79c645cf1949752e7172d733eec950ec4213a281
pluby <>
2015-02-06 02:20:18 +0000

    Merge changes from the HEAD branch

--- desktop/source/app/main_java.mm
+++ desktop/source/app/main_java.mm
@@ -202,6 +202,14 @@ int java_main( int argc, char **argv )
 	pTmpEnv = [NSString stringWithFormat:@"TEMP=%@", pTmpDir];
 	putenv( strdup( [pTmpEnv UTF8String] ) );
 
+
+	// NSTemporaryDirectory() may reset the above temporary directory
+	// environmental variables so invoke NSTemporaryDirectory() now and set
+	// a custom environmental variable to be used by the sal/osl/unx sources
+	NSTemporaryDirectory();
+	pTmpEnv = [NSString stringWithFormat:@"SAL_TMPDIR=%@", pTmpDir];
+	putenv( strdup( [pTmpEnv UTF8String] ) );
+
 	// Put mozilla NSS files somewhere other than the default of "/"
 	pTmpEnv = [NSString stringWithFormat:@"MOZILLA_CERTIFICATE_FOLDER=%@", pTmpDir];
 	putenv( strdup( [pTmpEnv UTF8String] ) );
