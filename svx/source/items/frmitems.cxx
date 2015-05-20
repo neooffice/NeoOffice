d66199bb8cc265dc49eaf049533fb8770934f546
pluby <>
2015-05-20 03:57:19 +0000

    Merge and adapt new unobrushitemhelper.* files from LibreOffice 4.4.2.2 svx module's source code licensed under the Mozilla Public License, v. 2.0 (MPL). The LibreOffice 4.4.2.2 source code has been modified to be distributed under the GPL under modification term 3.3 of the MPL.

--- svx/source/items/frmitems.cxx
+++ svx/source/items/frmitems.cxx
@@ -4207,6 +4207,27 @@ void SvxBrushItem::PurgeMedium() const
 #endif
 }
 
+#if SUPD == 310
+
+//UUUU
+sal_Int8 SvxBrushItem::getGraphicTransparency() const
+{
+    return pImpl->nGraphicTransparency;
+}
+
+// -----------------------------------------------------------------------
+//UUUU
+void SvxBrushItem::setGraphicTransparency(sal_Int8 nNew)
+{
+    if(nNew != pImpl->nGraphicTransparency)
+    {
+        pImpl->nGraphicTransparency = nNew;
+        ApplyGraphicTransparency_Impl();
+    }
+}
+
+#endif	// SUPD == 310
+
 // -----------------------------------------------------------------------
 
 const GraphicObject* SvxBrushItem::GetGraphicObject( SfxObjectShell* pSh ) const
