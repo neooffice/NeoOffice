837221a34224541068aac338080052b434e735a7
pluby <>
2015-06-30 00:36:55 +0000

    Add back custom files from NeoOffice-3_3_branch

---
+++ jvmfwk/plugins/sunmajor/pluginlib/otherjre.cxx
@@ -0,0 +1,227 @@
+/*************************************************************************
+ *
+ * Copyright 2008 by Sun Microsystems, Inc.
+ *
+ * $RCSfile$
+ * $Revision$
+ *
+ * This file is part of NeoOffice.
+ *
+ * NeoOffice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * NeoOffice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 3 along with NeoOffice.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.txt>
+ * for a copy of the GPLv3 License.
+ *
+ * Modified January 2013 by Patrick Luby. NeoOffice is distributed under
+ * GPL only under modification term 2 of the LGPL.
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+#include "precompiled_jvmfwk.hxx"
+
+#include "osl/thread.h"
+#include "otherjre.hxx"
+
+#ifdef MACOSX
+
+#ifndef USE_JAVA
+#include <dlfcn.h>
+#endif	// !USE_JAVA
+#include "sunversion.hxx"
+#include "diagnostics.h"
+
+#ifndef USE_JAVA
+
+#include <premac.h>
+#include <CoreServices/CoreServices.h>
+#include <postmac.h>
+
+#define OUSTR(x) ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM(x) )
+
+typedef OSErr Gestalt_Type( OSType selector, long *response );
+
+#endif	// !USE_JAVA
+
+#endif	// MACOSX
+
+using namespace rtl;
+using namespace std;
+
+
+namespace jfw_plugin
+{
+
+Reference<VendorBase> OtherInfo::createInstance()
+{
+    return new OtherInfo;
+}
+
+
+char const* const* OtherInfo::getJavaExePaths(int * size)
+{
+    static char const * ar[] = {
+#if defined(WNT) || defined(OS2)
+        "bin/java.exe",
+        "jre/bin/java.exe"
+#elif UNX
+        "bin/java",
+        "jre/bin/java"
+#endif
+    };
+        *size = sizeof (ar) / sizeof (char*);
+    return ar;
+}
+
+char const* const* OtherInfo::getRuntimePaths(int * size)
+{
+    static char const* ar[]= {
+#if defined(WNT) || defined(OS2)
+        "/bin/client/jvm.dll",
+        "/bin/hotspot/jvm.dll",
+        "/bin/classic/jvm.dll",
+	"/bin/jrockit/jvm.dll"
+#elif UNX
+#ifdef MACOSX
+        // Fix bug 1257 by explicitly loading the JVM instead of loading the
+        // shared JavaVM library
+        "/../Libraries/libjvm.dylib"
+#else
+        "/lib/" JFW_PLUGIN_ARCH "/client/libjvm.so", // for Blackdown PPC
+        "/lib/" JFW_PLUGIN_ARCH "/server/libjvm.so", // for Blackdown AMD64
+        "/lib/" JFW_PLUGIN_ARCH "/classic/libjvm.so", // for Blackdown PPC
+        "/lib/" JFW_PLUGIN_ARCH "/jrockit/libjvm.so", // for Java of BEA Systems
+        "/bin/classic/libjvm.so", // fallback for older for IBM Java
+        "/jre/bin/classic/libjvm.so" // fallback for older for IBM Java
+#endif
+#endif
+
+    };
+    *size = sizeof(ar) / sizeof (char*);
+    return ar;
+}
+
+char const* const* OtherInfo::getLibraryPaths(int* size)
+{
+
+#ifdef UNX        
+    static char const * ar[] = {
+#ifdef MACOSX
+        "/../Libraries",
+        "/lib"
+#else
+        "/bin",
+        "/jre/bin",
+        "/bin/classic",
+        "/jre/bin/classic",
+        "/lib/" JFW_PLUGIN_ARCH "/client",
+        "/lib/" JFW_PLUGIN_ARCH "/server",
+        "/lib/" JFW_PLUGIN_ARCH "/classic",
+        "/lib/" JFW_PLUGIN_ARCH "/jrockit",
+        "/lib/" JFW_PLUGIN_ARCH "/native_threads",
+        "/lib/" JFW_PLUGIN_ARCH
+#endif
+    };
+
+    *size = sizeof(ar) / sizeof (char*);
+    return ar;
+#else
+    size = 0;
+    return NULL;
+#endif
+}
+
+#ifdef MACOSX
+int OtherInfo::compareVersions(const rtl::OUString& sSecond) const
+#else	// MACOSX
+int OtherInfo::compareVersions(const rtl::OUString& /*sSecond*/) const
+#endif	// MACOSX
+{
+#ifdef MACOSX
+    OUString sFirst = getVersion();
+      
+    SunVersion version1(sFirst);
+    JFW_ENSURE(version1, OUSTR("[Java framework] sunjavaplugin"SAL_DLLEXTENSION
+                               " does not know the version: ")
+               + sFirst + OUSTR(" as valid for a SUN JRE."));
+#ifdef USE_JAVA
+    // Only run Java 1.6.x or later
+    if ( version1 < SunVersion( ::rtl::OUString::createFromAscii( "1.6.0" ) ) )
+        return -1;
+#else	// USE_JAVA
+    // If we are running Leopard, don't allow loading of any JVM earlier than
+    // Java 1.5.0
+    static bool initializedOnce = false;
+    static bool isLaterThanLeopard = false;
+    static bool isLeopard = false;
+    if ( ! initializedOnce )
+    {
+        void *pLib = dlopen( NULL, RTLD_LAZY | RTLD_LOCAL );
+        if ( pLib )
+        {
+            Gestalt_Type *pGestalt = (Gestalt_Type *)dlsym( pLib, "Gestalt" );
+            if ( pGestalt )
+            {
+                SInt32 res = 0;
+                pGestalt( gestaltSystemVersionMajor, &res );
+                if ( res == 10 )
+                {
+                    res = 0;
+                    pGestalt( gestaltSystemVersionMinor, &res );
+                    if ( res > 5 )
+                        isLaterThanLeopard = true;
+                    else if ( res ==  5 )
+                        isLeopard = true;
+                }
+                else
+                {
+                    isLaterThanLeopard = true;
+                }
+            }
+
+            dlclose( pLib );
+        }
+
+        initializedOnce = true;
+    }
+
+    // Only run Java 1.5.x on Leopard as Java 1.4.x is crashy and Java 1.6.x
+	// will hang on anything before Snow Leopard
+    if ( isLaterThanLeopard && version1 < SunVersion( ::rtl::OUString::createFromAscii( "1.6.0" ) ) )
+        return -1;
+    else if ( !isLaterThanLeopard && version1 > SunVersion( ::rtl::OUString::createFromAscii( "1.5.999" ) ) )
+        return -1;
+    else if ( isLeopard && version1 < SunVersion( ::rtl::OUString::createFromAscii( "1.5.0" ) ) )
+        return -1;
+#endif	// USE_JAVA
+
+    SunVersion version2(sSecond);
+    if ( ! version2)
+        throw MalformedVersionException(); 
+ 
+    if(version1 == version2)
+        return 0;
+    if(version1 > version2)
+        return 1;
+    else
+        return -1;
+#else	// MACOSX
+    //Need to provide an own algorithm for comparing version. 
+    //Because this function returns always 0, which means the version of
+    //this JRE and the provided version "sSecond" are equal, one cannot put
+    //any excludeVersion entries in the javavendors.xml file.
+    return 0;
+#endif	// MACOSX
+}
+
+}
