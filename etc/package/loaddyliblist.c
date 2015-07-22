ca8b2265a23eb00df44c665255a6f28725116cd0
pluby <>
2015-07-22 00:33:55 +0000

    Replace getline() with fgets() so that we don't need to depend on gcc

--- etc/package/loaddyliblist.c
+++ etc/package/loaddyliblist.c
@@ -1,18 +1,20 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/syslimits.h>
 
 int main( int argc, char **argv )
 {
 	int ret = 0;
 
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read = 0;
-	while ( ( read = getline( &line, &len, stdin ) ) >= 0 )
+	char line[ PATH_MAX ];
+	while ( fgets( line, sizeof( line ), stdin ) )
 	{
-		if ( read && line[read - 1] == '\n' )
-			line[read - 1] = '\0';
+		size_t len = strlen( line );
+		if ( len && line[ len - 1 ] == '\n' )
+			line[ len - 1 ] = '\0';
+
 		void *lib = dlopen( line, RTLD_NOW | RTLD_GLOBAL );
 		if ( lib )
 		{
@@ -24,9 +26,6 @@ int main( int argc, char **argv )
 			ret = 1;
 		}
 	}
-
-	if ( line )
-		free( line );
 
 	exit( ret );
 }
