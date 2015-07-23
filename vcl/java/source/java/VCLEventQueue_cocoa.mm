2d10674ebccaa60db4d46410563cf2bbe776dd69
pluby <>
2015-07-23 19:32:36 +0000

    Merge changes from the NeoOffice-2014_branch branch

--- vcl/java/source/java/VCLEventQueue_cocoa.mm
+++ vcl/java/source/java/VCLEventQueue_cocoa.mm
@@ -2814,8 +2814,9 @@ static CFDataRef aRTFSelection = nil;
 	{
 		// Fix hanging when opening a new window in full screen mode while
 		// running on OS X 10.11 by releasing the application mutex if we are
-		// waiting until done
-		ULONG nCount = bWait ? Application::ReleaseSolarMutex() : 0;
+		// waiting until done. If no application mutex exists, ignore mutex as
+		// we are likely to crash.
+		ULONG nCount = ( bWait && !Application::IsShutDown() && ImplGetSVData() && ImplGetSVData()->mpDefInst ? Application::ReleaseSolarMutex() : 0 );
 
 		@try
 		{
@@ -2825,7 +2826,10 @@ static CFDataRef aRTFSelection = nil;
 		{
 		}
 
-		Application::AcquireSolarMutex( nCount );
+		// If no application mutex exists, ignore mutex as we are likely to
+		// crash
+		if ( !Application::IsShutDown() && ImplGetSVData() && ImplGetSVData()->mpDefInst )
+			Application::AcquireSolarMutex( nCount );
 	}
 }
 
