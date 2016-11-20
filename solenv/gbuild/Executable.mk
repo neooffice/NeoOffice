47d3dece11cc1b6f251df182ac11757cbe07b0d2
pluby <>
2016-11-20 01:18:48 +0000

    Remove softlinks before touching them to avoid changing files in the LibreOffice build's workdir

---
+++ solenv/gbuild/Executable.mk
@@ -0,0 +1,167 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This file incorporates work covered by the following license notice:
+#
+#   Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements. See the NOTICE file distributed
+#   with this work for additional information regarding copyright
+#   ownership. The ASF licenses this file to you under the Apache
+#   License, Version 2.0 (the "License"); you may not use this file
+#   except in compliance with the License. You may obtain a copy of
+#   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+#
+#   Modified November 2016 by Patrick Luby. NeoOffice is only distributed
+#   under the GNU General Public License, Version 3 as allowed by Section 3.3
+#   of the Mozilla Public License, v. 2.0.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Executable class
+
+# defined by platform
+#  gb_Executable_Executable_platform
+
+gb_Executable_LAYER_DIRS := \
+	UREBIN:$(INSTROOT)/$(LIBO_URE_BIN_FOLDER) \
+	OOO:$(INSTROOT)/$(LIBO_BIN_FOLDER) \
+	SDKBIN:$(INSTDIR)/$(SDKDIRNAME)/bin \
+	NONE:$(gb_Executable_BINDIR) \
+
+gb_Executable_LAYER_DIRS_FOR_BUILD := \
+	UREBIN:$(INSTROOT_FOR_BUILD)/$(LIBO_URE_BIN_FOLDER_FOR_BUILD) \
+	OOO:$(INSTROOT_FOR_BUILD)/$(LIBO_BIN_FOLDER_FOR_BUILD) \
+	SDKBIN:$(INSTDIR_FOR_BUILD)/$(SDKDIRNAME_FOR_BUILD)/bin \
+	NONE:$(gb_Executable_BINDIR_FOR_BUILD) \
+
+$(dir $(call gb_Executable_get_runtime_target,%)).dir :
+	$(if $(wildcard $(dir $@)),,mkdir -p $(dir $@))
+
+$(dir $(call gb_Executable_get_runtime_target,%))%/.dir :
+	$(if $(wildcard $(dir $@)),,mkdir -p $(dir $@))
+
+$(call gb_Executable_get_runtime_target,%) :
+ifeq ($(strip $(PRODUCT_BUILD_TYPE)),java)
+	rm -f $@
+endif	# PRODUCT_BUILD_TYPE == java
+	touch $@
+
+.PHONY : $(call gb_Executable_get_clean_target,%)
+$(call gb_Executable_get_clean_target,%) :
+	$(call gb_Helper_abbreviate_dirs,\
+		rm -f $(call gb_Executable_get_target,$*) \
+			$(call gb_Executable_get_runtime_target,$*) \
+			$(AUXTARGETS))
+
+gb_Executable__get_dir_for_layer = $(patsubst $(1):%,%,$(filter $(1):%,$(call gb_Executable_LAYER_DIRS)))
+gb_Executable__get_dir_for_layer_for_build = $(patsubst $(1):%,%,$(filter $(1):%,$(call gb_Executable_LAYER_DIRS_FOR_BUILD)))
+gb_Executable__get_dir_for_exe = $(call gb_Executable__get_dir_for_layer,$(call gb_Executable_get_layer,$(1)))
+gb_Executable__get_dir_for_exe_for_build = $(call gb_Executable__get_dir_for_layer_for_build,$(call gb_Executable_get_layer,$(1)))
+
+define gb_Executable_Executable
+$(call gb_Postprocess_register_target,AllExecutables,Executable,$(1))
+ifeq (,$$(findstring $(1),$$(gb_Executable_KNOWN)))
+$$(eval $$(call gb_Output_info,Currently known executables: $(sort $(gb_Executable_KNOWN)),ALL))
+$$(eval $$(call gb_Output_error,Executable $(1) must be registered in Repository.mk or RepositoryExternal.mk))
+endif
+$(call gb_Executable__Executable_impl,$(1),$(call gb_Executable_get_linktarget,$(1)))
+
+endef
+
+# call gb_Executable__Executable_impl,exe,linktarget
+define gb_Executable__Executable_impl
+$(call gb_LinkTarget_LinkTarget,$(2),Executable_$(1),$(call gb_Executable_get_layer,$(1)))
+$(call gb_LinkTarget_set_targettype,$(2),Executable)
+$(call gb_LinkTarget_add_libs,$(2),$(gb_STDLIBS))
+$(call gb_Executable_get_runtime_target,$(1)) :| $(dir $(call gb_Executable_get_runtime_target,$(1))).dir
+$(call gb_Executable_get_runtime_target,$(1)) : $(call gb_Executable_get_target_for_build,$(1))
+$(call gb_Executable_get_clean_target,$(1)) : $(call gb_LinkTarget_get_clean_target,$(2))
+$(call gb_Executable_get_clean_target,$(1)) : AUXTARGETS :=
+$(call gb_Executable_Executable_platform,$(1),$(2),$(gb_Executable_BINDIR)/$(1).lib)
+
+$$(eval $$(call gb_Module_register_target,$(call gb_Executable_get_target,$(1)),$(call gb_Executable_get_clean_target,$(1))))
+$(call gb_Helper_make_userfriendly_targets,$(1),Executable)
+
+endef
+
+define gb_Executable_set_targettype_gui
+$(call gb_LinkTarget_get_target,$(call gb_Executable_get_linktarget,$(1))) : TARGETGUI := $(2)
+endef
+
+define gb_Executable_forward_to_Linktarget
+gb_Executable_$(1) = $$(call gb_LinkTarget_$(1),$$(call gb_Executable_get_linktarget,$$(1)),$$(2),$$(3),Executable_$$(1))
+
+endef
+
+$(eval $(foreach method,\
+	add_cobject \
+	add_cobjects \
+	add_cxxobject \
+	add_cxxobjects \
+	add_objcobject \
+	add_objcobjects \
+	add_objcxxobject \
+	add_objcxxobjects \
+	add_grammar \
+	add_grammars \
+	add_scanner \
+	add_scanners \
+	add_exception_objects \
+	add_generated_cobjects \
+	add_generated_cxxobjects \
+	add_generated_exception_objects \
+	add_cflags \
+	add_cxxflags \
+	add_objcflags \
+	add_objcxxflags \
+	add_defs \
+	set_include \
+	add_ldflags \
+	set_ldflags \
+	add_libs \
+	disable_standard_system_libs \
+	use_system_darwin_frameworks \
+	use_system_win32_libs \
+	set_library_path_flags \
+	use_api \
+	use_sdk_api \
+	use_udk_api \
+	use_internal_api \
+	use_internal_bootstrap_api \
+	use_internal_comprehensive_api \
+	use_libraries \
+	use_static_libraries \
+	use_external \
+	use_externals \
+	use_custom_headers \
+	use_package \
+	use_packages \
+	use_unpacked \
+	add_sdi_headers \
+	set_precompiled_header \
+	add_nativeres \
+	set_warnings_not_errors \
+	set_generated_cxx_suffix \
+	disable_compiler_plugins \
+,\
+	$(call gb_Executable_forward_to_Linktarget,$(method))\
+))
+
+# Run-time use
+
+# Add dependencies needed for running the executable
+#
+# gb_Executable_add_runtime_dependencies executable dependencies
+define gb_Executable_add_runtime_dependencies
+$(call gb_Executable_get_runtime_target,$(1)) : $(2)
+
+endef
+
+# vim: set noet sw=4:
---
+++ solenv/gbuild/UnoApiTarget.mk
@@ -0,0 +1,250 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This file incorporates work covered by the following license notice:
+#
+#   Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements. See the NOTICE file distributed
+#   with this work for additional information regarding copyright
+#   ownership. The ASF licenses this file to you under the Apache
+#   License, Version 2.0 (the "License"); you may not use this file
+#   except in compliance with the License. You may obtain a copy of
+#   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+#
+#   Modified November 2016 by Patrick Luby. NeoOffice is only distributed
+#   under the GNU General Public License, Version 3 as allowed by Section 3.3
+#   of the Mozilla Public License, v. 2.0.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# UnoApiTarget
+
+gb_UnoApiTarget_UNOIDLWRITEDEPS := $(call gb_Executable_get_runtime_dependencies,unoidl-write)
+gb_UnoApiTarget_UNOIDLWRITECOMMAND := $(call gb_Executable_get_command,unoidl-write)
+
+gb_UnoApiTarget_UNOIDLCHECKDEPS := $(call gb_Executable_get_runtime_dependencies,unoidl-check)
+gb_UnoApiTarget_UNOIDLCHECKCOMMAND := $(call gb_Executable_get_command,unoidl-check)
+
+define gb_UnoApiTarget__command
+$(call gb_Output_announce,$(2),$(true),UNO,4)
+mkdir -p $(dir $(1)) \
+$(if $(UNOAPI_ENTITIES), \
+	&& RESPONSEFILE=$(call var2file,$(shell $(gb_MKTEMP)),500,$(UNOAPI_ENTITIES))) \
+&& $(gb_UnoApiTarget_UNOIDLWRITECOMMAND) \
+	$(foreach rdb,$(UNOAPI_DEPRDBS),$(call gb_UnoApiTarget_get_target,$(rdb))) \
+	$(SRCDIR)/$(gb_UnoApiTarget_REG_$(2)) $(if $(UNOAPI_ENTITIES),@$${RESPONSEFILE}) $(1) \
+$(if $(UNOAPI_ENTITIES),&& rm -f $${RESPONSEFILE}) \
+$(if $(UNOAPI_REFERENCE), \
+	$(call gb_Output_announce,$(2),$(true),DBc,3) \
+	&& $(gb_UnoApiTarget_UNOIDLCHECKCOMMAND) $(UNOAPI_REFERENCE) -- \
+		$(foreach rdb,$(UNOAPI_DEPRDBS),$(call gb_UnoApiTarget_get_target,$(rdb))) \
+		$(1))
+endef
+
+$(call gb_UnoApiTarget_get_target,%) :
+	$(call gb_UnoApiTarget__command,$@,$*)
+
+.PHONY : $(call gb_UnoApiTarget_get_clean_target,%)
+$(call gb_UnoApiTarget_get_clean_target,%) :
+	$(call gb_Output_announce,$*,$(false),UNO,4)
+	-$(call gb_Helper_abbreviate_dirs,\
+		rm -rf $(call gb_UnoApiTarget_get_target,$*))
+
+define gb_UnoApiTarget_UnoApiTarget
+gb_UnoApiTarget_REG_$(1) := $(2)
+$(call gb_UnoApiTarget_get_target,$(1)) : UNOAPI_ENTITIES :=
+$(call gb_UnoApiTarget_get_target,$(1)) : UNOAPI_REFERENCE :=
+$(call gb_UnoApiTarget_get_target,$(1)) : UNOAPI_DEPRDBS :=
+$(call gb_UnoApiTarget_get_target,$(1)) : $(gb_UnoApiTarget_UNOIDLWRITEDEPS)
+$(call gb_UnoApiTarget_get_target,$(1)) : $(SRCDIR)/$(2) # may be dir, though
+
+endef
+
+define gb_UnoApiTarget_add_idlfile
+$(call gb_UnoApiTarget_get_target,$(1)) : UNOAPI_ENTITIES += $(subst /,.,$(2))$(if $(2),.)$(3)
+$(call gb_UnoApiTarget_get_target,$(1)) : $(SRCDIR)/$(gb_UnoApiTarget_REG_$(1))/$(2)/$(3).idl
+
+endef
+
+define gb_UnoApiTarget_add_idlfiles
+$(foreach idl,$(3),$(call gb_UnoApiTarget_add_idlfile,$(1),$(2),$(idl)))
+
+endef
+
+define gb_UnoApiTarget_set_reference_rdbfile
+$(call gb_UnoApiTarget_get_target,$(1)) : UNOAPI_REFERENCE := $(2)
+$(call gb_UnoApiTarget_get_target,$(1)) : $(2)
+$(call gb_UnoApiTarget_get_target,$(1)) : $(gb_UnoApiTarget_UNOIDLCHECKDEPS)
+
+endef
+
+define gb_UnoApiTarget_use_api
+$(call gb_UnoApiTarget_get_target,$(1)) : UNOAPI_DEPRDBS += $(2)
+$(call gb_UnoApiTarget_get_target,$(1)) : $(call gb_UnoApiTarget_get_target,$(2))
+
+endef
+
+# UnoApiHeadersTarget
+
+# defined by platform
+#  gb_UnoApiHeadersTarget_select_variant
+
+# Allow to redefine header variant.
+#
+# On iOS we use static linking because dynamic loading of own code
+# isn't allowed by the iOS App Store rules, and we want our code to be
+# eventually distributable there as part of apps.
+#
+# To avoid problems that this causes together with the lovely
+# intentional breaking of the One Definition Rule, for iOS we always
+# generate comprehensive headers for certain type RDBS. (The ODR
+# breakage doesn't harm, by accident or careful design, on platforms
+# where shared libraries are used.) To avoid generating the same headers
+# more than once, we are silently "redirecting" the target to point to
+# comprehensive headers instead.
+#
+# Example:
+# If gb_UnoApiHeadersTarget_select_variant is defined as
+#
+# ifeq ($(DISABLE_DYNLOADING),TRUE)
+# gb_UnoApiHeadersTarget_select_variant = $(if $(filter udkapi,$(1)),comprehensive,$(2))
+# else
+# gb_UnoApiHeadersTarget_select_variant = $(2)
+# endif
+#
+# then, for the DISABLE_DYNLOADING case, whenever a makefile uses
+# $(call gb_UnoApiHeadersTarget_get_target,udkapi) or $(call
+# gb_UnoApiHeadersTarget_get_dir,udkapi), it will get target or dir for
+# comprehensive headers instead.
+#
+# We are experimenting with static linking on Android, too. There for
+# technical reasons to get around silly limitations in the OS, sigh.
+#
+# gb_UnoApiHeadersTarget_select_variant api default-variant
+ifeq ($(origin gb_UnoApiHeadersTarget_select_variant),undefined)
+$(eval $(call gb_Output_error,gb_UnoApiHeadersTarget_select_variant must be defined by platform))
+endif
+
+gb_UnoApiHeadersTarget_CPPUMAKERDEPS := $(call gb_Executable_get_runtime_dependencies,cppumaker)
+gb_UnoApiHeadersTarget_CPPUMAKERCOMMAND := $(call gb_Executable_get_command,cppumaker)
+
+define gb_UnoApiHeadersTarget__command
+	$(gb_UnoApiHeadersTarget_CPPUMAKERCOMMAND) \
+		-Gc $(4) -O$(3) $(call gb_UnoApiTarget_get_target,$(2)) \
+		 $(UNOAPI_DEPS) && \
+	$(if $(filter $(PRODUCT_BUILD_TYPE),java),rm -f $(1) &&) \
+	touch $(1)
+
+endef
+
+$(call gb_UnoApiHeadersTarget_get_real_bootstrap_target,%) : \
+		$(gb_UnoApiHeadersTarget_CPPUMAKERDEPS)
+	$(call gb_Output_announce,$*,$(true),HPB,3) \
+	$(call gb_UnoApiHeadersTarget__command,$@,$*,$(call gb_UnoApiHeadersTarget_get_bootstrap_dir,$*))
+
+$(call gb_UnoApiHeadersTarget_get_real_comprehensive_target,%) : \
+		$(gb_UnoApiHeadersTarget_CPPUMAKERDEPS)
+	$(call gb_Output_announce,$*,$(true),HPC,3)
+	$(call gb_UnoApiHeadersTarget__command,$@,$*,$(call gb_UnoApiHeadersTarget_get_comprehensive_dir,$*),-C)
+
+$(call gb_UnoApiHeadersTarget_get_real_target,%) : \
+		$(gb_UnoApiHeadersTarget_CPPUMAKERDEPS)
+	$(call gb_Output_announce,$*,$(true),HPP,3) \
+	echo $(call gb_UnoApiHeadersTarget__command,$@,$*,$(call gb_UnoApiHeadersTarget_get_dir,$*),-L)
+
+.PHONY : $(call gb_UnoApiHeadersTarget_get_clean_target,%)
+$(call gb_UnoApiHeadersTarget_get_clean_target,%) :
+	$(call gb_Output_announce,$*,$(false),HPP,3)
+	$(call gb_Helper_abbreviate_dirs,\
+		rm -rf \
+			$(call gb_UnoApiHeadersTarget_get_real_dir,$*) \
+			$(call gb_UnoApiHeadersTarget_get_real_bootstrap_dir,$*) \
+			$(call gb_UnoApiHeadersTarget_get_real_comprehensive_dir,$*) \
+			$(call gb_UnoApiHeadersTarget_get_real_target,$*) \
+			$(call gb_UnoApiHeadersTarget_get_real_bootstrap_target,$*)) \
+			$(call gb_UnoApiHeadersTarget_get_real_comprehensive_target,$*)
+
+define gb_UnoApiHeadersTarget_UnoApiHeadersTarget
+$(call gb_UnoApiHeadersTarget_get_target,$(1)) : $(call gb_UnoApiTarget_get_target,$(1))
+$(call gb_UnoApiHeadersTarget_get_bootstrap_target,$(1)) : $(call gb_UnoApiTarget_get_target,$(1))
+$(call gb_UnoApiHeadersTarget_get_comprehensive_target,$(1)) : $(call gb_UnoApiTarget_get_target,$(1))
+$(call gb_UnoApiHeadersTarget_get_clean_target,$(1)) : $(call gb_UnoApiTarget_get_clean_target,$(1))
+
+$(call gb_UnoApiHeadersTarget_get_target,$(1)) : UNOAPI_DEPS :=
+$(call gb_UnoApiHeadersTarget_get_bootstrap_target,$(1)) : UNOAPI_DEPS :=
+$(call gb_UnoApiHeadersTarget_get_comprehensive_target,$(1)) : UNOAPI_DEPS :=
+
+# need dummy recipes so that header files are delivered in Package_inc;
+# otherwise make will consider the header to be up-to-date because it was
+# actually built by the recipe for gb_UnoApiHeadersTarget_get_target
+$(call gb_UnoApiHeadersTarget_get_real_dir,$(1))/%.hdl : \
+		$(call gb_Executable_get_target_for_build,cppumaker) \
+		| $(call gb_UnoApiHeadersTarget_get_real_target,$(1))
+	touch $$@
+
+$(call gb_UnoApiHeadersTarget_get_real_dir,$(1))/%.hpp : \
+		$(call gb_Executable_get_target_for_build,cppumaker) \
+		| $(call gb_UnoApiHeadersTarget_get_real_target,$(1))
+	touch $$@
+
+$(call gb_UnoApiHeadersTarget_get_real_bootstrap_dir,$(1))/%.hdl : \
+		$(call gb_Executable_get_target_for_build,cppumaker) \
+		| $(call gb_UnoApiHeadersTarget_get_real_bootstrap_target,$(1))
+	touch $$@
+
+$(call gb_UnoApiHeadersTarget_get_real_bootstrap_dir,$(1))/%.hpp : \
+		$(call gb_Executable_get_target_for_build,cppumaker) \
+		| $(call gb_UnoApiHeadersTarget_get_real_bootstrap_target,$(1))
+	touch $$@
+
+$(call gb_UnoApiHeadersTarget_get_real_comprehensive_dir,$(1))/%.hdl : \
+		$(call gb_Executable_get_target_for_build,cppumaker) \
+		| $(call gb_UnoApiHeadersTarget_get_real_comprehensive_target,$(1))
+	touch $$@
+
+$(call gb_UnoApiHeadersTarget_get_real_comprehensive_dir,$(1))/%.hpp : \
+		$(call gb_Executable_get_target_for_build,cppumaker) \
+		| $(call gb_UnoApiHeadersTarget_get_real_comprehensive_target,$(1))
+	touch $$@
+
+endef
+
+# ensure that idl change triggers the dummy rule to rebuild the headers
+# call gb_UnoApiHeadersTarget_add_headerfile,unoapi,headerfile
+define gb_UnoApiHeadersTarget_add_headerfile
+$(call gb_UnoApiHeadersTarget_get_dir,$(1))/$(2) : \
+	$(SRCDIR)/$(basename $(gb_UnoApiTarget_REG_$(1))/$(2)).idl
+
+endef
+
+# call gb_UnoApiHeadersTarget_add_headerfiles,unoapi,directory,headerfilenames
+define gb_UnoApiHeadersTarget_add_headerfiles
+$(foreach hdr,$(3),$(call gb_UnoApiHeadersTarget_add_headerfile,$(1),$(2)/$(hdr)))
+endef
+
+define gb_UnoApiHeadersTarget__use_api_for_target
+$(call gb_UnoApiHeadersTarget_get_$(3),$(1)) : $(call gb_UnoApiTarget_get_target,$(2))
+$(call gb_UnoApiHeadersTarget_get_$(3),$(1)) : UNOAPI_DEPS += -X$(call gb_UnoApiTarget_get_target,$(2))
+
+endef
+
+define gb_UnoApiHeadersTarget__use_api
+$(call gb_UnoApiHeadersTarget__use_api_for_target,$(1),$(2),target)
+$(call gb_UnoApiHeadersTarget__use_api_for_target,$(1),$(2),bootstrap_target)
+$(call gb_UnoApiHeadersTarget__use_api_for_target,$(1),$(2),comprehensive_target)
+
+endef
+
+define gb_UnoApiHeadersTarget_use_api
+$(foreach rdb,$(2),$(call gb_UnoApiHeadersTarget__use_api,$(1),$(rdb)))
+
+endef
+
+# vim: set noet sw=4 ts=4:
