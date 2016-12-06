b7f220d3fe7dc850bcc50d0325f57b8a186bd7e8
pluby <>
2016-12-06 22:49:35 +0000

    Correct license header

--- comphelper/inc/comphelper/embeddedobjectcontainer.hxx
+++ comphelper/inc/comphelper/embeddedobjectcontainer.hxx
@@ -33,6 +33,7 @@
  *
  *   This Source Code Form is subject to the terms of the Mozilla Public
  *   License, v. 2.0. If a copy of the MPL was not distributed with this
+ *   file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  ************************************************************************/
 #ifndef _COMPHELPER_OBJECTCONTAINER_HXX_
