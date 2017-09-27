c18bd91e5f8e331f1d935ac51b62b0c27fb7d4d9
pluby <>
2017-09-27 22:19:10 +0000

    Attempt to stop crashing due to uncaught Objective-C exceptions in the main thread by running until the [NSApplication run] selector exits cleanly

--- vcl/source/app/svmainhook_cocoa.mm
+++ vcl/source/app/svmainhook_cocoa.mm
@@ -225,7 +225,21 @@ void NSApplication_run()
 			// by adding our custom run loop mode to the list of common modes
 			CFRunLoopAddCommonMode( CFRunLoopGetMain(), CFSTR( "AWTRunLoopMode" ) );
 
-			[pApp run];
+			// Attempt to stop crashing due to uncaught Objective-C exceptions
+			// in the main thread by running until the [NSApplication run]
+			// selector exits cleanly
+			bool bContinue = true;
+			while ( bContinue )
+			{
+				try
+				{
+					[pApp run];
+					bContinue = false;
+				}
+				catch ( ... )
+				{
+				}
+			}
 		}
 	}
 
