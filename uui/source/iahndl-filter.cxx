6b4fa06d07825d644b2ae9d0854141fce2a52ad7
pluby <>
2017-10-07 14:08:44 +0000

    Correct copyright date

--- uui/source/iahndl-filter.cxx
+++ uui/source/iahndl-filter.cxx
@@ -16,7 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  * 
- *   Modified December 2017 by Patrick Luby. NeoOffice is only distributed
+ *   Modified October 2017 by Patrick Luby. NeoOffice is only distributed
  *   under the GNU General Public License, Version 3 as allowed by Section 3.3
  *   of the Mozilla Public License, v. 2.0.
  *
