f2a88952971ae304c98310e43bc4d264a96ba789
pluby <>
2018-04-04 03:32:06 +0000

    Attempt to fix Mac App Store crash by checking if the document's edit shell has been deleted after it was fetched

--- sw/source/filter/basflt/shellio.cxx
+++ sw/source/filter/basflt/shellio.cxx
@@ -15,6 +15,13 @@
  *   License, Version 2.0 (the "License"); you may not use this file
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ * 
+ *   Modified April 2018 by Patrick Luby. NeoOffice is only distributed
+ *   under the GNU General Public License, Version 3 as allowed by Section 3.3
+ *   of the Mozilla Public License, v. 2.0.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <hintids.hxx>
@@ -873,6 +880,11 @@ sal_uLong SwWriter::Write( WriterRef& rxWriter, const OUString* pRealFileName )
 
     pOutDoc->getIDocumentSettingAccess().set(IDocumentSettingAccess::PURGE_OLE, bWasPurgeOle );
 
+#ifdef USE_JAVA
+    // Attempt to fix Mac App Store crash by checking if the document's edit
+    // shell has been deleted after it was fetched
+    pESh = pOutDoc->GetEditShell();
+#endif	// USE_JAVA
     if( pESh )
     {
         pESh->EndAllAction();
