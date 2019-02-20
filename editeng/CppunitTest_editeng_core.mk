0d99ef90da063d6f98c4b7663936995e419ad19a
pluby <>
2019-02-20 01:18:58 +0000

    Merge LibreOffice 5.4.7.2 code

--- editeng/CppunitTest_editeng_core.mk
+++ editeng/CppunitTest_editeng_core.mk
@@ -38,16 +38,17 @@ $(eval $(call gb_CppunitTest_use_libraries,editeng_core, \
     salhelper \
     sax \
     sot \
+    sfx \
     svl \
     svt \
     test \
     tk \
     tl \
     ucbhelper \
+    unotest \
     utl \
     vcl \
     xo \
-	$(gb_UWINAPI) \
 ))
 
 ifeq ($(strip $(GUIBASE)),java)
@@ -59,6 +60,7 @@ endif	# GUIBASE == java
 $(eval $(call gb_CppunitTest_use_externals,editeng_core,\
 	boost_headers \
     icuuc \
+	libxml2 \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,editeng_core,\
@@ -66,10 +68,7 @@ $(eval $(call gb_CppunitTest_set_include,editeng_core,\
     $$(INCLUDE) \
 ))
 
-$(eval $(call gb_CppunitTest_use_api,editeng_core,\
-    offapi \
-    udkapi \
-))
+$(eval $(call gb_CppunitTest_use_sdk_api,editeng_core))
 
 $(eval $(call gb_CppunitTest_use_ure,editeng_core))
 $(eval $(call gb_CppunitTest_use_vcl,editeng_core))
