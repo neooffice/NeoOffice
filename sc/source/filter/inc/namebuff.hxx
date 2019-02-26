aa3bf20054fc88fd6bbf2fca3604bbaff8712cf1
pluby <>
2019-02-26 01:30:31 +0000

    Merge LibreOffice 5.4.7.2 code

--- sc/source/filter/inc/namebuff.hxx
+++ sc/source/filter/inc/namebuff.hxx
@@ -20,6 +20,7 @@
 #ifndef INCLUDED_SC_SOURCE_FILTER_INC_NAMEBUFF_HXX
 #define INCLUDED_SC_SOURCE_FILTER_INC_NAMEBUFF_HXX
 
+#include <memory>
 #include <rtl/ustring.hxx>
 #include <osl/diagnose.h>
 #include "compiler.hxx"
@@ -29,113 +30,32 @@
 #include "rangenam.hxx"
 #include "formulacell.hxx"
 
-#ifdef NO_LIBO_TOKEN_ARRAYS_LEAK_FIX
-#include <boost/unordered_map.hpp>
-#else	// NO_LIBO_TOKEN_ARRAYS_LEAK_FIX
-#include <unordered_map>
-#endif	// NO_LIBO_TOKEN_ARRAYS_LEAK_FIX
 #include <list>
+#include <unordered_map>
 
 class ScTokenArray;
-class NameBuffer;
 
 class StringHashEntry
 {
 private:
-    friend class NameBuffer;
     OUString          aString;
     sal_uInt32        nHash;
 
     static sal_uInt32   MakeHashCode( const OUString& );
 public:
     inline          StringHashEntry( const OUString& );
-    inline          StringHashEntry( void );
-    inline void     operator =( const sal_Char* );
-    inline void     operator =( const OUString& );
-    inline void     operator =( const StringHashEntry& );
     inline bool     operator ==( const StringHashEntry& ) const;
 };
 
-inline StringHashEntry::StringHashEntry( void )
+inline StringHashEntry::StringHashEntry( const OUString& r )
+    : aString( r )
+    , nHash( MakeHashCode(r) )
 {
-}
-
-inline StringHashEntry::StringHashEntry( const OUString& r ) : aString( r )
-{
-    nHash = MakeHashCode( r );
-}
-
-inline void StringHashEntry::operator =( const sal_Char* p )
-{
-    aString = OUString(p, strlen(p), RTL_TEXTENCODING_ASCII_US);
-    nHash = MakeHashCode( aString );
-}
-
-inline void StringHashEntry::operator =( const OUString& r )
-{
-    aString = r;
-    nHash = MakeHashCode( r );
-}
-
-inline void StringHashEntry::operator =( const StringHashEntry& r )
-{
-    nHash = r.nHash;
-    aString = r.aString;
 }
 
 inline bool StringHashEntry::operator ==( const StringHashEntry& r ) const
 {
     return ( nHash == r.nHash && aString ==  r.aString );
-}
-
-class NameBuffer : public ExcRoot
-{
-private:
-    sal_uInt16                  nBase;      // Index-Base
-    std::vector<StringHashEntry*> maHashes;
-
-public:
-
-    inline                  NameBuffer( RootData* );
-    inline                  NameBuffer( RootData*, sal_uInt16 nNewBase );
-
-    virtual                 ~NameBuffer();
-    inline const OUString*  Get( sal_uInt16 nIndex ) const;
-    inline sal_uInt16       GetLastIndex() const;
-    inline void             SetBase( sal_uInt16 nNewBase = 0 );
-    void                    operator <<( const OUString& rNewString );
-};
-
-inline NameBuffer::NameBuffer( RootData* p ) : ExcRoot( p )
-{
-    nBase = 0;
-}
-
-inline NameBuffer::NameBuffer( RootData* p, sal_uInt16 nNewBase ) : ExcRoot( p )
-{
-    nBase = nNewBase;
-}
-
-inline const OUString* NameBuffer::Get ( sal_uInt16 n ) const
-{
-    if( n < nBase || n >= maHashes.size() )
-        return NULL;
-
-    return &(maHashes[n]->aString);
-}
-
-inline sal_uInt16 NameBuffer::GetLastIndex () const
-{
-    int size = maHashes.size() + nBase;
-
-    OSL_ENSURE( size <= 0xFFFF, "*NameBuffer::GetLastIndex(): I'm sick and tired!" );
-
-    return static_cast<sal_uInt16>( size );
-}
-
-inline void NameBuffer::SetBase( sal_uInt16 nNewBase )
-{
-    nBase = nNewBase;
 }
 
 /**
@@ -144,7 +64,7 @@ inline void NameBuffer::SetBase( sal_uInt16 nNewBase )
 class SharedFormulaBuffer : public ExcRoot
 {
 #ifdef NO_LIBO_TOKEN_ARRAYS_LEAK_FIX
-    typedef boost::unordered_map<ScAddress, ScTokenArray*, ScAddressHashFunctor> TokenArraysType;
+    typedef std::unordered_map<ScAddress, ScTokenArray*, ScAddressHashFunctor> TokenArraysType;
 #else	// NO_LIBO_TOKEN_ARRAYS_LEAK_FIX
     typedef std::unordered_map<ScAddress, std::unique_ptr<ScTokenArray>, ScAddressHashFunctor> TokenArraysType;
 #endif	// NO_LIBO_TOKEN_ARRAYS_LEAK_FIX
@@ -158,7 +78,7 @@ public:
     const ScTokenArray* Find( const ScAddress& rRefPos ) const;
 };
 
-class RangeNameBufferWK3
+class RangeNameBufferWK3 final
 {
 private:
     struct Entry
@@ -172,23 +92,23 @@ private:
                             Entry( const OUString& rName, const OUString& rScName, const ScComplexRefData& rCRD )
                                 : aStrHashEntry( rName )
                                 , aScComplexRefDataRel( rCRD )
-                                , aScAbsName( rScName )
+                                , aScAbsName( rScName + "_ABS" )
                                 , nAbsInd(0)
                                 , nRelInd(0)
                                 , bSingleRef(false)
                             {
-                                aScAbsName = "_ABS";
                             }
     };
 
     LOTUS_ROOT*        m_pLotRoot;
-    ScTokenArray*      pScTokenArray;
+    std::unique_ptr<ScTokenArray>
+                       pScTokenArray;
     sal_uInt16         nIntCount;
     std::vector<Entry> maEntries;
 
 public:
     RangeNameBufferWK3(LOTUS_ROOT* pLotRoot);
-    virtual                 ~RangeNameBufferWK3();
+    ~RangeNameBufferWK3();
     void                    Add( const OUString& rName, const ScComplexRefData& rCRD );
     inline void             Add( const OUString& rName, const ScRange& aScRange );
     bool                    FindRel( const OUString& rRef, sal_uInt16& rIndex );
@@ -215,21 +135,13 @@ class ExtSheetBuffer : public ExcRoot
 {
 private:
     struct Cont
-        {
+    {
         OUString      aFile;
         OUString      aTab;
         sal_uInt16    nTabNum;    // 0xFFFF -> not set yet
                                 // 0xFFFE -> tried to set, but failed
                                 // 0xFFFD -> should be in the same workbook, but not found
         bool          bSWB;
-        bool          bLink;
-                    Cont( const OUString& rFilePathAndName, const OUString& rTabName ) :
-                        aFile( rFilePathAndName ),
-                        aTab( rTabName )
-                    {
-                        nTabNum = 0xFFFF;   // -> table not created yet
-                        bSWB = bLink = false;
-                    }
                     Cont( const OUString& rFilePathAndName, const OUString& rTabName,
                         const bool bSameWB ) :
                         aFile( rFilePathAndName ),
@@ -237,9 +149,8 @@ private:
                     {
                         nTabNum = 0xFFFF;   // -> table not created yet
                         bSWB = bSameWB;
-                        bLink = false;
                     }
-        };
+    };
 
     std::vector<Cont> maEntries;
 
@@ -247,13 +158,11 @@ public:
     inline          ExtSheetBuffer( RootData* );
 
     sal_Int16       Add( const OUString& rFilePathAndName,
-                        const OUString& rTabName, const bool bSameWorkbook = false );
+                        const OUString& rTabName, const bool bSameWorkbook );
 
     bool            GetScTabIndex( sal_uInt16 nExcSheetIndex, sal_uInt16& rIn_LastTab_Out_ScIndex );
-    bool            IsLink( const sal_uInt16 nExcSheetIndex ) const;
-    bool            GetLink( const sal_uInt16 nExcSheetIndex, OUString &rAppl, OUString &rDoc ) const;
 
-    void            Reset( void );
+    void            Reset();
 };
 
 inline ExtSheetBuffer::ExtSheetBuffer( RootData* p ) : ExcRoot( p )
@@ -262,14 +171,12 @@ inline ExtSheetBuffer::ExtSheetBuffer( RootData* p ) : ExcRoot( p )
 
 struct ExtName
 {
-    OUString          aName;
     sal_uInt32        nStorageId;
     sal_uInt16        nFlags;
 
-    inline          ExtName( const OUString& r, sal_uInt16 n ) : aName( r ), nStorageId( 0 ), nFlags( n ) {}
+    ExtName( sal_uInt16 n ) : nStorageId( 0 ), nFlags( n ) {}
 
-    bool            IsDDE( void ) const;
-    bool            IsOLE( void ) const;
+    bool            IsOLE() const;
 };
 
 class ExtNameBuff : protected XclImpRoot
@@ -277,9 +184,9 @@ class ExtNameBuff : protected XclImpRoot
 public:
     explicit        ExtNameBuff( const XclImpRoot& rRoot );
 
-    void            AddDDE( const OUString& rName, sal_Int16 nRefIdx );
-    void            AddOLE( const OUString& rName, sal_Int16 nRefIdx, sal_uInt32 nStorageId );
-    void            AddName( const OUString& rName, sal_Int16 nRefIdx );
+    void            AddDDE( sal_Int16 nRefIdx );
+    void            AddOLE( sal_Int16 nRefIdx, sal_uInt32 nStorageId );
+    void            AddName( sal_Int16 nRefIdx );
 
     const ExtName*  GetNameByIndex( sal_Int16 nRefIdx, sal_uInt16 nNameIdx ) const;
 
