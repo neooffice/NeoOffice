11ff31e0e64588c60d0e4a221d5572a85f432d2c
pluby <>
2019-03-01 14:58:01 +0000

    Replace NULL with nullptr

--- sfx2/source/dialog/infobar.cxx
+++ sfx2/source/dialog/infobar.cxx
@@ -36,7 +36,7 @@
 typedef sal_Bool UseDarkModeColors_Type();
 
 static ::osl::Module aModule;
-static UseDarkModeColors_Type *pUseDarkModeColors = NULL;
+static UseDarkModeColors_Type *pUseDarkModeColors = nullptr;
 
 static sal_Bool UseDarkModeColors()
 {
