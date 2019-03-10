85a2e5013e596959e621ae59f5ea59403de5c042
pluby <>
2019-03-10 21:05:47 +0000

    Merge LibreOffice 5.4.7.2 code

--- sw/source/filter/ww8/ww8par2.hxx
+++ sw/source/filter/ww8/ww8par2.hxx
@@ -34,41 +34,41 @@ class WW8RStyle;
 class WW8DupProperties
 {
 public:
-    WW8DupProperties(SwDoc &rDoc, SwWW8FltControlStack *pStk);
+    WW8DupProperties(SwDoc &rDoc, SwWW8FltControlStack *pStack);
     void Insert(const SwPosition &rPos);
 private:
-    //No copying
-    WW8DupProperties(const WW8DupProperties&);
-    WW8DupProperties& operator=(const WW8DupProperties&);
+    WW8DupProperties(const WW8DupProperties&) = delete;
+    WW8DupProperties& operator=(const WW8DupProperties&) = delete;
     SwWW8FltControlStack* pCtrlStck;
     SfxItemSet aChrSet,aParSet;
 };
 
 struct WW8SwFlyPara
 {
-    SwFlyFrmFmt* pFlyFmt;
+    SwFlyFrameFormat* pFlyFormat;
 
-                // 1. Teil: daraus abgeleitete Sw-Attribute
+                // part 1: directly derived Sw attributes
     sal_Int16 nXPos, nYPos;         // Position
-    sal_Int16 nLeMgn, nRiMgn;       // Raender
-    sal_Int16 nUpMgn, nLoMgn;       // Raender
-    sal_Int16 nWidth, nHeight;      // Groesse
+    sal_Int16 nLeMgn, nRiMgn;       // borders
+    sal_Int16 nUpMgn, nLoMgn;       // borders
+    sal_Int16 nWidth, nHeight;      // size
     sal_Int16 nNetWidth;
 
-    SwFrmSize eHeightFix;       // Hoehe Fix oder Min
-    RndStdIds eAnchor;          // Bindung
-    short eHRel;     // Seite oder Seitenrand
-    short eVRel;     // Seite oder Seitenrand
-    sal_Int16 eVAlign;       // Oben, unten, mittig
-    sal_Int16 eHAlign;       // links, rechts, mittig
-    SwSurround eSurround;       // Wrap-Mode
+    SwFrameSize eHeightFix;         // height fixed or min
+    RndStdIds   eAnchor;            // binding
+    short       eHRel;              // page or page border
+    short       eVRel;              // page or page border
+    sal_Int16   eVAlign;            // up, down, centered
+    sal_Int16   eHAlign;            // left, right, centered
+    css::text::WrapTextMode
+                eSurround;          // wrap mode
 
-    sal_uInt8 nXBind, nYBind;        // relativ zu was gebunden
+    sal_uInt8 nXBind, nYBind;        // bound relative to what
 
-                // 2.Teil: sich waehrend des Einlesens ergebende AEnderungen
+                // part 2: changes found during reading
     long nNewNetWidth;
-    SwPosition* pMainTextPos;   // um nach Apo in Haupttext zurueckzukehren
-    sal_uInt16 nLineSpace;          // LineSpace in tw fuer Graf-Apos
+    SwPosition* pMainTextPos;   // to return to main text after apo
+    sal_uInt16 nLineSpace;          // line space in tw for graf apos
     bool bAutoWidth;
     bool bToggelPos;
 
@@ -90,17 +90,17 @@ class WW8RStyle: public WW8Style
 {
 friend class SwWW8ImplReader;
     wwSprmParser maSprmParser;
-    SwWW8ImplReader* pIo;   // Parser-Klasse
-    SvStream* pStStrm;      // Input-File
+    SwWW8ImplReader* pIo;   // parser class
+    SvStream* pStStrm;      // input file
 
-    SwNumRule* pStyRule;    // Bullets und Aufzaehlungen in Styles
+    SwNumRule* pStyRule;    // bullets and enumerations in styles
 
-    sal_uInt8* pParaSprms;           // alle ParaSprms des UPX falls UPX.Papx
-    sal_uInt16 nSprmsLen;           // Laenge davon
+    sal_uInt8* pParaSprms;           // all ParaSprms of the UPX if UPX.Papx
+    sal_uInt16 nSprmsLen;           // its length
 
-    sal_uInt8 nWwNumLevel;           // fuer Bullets und Aufzaehlungen in Styles
+    sal_uInt8 nWwNumLevel;           // for bullets and enumerations in styles
 
-    bool bTxtColChanged;
+    bool bTextColChanged;
     bool bFontChanged;      // For Simulating Default-Font
     bool bCJKFontChanged;   // For Simulating Default-CJK Font
     bool bCTLFontChanged;   // For Simulating Default-CTL Font
@@ -108,7 +108,7 @@ friend class SwWW8ImplReader;
     bool bFCTLSizeChanged;  // For Simulating Default-CTL FontSize
     bool bWidowsChanged;    // For Simulating Default-Widows / Orphans
 
-    void ImportSprms(sal_Size nPosFc, short nLen, bool bPap);
+    void ImportSprms(std::size_t nPosFc, short nLen, bool bPap);
     void ImportSprms(sal_uInt8 *pSprms, short nLen, bool bPap);
     void ImportGrupx(short nLen, bool bPara, bool bOdd);
     short ImportUPX(short nLen, bool bPAP, bool bOdd);
@@ -117,8 +117,6 @@ friend class SwWW8ImplReader;
     void Import1Style(sal_uInt16 nNr);
     void RecursiveReg(sal_uInt16 nNr);
 
-    void ImportStyles();
-
     void ImportNewFormatStyles();
     void ScanStyles();
     void ImportOldFormatStyles();
@@ -126,21 +124,19 @@ friend class SwWW8ImplReader;
     bool PrepareStyle(SwWW8StyInf &rSI, ww::sti eSti, sal_uInt16 nThisStyle, sal_uInt16 nNextStyle);
     void PostStyle(SwWW8StyInf &rSI, bool bOldNoImp);
 
-    //No copying
-    WW8RStyle(const WW8RStyle&);
-    WW8RStyle& operator=(const WW8RStyle&);
+    WW8RStyle(const WW8RStyle&) = delete;
+    WW8RStyle& operator=(const WW8RStyle&) = delete;
 public:
     WW8RStyle( WW8Fib& rFib, SwWW8ImplReader* pI );
     void Import();
     void PostProcessStyles();
-    const sal_uInt8* HasParaSprm( sal_uInt16 nId ) const;
+    SprmResult HasParaSprm(sal_uInt16 nId) const;
 };
 
 class WW8FlySet: public SfxItemSet
 {
 private:
-    //No copying
-    const WW8FlySet& operator=(const WW8FlySet&);
+    const WW8FlySet& operator=(const WW8FlySet&) = delete;
     void Init(const SwWW8ImplReader& rReader, const SwPaM* pPaM);
 public:
     WW8FlySet(SwWW8ImplReader& rReader, const WW8FlyPara* pFW,
@@ -151,9 +147,16 @@ public:
 
 #ifndef NO_LIBO_WW8_TABLE_LEAK_FIX
 
+// Gets filled in WW8TabDesc::MergeCells().
+// Algorithm must ensure proper row and column order in WW8SelBoxInfo!
 class WW8SelBoxInfo
-    : public std::vector<SwTableBox*>, private boost::noncopyable
 {
+private:
+    std::vector<std::vector<SwTableBox*> > m_vRows;
+
+    WW8SelBoxInfo(WW8SelBoxInfo const&) = delete;
+    WW8SelBoxInfo& operator=(WW8SelBoxInfo const&) = delete;
+
 public:
     short nGroupXStart;
     short nGroupWidth;
@@ -162,57 +165,81 @@ public:
     WW8SelBoxInfo(short nXCenter, short nWidth)
         : nGroupXStart( nXCenter ), nGroupWidth( nWidth ), bGroupLocked(false)
     {}
+
+    size_t size() const
+    {
+        size_t nResult = 0;
+        for (auto& it : m_vRows)
+            nResult += it.size();
+        return nResult;
+    }
+
+    size_t rowsCount() const { return m_vRows.size(); }
+
+    const std::vector<SwTableBox*>& row( size_t nIndex ) { return m_vRows[nIndex]; }
+
+    void push_back( SwTableBox* pBox )
+    {
+        bool bDone = false;
+        for (auto& iRow : m_vRows)
+            if (iRow[0]->GetUpper() == pBox->GetUpper())
+            {
+                iRow.push_back(pBox);
+                bDone = true;
+                break;
+            }
+        if (!bDone)
+        {
+            const size_t sz = m_vRows.size();
+            m_vRows.resize(sz+1);
+            m_vRows[sz].push_back(pBox);
+        }
+    }
 };
 
-typedef boost::ptr_vector<WW8SelBoxInfo> WW8MergeGroups;
-
-class WW8TabDesc: private boost::noncopyable
+class WW8TabDesc
 {
-    std::vector<OUString> aNumRuleNames;
+    std::vector<OUString> m_aNumRuleNames;
     sw::util::RedlineStack *mpOldRedlineStack;
 
-    SwWW8ImplReader* pIo;
+    SwWW8ImplReader* m_pIo;
 
-    WW8TabBandDesc* pFirstBand;
-    WW8TabBandDesc* pActBand;
+    WW8TabBandDesc* m_pFirstBand;
+    WW8TabBandDesc* m_pActBand;
 
-#ifdef NO_LIBO_WW8_TABLE_LEAK_FIX
-    SwPosition* pTmpPos;
-#else	// NO_LIBO_WW8_TABLE_LEAK_FIX
-    std::unique_ptr<SwPosition> m_xTmpPos;
-#endif	// NO_LIBO_WW8_TABLE_LEAK_FIX
+    SwPosition* m_pTmpPos;
 
-    SwTableNode* pTblNd;            // table node
-    const SwTableLines* pTabLines;  // row array of node
-    SwTableLine* pTabLine;          // current row
-    SwTableBoxes* pTabBoxes;        // boxes array in current row
-    SwTableBox* pTabBox;            // current cell
+    SwTableNode* m_pTableNd;          // table node
+    const SwTableLines* m_pTabLines;  // row array of node
+    SwTableLine* m_pTabLine;          // current row
+    SwTableBoxes* m_pTabBoxes;        // boxes array in current row
+    SwTableBox* m_pTabBox;            // current cell
 
-    WW8MergeGroups aMergeGroups;   // list of all cells to be merged
+    std::vector<std::unique_ptr<WW8SelBoxInfo>> m_MergeGroups;   // list of all cells to be merged
 
-    WW8_TCell* pAktWWCell;
+    WW8_TCell* m_pAktWWCell;
 
-    short nRows;
-    short nDefaultSwCols;
-    short nBands;
-    short nMinLeft;
-    short nConvertedLeft;
-    short nMaxRight;
-    short nSwWidth;
-    short nPreferredWidth;
-    short nOrgDxaLeft;
+    short m_nRows;
+    short m_nDefaultSwCols;
+    short m_nBands;
+    short m_nMinLeft;
+    short m_nConvertedLeft;
+    short m_nMaxRight;
+    short m_nSwWidth;
+    short m_nPreferredWidth;
+    short m_nOrgDxaLeft;
 
-    bool bOk;
-    bool bClaimLineFmt;
-    sal_Int16 eOri;
-    bool bIsBiDi;
+    bool m_bOk;
+    bool m_bClaimLineFormat;
+    sal_Int16 m_eOri;
+    bool m_bIsBiDi;
                                 // 2. common admin info
-    short nAktRow;
-    short nAktBandRow;          // SW: row of current band
+    short m_nAktRow;
+    short m_nAktBandRow;          // SW: row of current band
                                 // 3. admin info for writer
-    short nAktCol;
+    short m_nAktCol;
 
-    sal_uInt16 nRowsToRepeat;
+    sal_uInt16 m_nRowsToRepeat;
 
     // 4. methods
 
@@ -222,7 +249,7 @@ class WW8TabDesc: private boost::noncopyable
     void SetTabVertAlign( SwTableBox* pBox, short nWwIdx );
     void SetTabDirection( SwTableBox* pBox, short nWwIdx );
     void CalcDefaults();
-    bool SetPamInCell(short nWwCol, bool bPam);
+    void SetPamInCell(short nWwCol, bool bPam);
     void InsertCells( short nIns );
     void AdjustNewBand();
 
@@ -230,34 +257,37 @@ class WW8TabDesc: private boost::noncopyable
 
     // single box - maybe used in a merge group
     // (the merge groups are processed later at once)
-    SwTableBox* UpdateTableMergeGroup(WW8_TCell& rCell,
+    void UpdateTableMergeGroup(WW8_TCell& rCell,
         WW8SelBoxInfo* pActGroup, SwTableBox* pActBox, sal_uInt16 nCol  );
     void StartMiserableHackForUnsupportedDirection(short nWwCol);
     void EndMiserableHackForUnsupportedDirection(short nWwCol);
 
+    WW8TabDesc(WW8TabDesc const&) = delete;
+    WW8TabDesc& operator=(WW8TabDesc const&) = delete;
+
 public:
-    const SwTable* pTable;          // table
-    SwPosition* pParentPos;
-    SwFlyFrmFmt* pFlyFmt;
-    SfxItemSet aItemSet;
+    const SwTable* m_pTable;          // table
+    SwPosition* m_pParentPos;
+    SwFlyFrameFormat* m_pFlyFormat;
+    SfxItemSet m_aItemSet;
     bool IsValidCell(short nCol) const;
     bool InFirstParaInCell() const;
 
     WW8TabDesc( SwWW8ImplReader* pIoClass, WW8_CP nStartCp );
-    bool Ok() const { return bOk; }
-    void CreateSwTable(SvxULSpaceItem* pULSpaceItem = 0);
+    bool Ok() const { return m_bOk; }
+    void CreateSwTable();
     void UseSwTable();
-    void SetSizePosition(SwFrmFmt* pFrmFmt);
+    void SetSizePosition(SwFrameFormat* pFrameFormat);
     void TableCellEnd();
     void MoveOutsideTable();
     void ParkPaM();
     void FinishSwTable();
     void MergeCells();
-    short GetMinLeft() const { return nConvertedLeft; }
+    short GetMinLeft() const { return m_nConvertedLeft; }
     ~WW8TabDesc();
 
-    const WW8_TCell* GetAktWWCell() const { return pAktWWCell; }
-    short GetAktCol() const { return nAktCol; }
+    const WW8_TCell* GetAktWWCell() const { return m_pAktWWCell; }
+    short GetAktCol() const { return m_nAktCol; }
     // find name of numrule valid for current WW-COL
     OUString GetNumRuleName() const;
     void SetNumRuleName( const OUString& rName );
