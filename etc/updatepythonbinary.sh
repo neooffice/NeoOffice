60ff4152dd63a6988c4937bb2d97f473474cd7c7
pluby <>
2019-09-14 04:19:12 +0000

    Fix failure to load Python shared libraries on macOS 10.15 by correcting the path to the Python framework

---
+++ etc/updatepythonbinary.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+if [ ! -w "$1" ] ; then
+	echo "Usage: $0 <source file>" >&2
+	exit 1
+fi
+
+for i in `otool -L "$1" | awk '{ print $1 }' | grep -e '@__________________________________________________OOO/LibreOfficePython.framework/Versions/3.3/LibreOfficePython$'`
+do
+	install_name_tool -change "$i" '@loader_path/../../../'`basename "$i"` "$1"
+done
+
+exit 0
