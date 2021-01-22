6bdeb5b8110caec71517c314b7099c7a844455ab
pluby <>
2021-01-22 23:58:03 +0000

    Eliminate one more case of temporary hang on macOS 11 by not requesting ordered windows

--- vcl/java/source/java/VCLEventQueue_cocoa.mm
+++ vcl/java/source/java/VCLEventQueue_cocoa.mm
@@ -888,7 +888,9 @@ static NSUInteger nMouseMask = 0;
 	NSApplication *pApp = [NSApplication sharedApplication];
 	if ( pApp && ![pApp isActive] )
 	{
-		[pApp enumerateWindowsWithOptions:NSWindowListOrderedFrontToBack usingBlock:^(NSWindow *pWindow, BOOL *bStop) {
+		// Eliminate temporary hang on macOS 11 by not requesting ordered
+		// windows
+		[pApp enumerateWindowsWithOptions:0 usingBlock:^(NSWindow *pWindow, BOOL *bStop) {
 			if ( bStop )
 				*bStop = NO;
 
