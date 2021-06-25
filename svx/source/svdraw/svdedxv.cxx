bf5683eadbc931b2f72dc9e96a87ccc967c39501
pluby <>
2021-06-25 21:09:47 +0000

    Merge changes from the NeoOffice-2021_branch branch

--- svx/source/svdraw/svdedxv.cxx
+++ svx/source/svdraw/svdedxv.cxx
@@ -15,6 +15,13 @@
  *   License, Version 2.0 (the "License"); you may not use this file
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ * 
+ *   Modified June 2021 by Patrick Luby. NeoOffice is only distributed
+ *   under the GNU General Public License, Version 3 as allowed by Section 3.3
+ *   of the Mozilla Public License, v. 2.0.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <com/sun/star/i18n/WordType.hpp>
@@ -1597,7 +1604,12 @@ bool SdrObjEditView::SetAttributes(const SfxItemSet& rSet, bool bReplaceAll)
             FlushComeBackTimer();
             bRet=true;
         }
+#ifdef USE_JAVA
+        // Fix Mac App Store crash by checking for NULL outliner view
+        if(!bNoEEItems && pTextEditOutlinerView)
+#else	// USE_JAVA
         if(!bNoEEItems)
+#endif	// USE_JAVA
         {
             // and now the attributes to the EditEngine
             if (bReplaceAll) {
