f2fb30608c1bd7c2a5830735afb3660a5060315c
PL <pluby@guibomacbookpro.local>
2022-10-10 16:17:08 -0400

    Remove files not in NeoOffice-2022_3 tag

--- etc/upgrade/savechanges.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "../$1" -o ! -f "$2" ] ; then
-echo "Usage: $0 <module> <file>" >&2
-exit 1
-fi
-
-diff -dup "/Volumes/Sierra/NeoBuilds/libreoffice-4.4.7.2/$1/$2" "$2"
-cp "../build/libreoffice-5.4.7.2/$1/$2" "$2"
