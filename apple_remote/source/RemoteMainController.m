60e9caf42e00d4d414f606caf9e5b787845504e3
PL <>
2023-03-29 19:00:56 -0400

    Remove GPL modification so that the source code is licensed under the same license as LibreOffice

--- apple_remote/source/RemoteMainController.m
+++ apple_remote/source/RemoteMainController.m
@@ -26,15 +26,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  * 
- * This file incorporates work covered by the following license notice:
- *
- *   Modified February 2017 by Patrick Luby. NeoOffice is only distributed
- *   under the GNU General Public License, Version 3 as allowed by Section 3.3
- *   of the Mozilla Public License, v. 2.0.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  *****************************************************************************/
 
 #import <apple_remote/RemoteMainController.h>
